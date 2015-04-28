17fbff7a8a14bc1ee53217f9b316d611e0047a3e
Derk Rembold <rembold@de.ibm.com>
2015-04-28 10:48:11 +0200

    files to create base image, corrected pervfiles.mk
    Change-Id: I78f6f1aa49c2f11b2e3ede82f19b48d7f14a50a8
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/17478
    Reviewed-by: Brian Silver <bsilver@us.ibm.com>
    Reviewed-by: Derk Rembold <rembold@de.ibm.com>
    Tested-by: Derk Rembold <rembold@de.ibm.com>

--- sbe/image/topfiles.mk
+++ sbe/image/topfiles.mk
@@ -1,6 +1,11 @@
 TOP-C-SOURCES = base_ppe_demo.c
-TOP-CPP-SOURCES = base_main.C
+TOP-CPP-SOURCES = 
 TOP-S-SOURCES = base_ppe_header.S
 
+TOP-FIXED-HEADERS += $(IMAGE_SRCDIR)/proc_sbe_fixed_perv.H
+TOP-FIXED-HEADERS += $(IMAGE_SRCDIR)/proc_sbe_fixed_proc_chip.H
+TOP-FIXED-HEADERS += $(IMAGE_SRCDIR)/proc_sbe_fixed_core.H
+TOP-FIXED-HEADERS += $(IMAGE_SRCDIR)/proc_sbe_fixed_ex.H
+TOP-FIXED-HEADERS += $(IMAGE_SRCDIR)/proc_sbe_fixed_eq.H
 
 TOP_OBJECTS = $(TOP-C-SOURCES:.c=.o) $(TOP-CPP-SOURCES:.C=.o) $(TOP-S-SOURCES:.S=.o)
