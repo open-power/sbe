9ed31b209ed33463e525102a8ceda965489f3331
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-06-17 13:50:58 +0200

    PERV SBE: Level 1 Procedure - p9_sbe_arrayinit
    HWP name changed from sbe_nestarrayinit to sbe_arrayinit
    Has new UML framework perv profile release 1.2.14
    
    Change-Id: I768c9c1c3271d278adbf5410d1e9faa7d4907906
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/18552
    Reviewed-by: Bilicon Patil <bilpatil@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: Derk Rembold <rembold@de.ibm.com>
    Tested-by: Derk Rembold <rembold@de.ibm.com>

---
+++ hwp/perv/p9_sbe_arrayinit.C
@@ -0,0 +1,29 @@
+//------------------------------------------------------------------------------
+/// @file  p9_sbe_arrayinit.C
+///
+/// @brief array init procedure to be called with any chiplet target
+//------------------------------------------------------------------------------
+// *HWP HWP Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP HWP Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner         : Brian Silver <bsilver@us.ibm.com>
+// *HWP Team             : Perv
+// *HWP Level            : 1
+// *HWP Consumed by      : SBE
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_sbe_arrayinit.H"
+
+
+
+fapi2::ReturnCode p9_sbe_arrayinit(const
+                                   fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> & i_target_chip)
+{
+    FAPI_DBG("Entering ...");
+
+    FAPI_DBG("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
