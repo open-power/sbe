7e816ad7b12d452b3d00661d902f8db9b078332e
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-06-17 13:35:57 +0200

    PERV SBE: Level 1 Procedure - p9_sbe_gptr_time_repr_initf
    HWP name changed from sbe_nest_gptr_time_repr_initf to sbe_gptr_time_repr_initf
    Has new UML framework perv profile release 1.2.14
    
    Change-Id: I8a2be35546490c2d095d8cc9dc737f8224bfc1be
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/18548
    Reviewed-by: Bilicon Patil <bilpatil@in.ibm.com>
    Reviewed-by: Johannes Koesters <koesters@de.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-by: Derk Rembold <rembold@de.ibm.com>
    Tested-by: Derk Rembold <rembold@de.ibm.com>

---
+++ hwp/perv/p9_sbe_gptr_time_repr_initf.C
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+/// @file  p9_sbe_gptr_time_repr_initf.C
+///
+/// @brief Scan 0  and Load repair , time and GPTR rings for all enabled chiplets
+// *!
+// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
+// *! BACKUP NAME :                   Email:
+//------------------------------------------------------------------------------
+// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP FW Owner    : Brian Silver <bsilver@us.ibm.com>
+// *HWP Team        : Perv
+// *HWP Level       : 1
+// *HWP Consumed by : SBE
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_sbe_gptr_time_repr_initf.H"
+
+
+
+fapi2::ReturnCode p9_sbe_gptr_time_repr_initf(const
+        fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> & i_target_chip)
+{
+    FAPI_DBG("Entering ...");
+
+    FAPI_DBG("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
