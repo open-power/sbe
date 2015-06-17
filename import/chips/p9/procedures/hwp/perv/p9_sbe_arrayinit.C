3df61fc5a26d8686ef2767a46dadb6bf6a4059b7
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-06-17 13:50:58 +0200

    PERV SBE: Level 1 Procedure - p9_sbe_arrayinit
    HWP name changed from sbe_nestarrayinit to sbe_arrayinit
    Has new UML framework perv profile release 1.2.14
    
    Change-Id: I9b9dfe6b69b3c11fa609e654ebb9156444b1d341
    Original-Change-Id: I768c9c1c3271d278adbf5410d1e9faa7d4907906
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/18552
    Reviewed-by: Bilicon Patil <bilpatil@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: Derk Rembold <rembold@de.ibm.com>
    Tested-by: Derk Rembold <rembold@de.ibm.com>
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21824
    Tested-by: Jenkins Server
    Reviewed-by: Amit J. Tendolkar <amit.tendolkar@in.ibm.com>

---
+++ import/chips/p9/procedures/hwp/perv/p9_sbe_arrayinit.C
@@ -0,0 +1,47 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: chips/p9/procedures/hwp/perv/p9_sbe_arrayinit.C $             */
+/*                                                                        */
+/* IBM CONFIDENTIAL                                                       */
+/*                                                                        */
+/* EKB Project                                                            */
+/*                                                                        */
+/* COPYRIGHT 2015                                                         */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* The source code for this program is not published or otherwise         */
+/* divested of its trade secrets, irrespective of what has been           */
+/* deposited with the U.S. Copyright Office.                              */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @file  p9_sbe_arrayinit.C
+///
+/// @brief array init procedure to be called with any chiplet target
+//------------------------------------------------------------------------------
+// *HWP HWP Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP HWP Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner         : Brian Silver <bsilver@us.ibm.com>
+// *HWP Team             : Perv
+// *HWP Level            : 1
+// *HWP Consumed by      : SBE
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_sbe_arrayinit.H"
+
+
+
+fapi2::ReturnCode p9_sbe_arrayinit(const
+                                   fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
+{
+    FAPI_DBG("Entering ...");
+
+    FAPI_DBG("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
