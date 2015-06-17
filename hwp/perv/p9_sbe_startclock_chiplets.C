da5d631051d04e7c37c9a83a4fbf7bee66890bad
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-06-17 13:22:10 +0200

    PERV SBE: Level 1 Procedure - p9_sbe_startclock_chiplets
    Start clock procedure for Xbus, Obus, PCIe
    
    Change-Id: Ia7ef79f162b6ec8eef398f690e8bcdf404318553
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/18546
    Reviewed-by: Bilicon Patil <bilpatil@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: Derk Rembold <rembold@de.ibm.com>
    Tested-by: Derk Rembold <rembold@de.ibm.com>

---
+++ hwp/perv/p9_sbe_startclock_chiplets.C
@@ -0,0 +1,29 @@
+//------------------------------------------------------------------------------
+/// @file  p9_sbe_startclock_chiplets.C
+///
+/// @brief Start clock procedure for XBUS, OBUS, PCIe
+//------------------------------------------------------------------------------
+// *HWP HWP Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP HWP Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner         : sunil kumar <skumar8j@in.ibm.com>
+// *HWP Team             : Perv
+// *HWP Level            : 1
+// *HWP Consumed by      : SBE
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_sbe_startclock_chiplets.H"
+
+
+
+fapi2::ReturnCode p9_sbe_startclock_chiplets(const
+        fapi2::Target<fapi2::TARGET_TYPE_PERV> & i_target_chiplets)
+{
+    FAPI_DBG("Entering ...");
+
+    FAPI_DBG("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
