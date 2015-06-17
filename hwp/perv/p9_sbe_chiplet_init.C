9661ee5ad774142fab2cacc74e9eb455d3763b1e
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-06-17 13:43:56 +0200

    PERV SBE: Level 1 Procedure - p9_sbe_chiplet_init
    HWP name changed from sbe_nest_chiplet_init to sbe_chiplet_init
    Has new UML framework perv profile release 1.2.14
    
    Change-Id: I60455cc8e7cb79758895abba032a8ebb8f6a719f
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/18550
    Reviewed-by: Derk Rembold <rembold@de.ibm.com>
    Tested-by: Derk Rembold <rembold@de.ibm.com>

---
+++ hwp/perv/p9_sbe_chiplet_init.C
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+/// @file  p9_sbe_chiplet_init.C
+///
+/// @brief init procedure for all enabled chiplets
+// *!
+// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
+// *! BACKUP NAME :                   Email:
+//------------------------------------------------------------------------------
+// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP FW Owner    : Brian Silver <bsilver@us.ibm.com>
+// *HWP Team        : Perv
+// *HWP Level       : 1
+// *HWP Consumed by : SBE
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_sbe_chiplet_init.H"
+
+
+
+fapi2::ReturnCode p9_sbe_chiplet_init(const
+                                      fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> & i_target_chip)
+{
+    FAPI_DBG("Entering ...");
+
+    FAPI_DBG("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
