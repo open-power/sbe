d68a0096594cb5b4431ae7ff41575aab6b610031
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-06-17 12:51:28 +0200

    PERV SBE: Level 1 Procedure - p9_sbe_chiplet_pll_initf
    procedure for scan initializing PLL config bits for L2 and L3 plls
    
    Change-Id: I62ea0dd17c1714c817fb271af0cc5643a2b67273
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/18542
    Reviewed-by: Bilicon Patil <bilpatil@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: Johannes Koesters <koesters@de.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-by: Derk Rembold <rembold@de.ibm.com>
    Tested-by: Derk Rembold <rembold@de.ibm.com>

---
+++ hwp/perv/p9_sbe_chiplet_pll_initf.C
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+/// @file  p9_sbe_chiplet_pll_initf.C
+///
+/// @brief procedure for scan initializing PLL config bits for L2 and L3 plls
+// *!
+// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
+// *! BACKUP NAME : srinivas naga     Email: srinivan@in.ibm.com
+//------------------------------------------------------------------------------
+// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP FW Owner    : sunil kumar <skumar8j@in.ibm.com>
+// *HWP Team        : Perv
+// *HWP Level       : 1
+// *HWP Consumed by : SBE
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_sbe_chiplet_pll_initf.H"
+
+
+
+fapi2::ReturnCode p9_sbe_chiplet_pll_initf(const
+        fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> & i_target_chip)
+{
+    FAPI_DBG("Entering ...");
+
+    FAPI_DBG("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
