758f7fad0b2011f0591b87bddee74f361797f962
Derk Rembold <rembold@de.ibm.com>
2015-06-30 10:59:20 +0200

    add fixed section into pibmem image
    modified linksbe.cmd
    worked in Sachins comments. Removed sbefixed.S from sbefw file.
    improved sbefwfiles.mk
    
    Change-Id: I773a701f1a7428d969c08254d07e0b6e6d9d9b79
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/18848
    Reviewed-by: Derk Rembold <rembold@de.ibm.com>
    Tested-by: Derk Rembold <rembold@de.ibm.com>

--- sbe/sbefw/topfiles.mk
+++
@@ -1,5 +0,0 @@
-TOP-CPP-SOURCES = sbemain.C  sbeirq.C sbecmdreceiver.C sbecmdprocessor.C sbecmdparser.C sbecmdscomaccess.C sbecmdiplcontrol.C sbefifo.C
-TOP-C-SOURCES =
-TOP-S-SOURCES =
-
-TOP_OBJECTS = $(TOP-C-SOURCES:.c=.o) $(TOP-CPP-SOURCES:.C=.o) $(TOP-S-SOURCES:.S=.o)
