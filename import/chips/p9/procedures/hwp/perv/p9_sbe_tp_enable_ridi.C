39ba334ccb149718e169a770fa0c276a60e01946
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-07-14 08:19:24 +0200

    PERV SBE: Level 2 Procedure - p9_sbe_tp_enable_ridi
    Enables ridi bits in RC regs after scan initialize and start clock the pervasive chiplet
    
    Change-Id: I076c0c4b88018f27c5a77a1e8cbb8034c8c9f665
    Original-Change-Id: I21c6efd954bc6befedda68bea6059f27394d4d44
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/19077
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Martin Peschke <mpeschke@de.ibm.com>
    Reviewed-by: Derk Rembold <rembold@de.ibm.com>
    Tested-by: Derk Rembold <rembold@de.ibm.com>
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21829
    Tested-by: Jenkins Server
    Reviewed-by: Amit J. Tendolkar <amit.tendolkar@in.ibm.com>

--- import/chips/p9/procedures/hwp/perv/p9_sbe_tp_enable_ridi.C
+++ import/chips/p9/procedures/hwp/perv/p9_sbe_tp_enable_ridi.C
@@ -20,30 +20,39 @@
 /// @file  p9_sbe_tp_enable_ridi.C
 ///
 /// @brief enables ridi bits in RC regs after scan initialize and start clock the pervasive chiplet
-// *!
-// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
-// *! BACKUP NAME :                   Email:
 //------------------------------------------------------------------------------
-// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
-// *HWP FW Owner    : sunil kumar <skumar8j@in.ibm.com>
-// *HWP Team        : Perv
-// *HWP Level       : 1
-// *HWP Consumed by : SBE
+// *HWP HW Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner         : sunil kumar <skumar8j@in.ibm.com>
+// *HWP Team             : Perv
+// *HWP Level            : 2
+// *HWP Consumed by      : SBE
 //------------------------------------------------------------------------------
 
 
 //## auto_generated
 #include "p9_sbe_tp_enable_ridi.H"
 
+#include "perv_scom_addresses.H"
 
 
 fapi2::ReturnCode p9_sbe_tp_enable_ridi(const
                                         fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
 {
+    fapi2::buffer<uint64_t> l_data64;
     FAPI_DBG("Entering ...");
+
+    FAPI_INF("Enable Recievers, Drivers DI1 & DI2");
+    //Setting ROOT_CTRL1 register value
+    FAPI_TRY(fapi2::getScom(i_target_chip, PERV_ROOT_CTRL1_SCOM, l_data64));
+    l_data64.setBit<19>();  //PIB.ROOT_CTRL1.TP_RI_DC_B = 1
+    l_data64.setBit<20>();  //PIB.ROOT_CTRL1.TP_DI1_DC_B = 1
+    l_data64.setBit<21>();  //PIB.ROOT_CTRL1.TP_DI2_DC_B = 1
+    FAPI_TRY(fapi2::putScom(i_target_chip, PERV_ROOT_CTRL1_SCOM, l_data64));
 
     FAPI_DBG("Exiting ...");
 
-    return fapi2::FAPI2_RC_SUCCESS;
+fapi_try_exit:
+    return fapi2::current_err;
 
 }
