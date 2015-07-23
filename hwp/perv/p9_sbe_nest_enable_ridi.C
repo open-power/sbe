bbe4e5926f9040cfd6999ab02cb3f2023e77e264
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-07-23 11:41:42 +0200

    PERV SBE: Level 2 Procedure - p9_sbe_nest_enable_ridi
    Enable ridi controls for NEST logic
    
    Change-Id: I4b9e72d195f99f8a42678cedbd9907aa5a22e895
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/19277
    Tested-by: Jenkins Server
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: SRINIVAS V. POLISETTY <srinivan@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- hwp/perv/p9_sbe_nest_enable_ridi.C
+++ hwp/perv/p9_sbe_nest_enable_ridi.C
@@ -2,30 +2,89 @@
 /// @file  p9_sbe_nest_enable_ridi.C
 ///
 /// @brief Enable ridi controls for NEST logic
-// *!
-// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
-// *! BACKUP NAME :                   Email:
 //------------------------------------------------------------------------------
-// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
-// *HWP FW Owner    : sunil kumar <skumar8j@in.ibm.com>
-// *HWP Team        : Perv
-// *HWP Level       : 1
-// *HWP Consumed by : SBE
+// *HWP HW Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner        : sunil kumar <skumar8j@in.ibm.com>
+// *HWP Team            : Perv
+// *HWP Level           : 2
+// *HWP Consumed by     : SBE
 //------------------------------------------------------------------------------
 
 
 //## auto_generated
 #include "p9_sbe_nest_enable_ridi.H"
 
+#include "perv_scom_addresses.H"
 
+
+static fapi2::ReturnCode p9_sbe_nest_enable_ridi_net_ctrl_action_function(
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV> & i_target_chiplet);
 
 fapi2::ReturnCode p9_sbe_nest_enable_ridi(const
         fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> & i_target_chip)
 {
+    auto l_perv_functional_vector =
+        i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+        (fapi2::TARGET_STATE_FUNCTIONAL);
     FAPI_DBG("Entering ...");
+
+    for (auto l_chplt_trgt : l_perv_functional_vector)
+    {
+        uint8_t l_attr_chip_unit_pos = 0; //actual value is read in FAPI_ATTR_GET below
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_chplt_trgt,
+                               l_attr_chip_unit_pos));
+
+        if (!((l_attr_chip_unit_pos == 0x07
+                || l_attr_chip_unit_pos == 0x08/* McChiplet */) ||
+                (l_attr_chip_unit_pos == 0x02 || l_attr_chip_unit_pos == 0x03
+                 || l_attr_chip_unit_pos == 0x04
+                 || l_attr_chip_unit_pos == 0x05/* NestChiplet */)))
+        {
+            continue;
+        }
+
+        FAPI_INF("Call p9_sbe_nest_enable_ridi_net_ctrl_action_function");
+        FAPI_TRY(p9_sbe_nest_enable_ridi_net_ctrl_action_function(l_chplt_trgt));
+    }
 
     FAPI_DBG("Exiting ...");
 
-    return fapi2::FAPI2_RC_SUCCESS;
+fapi_try_exit:
+    return fapi2::current_err;
+
+}
+
+/// @brief Enable Drivers/Recievers of Nest chiplet
+///
+/// @param[in]     i_target_chiplet   Reference to TARGET_TYPE_PERV target
+/// @return  FAPI2_RC_SUCCESS if success, else error code.
+static fapi2::ReturnCode p9_sbe_nest_enable_ridi_net_ctrl_action_function(
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV> & i_target_chiplet)
+{
+    bool l_read_reg = false;
+    fapi2::buffer<uint64_t> l_data64;
+    FAPI_DBG("Entering ...");
+
+    FAPI_INF("Check for chiplet enable");
+    //Getting NET_CTRL0 register value
+    FAPI_TRY(fapi2::getScom(i_target_chiplet, PERV_NET_CTRL0, l_data64));
+    l_read_reg = l_data64.getBit<0>();  //l_read_reg = NET_CTRL0.CHIPLET_ENABLE
+
+    if ( l_read_reg )
+    {
+        FAPI_INF("Enable Recievers, Drivers DI1 & DI2");
+        //Setting NET_CTRL0 register value
+        l_data64.flush<0>();
+        l_data64.setBit<19>();  //NET_CTRL0.RI_N = 1
+        l_data64.setBit<20>();  //NET_CTRL0.DI1_N = 1
+        l_data64.setBit<21>();  //NET_CTRL0.DI2_N = 1
+        FAPI_TRY(fapi2::putScom(i_target_chiplet, PERV_NET_CTRL0_WOR, l_data64));
+    }
+
+    FAPI_DBG("Exiting ...");
+
+fapi_try_exit:
+    return fapi2::current_err;
 
 }
