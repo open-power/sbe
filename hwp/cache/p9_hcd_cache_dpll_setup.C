af2801fc63e15d56dd19ec0dbdf5512d78c807da
David Young <davidy@us.ibm.com>
2015-08-12 21:18:28 -0500

    PPE-HWP: [Level 2] Dllsetup Hcode Procedure
    Dllsetup runs on cronus.  Wrapper function included.
    
    XML error file added.
    cachehcderror.mk updates with dll errors xml file
    
    Change-Id: I51189a9ca7c2541ddc86aeb7896feb23a2426f5b
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/19807
    Tested-by: Jenkins Server
    Reviewed-by: Bilicon Patil <bilpatil@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- hwp/cache/p9_hcd_cache_dpll_setup.C
+++ hwp/cache/p9_hcd_cache_dpll_setup.C
@@ -2,177 +2,274 @@
 /// @file  p9_hcd_cache_dpll_setup.C
 /// @brief Quad DPLL Setup
 ///
-/// *HWP HWP Owner   : David Du       <daviddu@us.ibm.com>
-/// *HWP FW Owner    : Sangeetha T S  <sangeet2@in.ibm.com>
-/// *HWP Team        : PM
-/// *HWP Consumed by : SBE:SGPE
-/// *HWP Level       : 1
-///
-/// Procedure Summary:
-///   Initfiles in procedure defined on VBU ENGD wiki  (TODO add link)
-///   DPLL tune bits are not dependent on frequency
-///   Put DPLL into bypass
-///   Set DPLL syncmux sel
-///   Set clock controller scan ratio to 1:1 as this is done at refclk speeds
-///   Load the EX DPLL scan ring
-///   Set clock controller scan ratio to 8:1 for future scans
-///   Frequency is controlled by the Quad PPM
-///   Actual frequency value for boot is stored into the Quad PPM by
-///     p9_hcd_setup_evid.C in istep 2
-///   In real cache STOP exit, the frequency value is persistent
-///   Enable the DPLL in the correct mode
-///   non-dynamic
-///   Slew rate established per DPLL team
-///   Take the cache glitchless mux out of reset
-///     (TODO:  is there still a reset on P9?)
-///   Remove DPLL bypass
-///   Drop DPLL Tholds
-///   Check for DPLL lock
-///   Timeout:  200us
-///   Switch cache glitchless mux to use the DPLL
-///
+// *HWP HWP Owner   : David Young       <davidy@us.ibm.com>
+// *HWP FW Owner    : Sangeetha T S  <sangeet2@in.ibm.com>
+// *HWP Team        : PM
+// *HWP Consumed by : SBE:SGPE
+// *HWP Level       : 2
+//
+// Procedure Summary:
+//   Put DPLL into bypass
+//   Set DPLL syncmux sel
+//   *Set clock controller scan ratio to 1:1 as this is done at refclk speeds
+//   *xLoad the EX DPLL scan ring
+//   *Set clock controller scan ratio to 8:1 for future scans
+//   Frequency is controlled by the Quad PPM
+//   Actual frequency value for boot is stored into the Quad PPM by
+//     p9_hcd_setup_evid.C in istep 2
+//   In real cache STOP exit, the frequency value is persistent
+//   Enable the DPLL in the correct mode
+//   non-dynamic
+//   Slew rate established per DPLL team
+//   Take the cache glitchless mux out of reset
+//     (TODO:  is there still a reset on P9?)
+//   Remove DPLL bypass
+//   Drop DPLL Tholds
+//   Check for DPLL lock
+//   Timeout:  200us
+//   Switch cache glitchless mux to use the DPLL
+//
 
 //-----------------------------------------------------------------------------
 // Includes
 //-----------------------------------------------------------------------------
 #include <fapi2.H>
-//#include <common_scom_addresses.H>
+#if 0
+#include <common_scom_addresses.H>
 //will be replaced with real scom address header file
+#endif
 #include "p9_hcd_cache_dpll_setup.H"
 
 //-----------------------------------------------------------------------------
 // Constant Definitions
 //-----------------------------------------------------------------------------
 
-//#define	DPLL_LOCK_BIT,                 50
-//#define	DPLL_LOCK_MAX_POLL_LOOPS,      0x1600
-//#define	DPLL_LOCK_DELAY_CYCLES,        0x10
-//#define	STEP_SBE_DPLL_SETUP_LOCK,      1
-//#define	STEP_SBE_DPLL_GMUX_SET,        2
-//#define	STEP_SBE_DPLL_SETUP_COMPLETE,  3
+#define QPPM_DPLLFREQ 0x100f0151
+#define QPPM_DPLLPARM 0x100f0152
+#define QPPM_DPLLPARM_CLR 0x100f0153
+#define QPPM_DPLLPARM_OR 0x100f0154
+#define QPPM_DPLLSTAT 0x100f0155
 
-// For a 2000GHz nest (500MHz Pervasive), 2ns clocks exists
-// For a DPLL lock time of >150us from power on, 150000/2 = 75000
-// d75000 => 0x124F8   ----> round up to 0x15000
+#define PERV_CLK_REGION 0x00030006
+#define PERV_TP_CLK_REGION 0x01030006
+#define PERV_N0_CLK_REGION 0x02030006
 
-// For a 2400GHz nest (600MHz Pervasive), 1.667ns clocks exists
-// For a DPLL lock time of >150us from power on, 150000/1.667 = 89982
-// d90000 => 0x15f90   ----> round up to 0x16000
+enum PERV_CLK_REGION_param {
+    CLOCK_CMD_SIZE=2,
+    CLOCK_CMD_BIT=0,
+    CLOCK_REGION_UNIT0_BIT=2,
+    CLOCK_REGION_UNIT1=3,
+    CLOCK_REGION_UNIT2=4,
+    CLOCK_REGION_UNIT3=5,
+    CLOCK_REGION_UNIT4=6,
+    CLOCK_REGION_UNIT5=7,
+    CLOCK_REGION_PLL=8,
+    CLOCK_REGION_OSCSW=9,
+    SEL_THOLD_SL=10,
+    SEL_THOLD_NSL=11,
+    SEL_THOLD_ARY=12
+};
 
-//#define DPLL_LOCK_DELAY 0x10
-//#define DPLL_LOCK_LOOP_NUMBER 0x1600
+enum PERV_CLK_REGION_cmd {
+    CLOCK_CMD_NOP=0,
+    CLOCK_CMD_START=1,
+    CLOCK_CMD_STOP=2,
+    CLOCK_CMD_PULSE=3
+};
 
-//#define ex_dpll_lock_delay_mult  0x1600
-//#define ex_glsmux_post_delay_mult  0x1
+enum {FMAX_BIT=3, FMIN_BIT=15, FMAX_FIELD_SIZE=9, FMIN_FIELD_SIZE=9};
+
+enum {
+    PDPLL_LOCK_SEL_BIT=0,
+    DYNAMIC_FILTER_ENABLE_BIT=1,
+    FF_BYPASS_BIT=2,
+    FF_SLEWRATE_BIT=6,
+    FF_SLEWRATE_FIELD_SIZE=10
+};
+
+enum {
+    FREQOUT_BIT=1,
+    EXTENDED_FREQOUT_BIT=12,
+    FSAFE_ACTIVE_BIT=16,
+    FREQ_CHANGE_BIT=22,
+    LOCK_BIT=23,
+    DPLL_CHAR_TBD_BIT=24,
+    FASTER_THAN_FMAX_PLUS_DELTA1_BIT=25,
+    SLOWER_THAN_FMIN_MINUS_DELTA2_BIT=26,
+    FREQOUT_MAX_BIT=33,
+    FREQOUT_MAX_EXTENDED_BIT=44,
+    FREQOUT_MIN_BIT=49,
+    FREQOUT_MIN_EXTENDED_BIT=60
+};
+
+enum {FREQOUT_FIELD_SIZE=11, EXTENDED_FREQOUT_FIELD_SIZE=4,
+      FREQOUT_MAX_FIELD_SIZE=11, FREQOUT_MAX_EXTENDED_FIELD_SIZE=4,
+      FREQOUT_MIN_FIELD_SIZE=11, FREQOUT_MIN_EXTENDED_FIELD_SIZE=4};
+
+#define QPPM_QACCR 0x100f0160
+#define QPPM_QACCR_CLR 0x100f0161
+#define QPPM_QACCR_OR 0x100f0162
+
+enum QPPM_QACCR_fields {
+    CORE_CLK_SB_STRENGTH_BIT=0, CORE_CLK_SB_STRENGTH_LENGTH=4,
+    CORE_CLK_SB_SPARE_BIT=4, CORE_CLK_SB_SPARE_LENGTH=1,
+    CORE_CLK_SB_PULSE_MODE_EN_BIT=5, CORE_CLK_SB_PULSE_MODE_EN_LENGTH=1,
+    CORE_CLK_SB_PULSE_MODE_BIT=6, CORE_CLK_SB_PULSE_MODE_LENGTH=2,
+    CORE_CLK_SW_RESCLK_BIT=8, CORE_CLK_SW_RESCLK_LENGTH=4,
+    CORE_CLK_SW_SPARE_BIT=12, CORE_CLK_SW_SPARE_LENGTH=1,
+    L2_EX0_CLK_SYNC_ENABLE_BIT=13, L2_EX0_CLK_SYNC_ENABLE_LENGTH=1,
+    RESERVED_14_151_BIT=14, RESERVED_14_151_LENGTH=2,
+    L2_EX0_CLKGLM_ASYNC_RESET_BIT=16, L2_EX0_CLKGLM_ASYNC_RESET_LENGTH=1,
+    RESERVED_17_182_BIT=17, RESERVED_17_182_LENGTH=2,
+    L2_EX0_CLKGLM_SEL_BIT=19, L2_EX0_CLKGLM_SEL_LENGTH=1,
+    L2_EX0_CLK_SB_STRENGTH_BIT=20, L2_EX0_CLK_SB_STRENGTH_LENGTH=4,
+    L2_EX0_CLK_SB_SPARE0_BIT=24, L2_EX0_CLK_SB_SPARE0_LENGTH=1,
+    L2_EX0_CLK_SB_PULSE_MODE_EN_BIT=25, L2_EX0_CLK_SB_PULSE_MODE_EN_LENGTH=1,
+    L2_EX0_CLK_SB_PULSE_MODE_BIT=26, L2_EX0_CLK_SB_PULSE_MODE_LENGTH=2,
+    L2_EX0_CLK_SW_RESCLK_BIT=28, L2_EX0_CLK_SW_RESCLK_LENGTH=4,
+    L2_EX0_CLK_SW_SPARE1_BIT=32, L2_EX0_CLK_SW_SPARE1_LENGTH=1,
+    L2_EX1_CLK_SYNC_ENABLE_BIT=33, L2_EX1_CLK_SYNC_ENABLE_LENGTH=1,
+    RESERVED_34_353_BIT=34, RESERVED_34_353_LENGTH=2,
+    L2_EX1_CLKGLM_ASYNC_RESET_BIT=36, L2_EX1_CLKGLM_ASYNC_RESET_LENGTH=1,
+    RESERVED_37_384_BIT=37, RESERVED_37_384_LENGTH=2,
+    L2_EX1_CLKGLM_SEL_BIT=39, L2_EX1_CLKGLM_SEL_LENGTH=1,
+    L2_EX1_CLK_SB_STRENGTH_BIT=40, L2_EX1_CLK_SB_STRENGTH_LENGTH=4,
+    L2_EX1_CLK_SB_SPARE0_BIT=44, L2_EX1_CLK_SB_SPARE0_LENGTH=1,
+    L2_EX1_CLK_SB_PULSE_MODE_EN_BIT=45, L2_EX1_CLK_SB_PULSE_MODE_EN_LENGTH=1,
+    L2_EX1_CLK_SB_PULSE_MODE_BIT=46, L2_EX1_CLK_SB_PULSE_MODE_LENGTH=2,
+    L2_EX1_CLK_SW_RESCLK_BIT=48, L2_EX1_CLK_SW_RESCLK_LENGTH=4,
+    L2_EX1_CLK_SW_SPARE1_BIT=52, L2_EX1_CLK_SW_SPARE1_LENGTH=1,
+    RESERVED_53_55_BIT=53, RESERVED_53_55_LENGTH=3,
+    L3_CLK_SB_STRENGTH_BIT=56, L3_CLK_SB_STRENGTH_LENGTH=4,
+    L3_CLK_SB_SPARE0_BIT=60, L3_CLK_SB_SPARE0_LENGTH=1,
+    L3_CLK_SB_PULSE_MODE_EN_BIT=61, L3_CLK_SB_PULSE_MODE_EN_LENGTH=1,
+    L3_CLK_SB_PULSE_MODE_BIT=62, L3_CLK_SB_PULSE_MODE_LENGTH=2};
+
+
+#define PPM_CGCR     0x100f0165
+#define PPM_CGCR_CLR 0x100f0166
+#define PPM_CGCR_OR  0x100f0167
+
+enum PPM_CGCR_fields {
+    CLKGLM_ASYNC_RESET_BIT=0, CLKGLM_ASYNC_RESET_LENGTH=1,
+    RESERVED_1_21_BIT=1, RESERVED_1_21_LENGTH=2,
+    CLKGLM_SEL_BIT=3, CLKGLM_SEL_LENGTH=1,
+    RESERVED2_BIT=4, RESERVED2_LENGTH=60};
+
+enum PPM_CGCR_CLKGLM_SEL_field_options{
+    REFCLK=0,   //0: refclk
+    DPLL= 1};   //1: DPLL output
+                // (full speed Div1 for Core, half speed Div2 for Cache)
+
+
+enum {CYCLES_PER_MS = 500000};
+
+// TODO: count inst per loop in dissassembly
+enum {INST_PER_LOOP = 8};
 
 //-----------------------------------------------------------------------------
 // Procedure: Quad DPLL Setup
 //-----------------------------------------------------------------------------
 
-extern "C"
-{
+// Setup DPLL like this: (from dpll_spec_14nm_20140107)
+//  (marked as IPL from IPL document)
+// Initial State:  (Do we need these (1-3) in IPL?)
+// 1) reset, dpll_func_clksel, and all SL_HLD inputs are asserted
+// 2) If grid clock connected to dpll clkout, bypass also has to be asserted to allow refclk on grid
+// 3) Scan init:
+//    - All scan rings in DPLL are scanned with zeroes
+//    - Scan in VPD table values into the DPLL_MODE ring (config registers)
+//     lf_param_dc<0:15> = b1010001010000000# will change based on HW characterization
+//     lf_sdorder_dc<0:1> = b10
+//     cd_div124_dc<0:1> = b10
+//     cd_dpllout124_dc<0:1> = b10
+//     vc_vtune_dc<0:2> = 0b100 # may change based on HW characterization
+//     ref_div_dc<0:5> = b000001
+//     ref_div_vreg_dc<0:4> = b000010
+//     refclk_sel_dc = b1
+//     All configuration register bits not specified should be set to b0
+
+#define FAPI_CLEANUP() fapi_try_exit:
 
 fapi2::ReturnCode
 p9_hcd_cache_dpll_setup(
     const fapi2::Target<fapi2::TARGET_TYPE_EQ>& i_target)
 {
-
-#if 0
-    fapi2::buffer<uint64_t> data;
+    uint32_t l_loopsPerMs;
+    fapi2::buffer<uint64_t> l_data;
 
     FAPI_INF("EX Chiplet -> Start DPLL setup" )
-
-    // The setup of P0 happens external to this procedure
-    // P0 is set to point to either a single EX core, or to multiple EX
-    // cores using multicast
-    FAPI_DBG("EX Chiplet dpll_setup P0 -> 0x%02llx" , io_pore.p0.read())
-
-    // Skip DPLL and Glitchless mux setup for Sleep
-    // Glitchless mux change for the core (Sleep) done in
-    // p9_sbe_ex_chiplet_reset.
-
-    // - ifsleep D0, glm_end
-    // >>> IPL/WINKLE
-
-    // - dpll_start:
-
-    // - setp1_mcreadand D0
-
     FAPI_INF("Set up CPM PARM register for DPLL")
-    // = ld      D0, EX_DPLL_CPM_PARM_REG_0x100F0152, P1
-    FAPI_TRY(getScom(i_target, EX_DPLL_CPM_PARM_REG_0x100F0152, data));
 
+    FAPI_TRY(fapi2::getScom(i_target, QPPM_DPLLPARM, l_data));
     // ----- Make sure that ff_bypass is set to "1" -----
     // This is necessary to ensure that the DPLL is in Mode 1.  If not,
     // the lock times will go from ~30us to 3-5ms
-    // Set bit 11 of DPLL Mode register  0x150F0152
 
     FAPI_DBG("Put DPLL into Mode 1 by asserting ff_bypass.")
-    FAPI_DBG(">>> Work around  <<<<: Also disabling ping-pong synchronizer" )
-    // = ori     D0, D0, (BIT(11)|BIT(32))
-    data.setBit<11>().setBit<32>();
-
-    FAPI_DBG("Clear bits 15,16,18-23 and set bit 17 of DPLL reg 0x10F0152")
-    // = andi    D0, D0, 0xFFFE00FFFFFFFFFF
-    // = ori     D0, D0, BIT(17)
-    data.insertFromRight<15,9>(0x40);
+    l_data.setBit<FF_BYPASS_BIT>();
 
     FAPI_DBG("Set slew rate to a modest value")
-    // = ori     D0, D0, 0x8000000000000000
-    // = std     D0, EX_DPLL_CPM_PARM_REG_0x100F0152, P0
-    data.setBit<0>();
-    FAPI_TRY(putScom(i_target, EX_DPLL_CPM_PARM_REG_0x100F0152, data));
+    l_data.insertFromRight<FF_SLEWRATE_BIT,FF_SLEWRATE_FIELD_SIZE>(0x40);
+    FAPI_TRY(fapi2::putScom(i_target, QPPM_DPLLPARM, l_data));
 
     // ----- Clear dpllclk_muxsel (syncclk_muxsel) to "0" -----
     FAPI_INF("Reset syncclk_muxsel or dpllclk_muxsel")
-    // = sti     GENERIC_GP0_AND_0x00000004, P0, ~BIT(1)
-    FAPI_TRY(putScom(i_target, GENERIC_GP0_AND_0x00000004,
-                     fapi2:buffer<uint64_t>().flush<1>().clearBit<1>()));
+    // TC_SYNCCLK_MUXSEL_DC: syncclk mux for DPLL ,Reset(0b1)
+    // TODO: do we need this in this procedure?
 
-    // = sti     EX_PMGP0_AND_0x100F0101, P0, ~(BIT(11))
-    FAPI_TRY(putScom(i_target, EX_PMGP0_AND_0x100F0101,
-                     fapi2:buffer<uint64_t>().flush<1>().clearBit<11>()));
+    FAPI_TRY(fapi2::putScom(i_target, QPPM_QACCR_CLR,
+                     l_data.flush<0>().
+                     setBit<L2_EX0_CLK_SYNC_ENABLE_BIT>().
+                     setBit<L2_EX1_CLK_SYNC_ENABLE_BIT>()
+                      ));
+
+    FAPI_TRY(fapi2::putScom(i_target, PPM_CGCR_CLR,
+                     l_data.flush<0>().
+                     setBit<CLKGLM_SEL_BIT>()
+                      ));
 
     // ----- Take DPLL out of bypass -----
-    // Clear bit 5 of EX GP3 Register
     FAPI_INF("EX Chiplet -> Take DPLL out of bypass" )
-    // = sti     EX_GP3_AND_0x100F0013, P0, ~(BIT(5))
-    FAPI_TRY(putScom(i_target, EX_GP3_AND_0x100F0013,
-                     fapi2:buffer<uint64_t>().flush<1>().clearBit<5>()));
+    FAPI_TRY(fapi2::putScom(i_target, QPPM_DPLLPARM_CLR,
+            l_data.flush<0>().setBit<FF_BYPASS_BIT>()));
 
-    // ----- Drop DPLL tholds  -----
-    // Clear bit 3 of EX PMGP0 Register
+
+    // TODO: There doesn't seem to be a CLR version of this register...
     FAPI_INF("EX Chiplet -> Drop internal DPLL THOLD" )
-    // = sti     EX_PMGP0_AND_0x100F0101, P0, ~(BIT(3))
-    FAPI_TRY(putScom(i_target, EX_PMGP0_AND_0x100F0101,
-                     fapi2:buffer<uint64_t>().flush<1>().clearBit<3>()));
+    //DPLL_THOLD: Thold for the DPLL clock domain in the chiplet
+    FAPI_TRY(fapi2::getScom(i_target, PERV_TP_CLK_REGION, l_data));
+
+    FAPI_TRY(fapi2::putScom(i_target, PERV_TP_CLK_REGION,
+                 l_data.clearBit<SEL_THOLD_SL>()));
 
     // ----- Delay to wait for DPLL to lock  -----
     // TODO:  Determine whether or not we should POLL instead of put delay here.
     // Wait for >150us
 
+    // Check for lock
     FAPI_INF("Wait for DPLL to lock" )
     // - waits       DPLL_LOCK_LOOP_NUMBER*DPLL_LOCK_DELAY
+    l_loopsPerMs = CYCLES_PER_MS/INST_PER_LOOP;
+    do
+    {
+        FAPI_TRY(fapi2::getScom(i_target, QPPM_DPLLSTAT, l_data));  // poll
+        break; // TODO:  Skipping the lock checking until model is ready
+    } while ((l_data.getBit<LOCK_BIT>() != 1 ) &&
+            (--l_loopsPerMs >= 0));
 
-    // Check for lock
-    // - ldandi  D0, EX_PMGP0_0x100F0100, P1, BIT(DPLL_LOCK_BIT)
-    // - braz    D0, dpll_nolock
+    FAPI_ASSERT((l_loopsPerMs != 0),
+                 fapi2::PMPROC_DPLL_LOCK_TIMEOUT()
+                        .set_ADDRESS(QPPM_DPLLSTAT),
+                 "DPLL lock timeout");
 
-    // ----- Recycle DPLL in and out of bypass -----
-    // Clear bit 5 of EX GP3 Register
     FAPI_INF("EX Chiplet -> Recycle DPLL in and out of bypass" )
-    // = sti     EX_GP3_OR_0x100F0014, P0, BIT(5)
-    FAPI_TRY(putScom(i_target, EX_GP3_OR_0x100F0014,
-                     fapi2:buffer<uint64_t>().setBit<5>()));
-
-    // = sti     EX_GP3_AND_0x100F0013, P0, ~(BIT(5))
-    FAPI_TRY(putScom(i_target, EX_GP3_AND_0x100F0013,
-                     fapi2:buffer<uint64_t>().flush<1>().clearBit<5>()));
+    FAPI_TRY(fapi2::putScom(i_target, QPPM_DPLLPARM_OR,
+            l_data.flush<0>().setBit<FF_BYPASS_BIT>()));
+    FAPI_TRY(fapi2::putScom(i_target, QPPM_DPLLPARM_CLR,
+            l_data));
 
     FAPI_INF("EX Chiplet -> EX DPLL is locked" )
-    // - updatestep STEP_SBE_DPLL_SETUP_LOCK, D0, P1
-
-    // - glm_start:
 
     // ----- Set Glitch-Less MUXes for IPL/Winkle case ----
     // For Sleep, the core glitchless mux is change earlier as the DPLL
@@ -180,79 +277,33 @@ p9_hcd_cache_dpll_setup(
     //
     // Set various gl muxes to pass the output of the DPLLs to the clock grid.
 
-    // Read-modify-write (vs sti AND and sti OR) is done so that glitchless
-    // mux change field change is atomic (eg 1 store)
-    // - setp1_mcreadand D1
-
-    // = ld      D1,EX_PMGP0_0x100F0100,P1
-    FAPI_TRY(getScom(i_target, EX_PMGP0_0x100F0100, data));
-
-    // IPL/Winkle - Switch glitchless mux primary source to 001 = DPLL for bits
-    // 27:29
-
-    // Note: GLM async reset occured in p9_sbe_ex_chiplet_reset.
-
-    // Set tp_clkglm_sel_dc to "001"  (EX PM GP0 bits 27-29)
     FAPI_INF("EX Chiplet -> Set glitchless mux select for primary chiplet clock source to 001 ")
-    // Set tp_clkglm_eco_sel_dc to "0"  (EX PM GP0 bit 30)
-    // Set the core glitchless mux to use the primary input (b00).
-    // EX PM GP0 bits 32-33)
-    FAPI_INF("EX Chiplet -> Set glitchless mux select for core and eco domain to 0 ")
-    // = andi    D1, D1, ~(BIT(27)|BIT(28)|BIT(30)|BIT(32)|BIT(33))
-    // = ori     D1, D1, (BIT(29))
-    data.insertFromRight<27,4>(0x2).inerstFromRight<32,2>(0x0);
+    FAPI_INF("EX Chiplet -> Do we need: Set glitchless mux select for core and eco domain to 0?")
+    l_data.flush<0>().setBit<CLKGLM_SEL_BIT>();  // setBit corresponds to 1: DPLL
 
     // Store the final result to the hardware
-    // = std     D1,EX_PMGP0_0x100F0100,P0
-    FAPI_TRY(putScom(i_target, EX_PMGP0_0x100F0100, data));
-
-    // - glm_end:
-
-    // - updatestep STEP_SBE_DPLL_GMUX_SET, D0, P1
+    FAPI_TRY(fapi2::putScom(i_target, PPM_CGCR_OR, l_data));
 
     // ----- Drop ff_bypass to enable slewing -----
-    // ----- (Change from Mode 1 to mode 2)   -----
-    // Clear bit 11 of DPLL Mode register  0x150F0152
-    // CMO20131125-Further, drop the pp synchronizer bit32.
-    // CMO20131219-Keep the pp sync bit32 asserted to avoid x-leakage(HW276931).
     FAPI_INF("EX Chiplet -> Clear ff_bypass to switch into slew-controlled mode")
-    // - setp1_mcreadand D1
-    // = ld      D1, EX_DPLL_CPM_PARM_REG_0x100F0152, P1
-    // = andi    D1, D1, ~(BIT(11))
-    // = std     D1, EX_DPLL_CPM_PARM_REG_0x100F0152, P0
-    FAPI_TRY(getScom(i_target, EX_DPLL_CPM_PARM_REG_0x100F0152, data));
-    data.clearBit<11>();
-    FAPI_TRY(putScom(i_target, EX_DPLL_CPM_PARM_REG_0x100F0152, data));
+    l_data.flush<0>().setBit<FF_BYPASS_BIT>();
+    FAPI_TRY(fapi2::putScom(i_target, QPPM_DPLLPARM_CLR, l_data));
 
     // ----- Drop other tholds -----
-    // Clear bit 3 of EX PM GP0 Register
-    //    FAPI_INF("EX Chiplet -> Drop DPLL thold" )
-    //    sti     EX_PMGP0_AND_0x100F0101,P0, ~(BIT(3))
+    FAPI_INF("EX Chiplet -> Drop DPLL thold" )
+    //DPLL_THOLD: Thold for the DPLL clock domain in the chiplet
+    FAPI_TRY(fapi2::getScom(i_target, PERV_TP_CLK_REGION, l_data));
+    FAPI_TRY(fapi2::putScom(i_target, PERV_TP_CLK_REGION,
+                 l_data.clearBit<SEL_THOLD_NSL>().
+                      clearBit<SEL_THOLD_ARY>()));
+
 
     // - updatestep STEP_SBE_DPLL_SETUP_COMPLETE, D0, P1
     FAPI_INF("EX Chiplet -> DPLL setup completed" )
-
-    // DEBUG only
-    // - setp1_mcreadand D1
-    // = ld      D0,EX_GP0_0x10000000,P1
-    FAPI_TRY(getScom(i_target, EX_GP0_0x10000000, data));
-
-    // - dpll_nolock:
-    FAPI_ERR("EX_DPLL -> Failed to lock");
-    // - reqhalt RC_SBE_DPLL_SETUP_NOLOCK
-
-    return fapi2::FAPI2_RC_SUCCESS;
-
-    FAPI_CLEANUP();
-    return fapi2::FAPI2_RC_PLAT_ERR_SEE_DATA;
-
-#endif
-
-    return fapi2::FAPI2_RC_SUCCESS;
-
+    // fapi2::current_err will be initialized with FAPI2_RC_SUCCESS
+    FAPI_CLEANUP()
+    return fapi2::current_err;
 } // Procedure
 
-
-} // extern C
 
 
