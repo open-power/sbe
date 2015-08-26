beaff28e2f7f74b40e9fc0ac384680d33ef26b10
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-08-26 10:48:59 +0200

    PERV SBE: Level 2 Procedure - p9_sbe_tp_switch_gears
    Switch from refclock to PLL AND adjust I2C
    
    Change-Id: I8e6c1d87009a269619e0b505b522bb12d41607e8
    Original-Change-Id: I00e7532173dc9b4b576cc331391a20a72c28c4f6
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/20099
    Tested-by: Jenkins Server
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21832
    Reviewed-by: Amit J. Tendolkar <amit.tendolkar@in.ibm.com>

--- import/chips/p9/procedures/hwp/perv/p9_sbe_tp_switch_gears.C
+++ import/chips/p9/procedures/hwp/perv/p9_sbe_tp_switch_gears.C
@@ -19,28 +19,36 @@
 //------------------------------------------------------------------------------
 /// @file  p9_sbe_tp_switch_gears.C
 ///
-/// @brief SBE switch gears Procedure
-// *!
-// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
-// *! BACKUP NAME :                   Email:
+/// @brief Switch from refclock to PLL AND adjust I2C
 //------------------------------------------------------------------------------
-// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
-// *HWP FW Owner    : Brian Silver <bsilver@us.ibm.com>
-// *HWP Team        : Perv
-// *HWP Level       : 1
-// *HWP Consumed by : SBE
+// *HWP HW Owner        : abagarw8 <srinivan@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner        : sunil kumar <skumarj8@in.ibm.com>
+// *HWP Team            : Perv
+// *HWP Level           : 2
+// *HWP Consumed by     : SBE
 //------------------------------------------------------------------------------
 
 
 //## auto_generated
 #include "p9_sbe_tp_switch_gears.H"
+
+#include "p9_sbe_gear_switcher.H"
+
+
 fapi2::ReturnCode p9_sbe_tp_switch_gears(const
         fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
 {
-    FAPI_DBG("p9_sbe_tp_switch_gears: Entering ...");
+    FAPI_DBG("Entering ...");
 
-    FAPI_DBG("p9_sbe_tp_switch_gears: Exiting ...");
+    FAPI_TRY(p9_sbe_gear_switcher_apply_i2c_bit_rate_divisor_setting(
+                 i_target_chip));
 
-    return fapi2::FAPI2_RC_SUCCESS;
+    FAPI_TRY(p9_sbe_gear_switcher_i2c_stop_sequence(i_target_chip));
+
+    FAPI_DBG("Exiting ...");
+
+fapi_try_exit:
+    return fapi2::current_err;
 
 }
