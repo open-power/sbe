204bb2929f695e3f11ef1887dcac13fb3cc4b890
Sunil.Kumar <skumar8j@in.ibm.com>
2015-09-14 03:35:33 -0500

    Error XML files and respective entries
    Change-Id: I59fffab259bc0a55f76ab626319602588800c9ba
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/20481
    Tested-by: Jenkins Server
    Reviewed-by: Abhishek Agarwal <abagarw8@in.ibm.com>
    Reviewed-by: SRINIVAS V. POLISETTY <srinivan@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- hwp/perv/perverrors.mk
+++ hwp/perv/perverrors.mk
@@ -17,6 +17,9 @@ ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_arrayinit_errors.xml
 ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_gear_switcher_errors.xml
 ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_setup_evid_errors.xml
 ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_common_errors.xml
-
-
-
+ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_perv_sbe_cmn_errors.xml
+ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_nest_startclocks_errors.xml
+ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_tp_chiplet_init3_errors.xml
+ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_npll_setup_errors.xml
+ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_chiplet_pll_setup_errors.xml
+ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_tp_arrayinit_errors.xml
