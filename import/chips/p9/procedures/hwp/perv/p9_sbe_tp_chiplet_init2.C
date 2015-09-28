c01023a990da57d691de4921ca703053995803f4
Sunil.Kumar <skumar8j@in.ibm.com>
2015-09-28 06:22:26 -0500

    Level 2 Procedure -p9_sbe_tp_chiplet_init2
    Change-Id: I3b4227456c9ad8a00cb2259f3a24b4c5ef2b9e80
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/20797
    Tested-by: Jenkins Server
    Reviewed-by: Soma Bhanutej <soma.bhanu@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21848
    Reviewed-by: Amit J. Tendolkar <amit.tendolkar@in.ibm.com>

--- import/chips/p9/procedures/hwp/perv/p9_sbe_tp_chiplet_init2.C
+++ import/chips/p9/procedures/hwp/perv/p9_sbe_tp_chiplet_init2.C
@@ -19,28 +19,56 @@
 //------------------------------------------------------------------------------
 /// @file  p9_sbe_tp_chiplet_init2.C
 ///
-/// @brief IPL STEP 2.10 : SBE TP Chiplet Init2 :: Run scan 0 module for pervasive
-// *!
-// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
-// *! BACKUP NAME :                   Email:
+/// @brief Run scan 0 module for pervasive
 //------------------------------------------------------------------------------
-// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
-// *HWP FW Owner    : Brian Silver <bsilver@us.ibm.com>
-// *HWP Team        : Perv
-// *HWP Level       : 1
-// *HWP Consumed by : SBE
+// *HWP HWP Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP HWP Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner         : sunil kumar <skumar8j@in.ibm.com>
+// *HWP Team             : Perv
+// *HWP Level            : 2
+// *HWP Consumed by      : SBE
 //------------------------------------------------------------------------------
 
 
 //## auto_generated
 #include "p9_sbe_tp_chiplet_init2.H"
+
+#include "p9_perv_sbe_cmn.H"
+
+
+enum P9_SBE_TP_CHIPLET_INIT2_Private_Constants
+{
+    REGIONS_EXCEPT_PIB_NET_PLL = 0x4FE,
+    SCAN_TYPES = 0xDCF
+};
+
 fapi2::ReturnCode p9_sbe_tp_chiplet_init2(const
         fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
 {
-    FAPI_DBG("p9_sbe_tp_chiplet_init2: Entering ...");
+    auto l_perv_functional_vector =
+        i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+        (fapi2::TARGET_STATE_FUNCTIONAL);
+    FAPI_DBG("Entering ...");
 
-    FAPI_DBG("p9_sbe_tp_chiplet_init2: Exiting ...");
+    FAPI_INF("Call Scan0 Module");
 
-    return fapi2::FAPI2_RC_SUCCESS;
+    // Get the TPChiplet target
+    for (auto it : l_perv_functional_vector)
+    {
+        uint8_t l_attr_chip_unit_pos = 0; //actual value is read in FAPI_ATTR_GET below
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, it, l_attr_chip_unit_pos));
+
+        if (l_attr_chip_unit_pos == 0x01)/* TPChiplet */
+        {
+            FAPI_TRY(p9_perv_sbe_cmn_scan0_module(it, REGIONS_EXCEPT_PIB_NET_PLL,
+                                                  SCAN_TYPES));
+            break;
+        }
+    }
+
+    FAPI_DBG("Exiting ...");
+
+fapi_try_exit:
+    return fapi2::current_err;
 
 }
