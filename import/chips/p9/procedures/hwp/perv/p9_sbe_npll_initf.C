8f17e1273af552c40979308df763aacd96a1d67a
Sunil.Kumar <skumar8j@in.ibm.com>
2015-09-28 06:40:15 -0500

    Level 2 Procedure - p9_sbe_npll_initf
    Change-Id: Iaca12394baabd96c03ba8f04958c4e088e7380c1
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/20799
    Tested-by: Jenkins Server
    Reviewed-by: Soma Bhanutej <soma.bhanu@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21847
    Reviewed-by: Amit J. Tendolkar <amit.tendolkar@in.ibm.com>

--- import/chips/p9/procedures/hwp/perv/p9_sbe_npll_initf.C
+++ import/chips/p9/procedures/hwp/perv/p9_sbe_npll_initf.C
@@ -20,27 +20,60 @@
 /// @file  p9_sbe_npll_initf.C
 ///
 /// @brief apply initfile for level 0 & 1 PLLs
-// *!
-// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
-// *! BACKUP NAME :                   Email:
 //------------------------------------------------------------------------------
-// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
-// *HWP FW Owner    : Brian Silver <bsilver@us.ibm.com>
-// *HWP Team        : Perv
-// *HWP Level       : 1
-// *HWP Consumed by : SBE
+// *HWP HW Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner        : sunil kumar <skumar8j@in.ibm.com>
+// *HWP Team            : Perv
+// *HWP Level           : 2
+// *HWP Consumed by     : SBE
 //------------------------------------------------------------------------------
 
 
 //## auto_generated
 #include "p9_sbe_npll_initf.H"
+
+#include "p9_perv_sbe_cmn.H"
+
+
+enum P9_SBE_NPLL_INITF_Private_Constants
+{
+    REGIONS_PLL_ONLY = 0x001,
+    SCAN_TYPES_BNDY_FUNC = 0x808,
+    SCAN_TYPES_GPTR = 0x200
+};
+
 fapi2::ReturnCode p9_sbe_npll_initf(const
                                     fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
 {
-    FAPI_DBG("p9_sbe_npll_initf: Entering ...");
+    auto l_perv_functional_vector =
+        i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+        (fapi2::TARGET_STATE_FUNCTIONAL);
+    FAPI_DBG("Entering ...");
 
-    FAPI_DBG("p9_sbe_npll_initf: Exiting ...");
+    // Get the TPChiplet target
+    for (auto it : l_perv_functional_vector)
+    {
+        uint8_t l_attr_chip_unit_pos = 0; //actual value is read in FAPI_ATTR_GET below
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, it, l_attr_chip_unit_pos));
 
-    return fapi2::FAPI2_RC_SUCCESS;
+        if ((l_attr_chip_unit_pos == 0x01))/* TPChiplet */
+        {
+            FAPI_INF("Call Scan0 Module (scan region=PLL, scan types=GPTR)");
+            FAPI_TRY(p9_perv_sbe_cmn_scan0_module(it, REGIONS_PLL_ONLY,
+                                                  SCAN_TYPES_GPTR));
+            FAPI_INF("Call Scan0 Module (scan region=PLL, scan types=GPTR)");
+            FAPI_TRY(p9_perv_sbe_cmn_scan0_module(it, REGIONS_PLL_ONLY,
+                                                  SCAN_TYPES_BNDY_FUNC));
+            break;
+        }
+    }
+
+    //TODO:Load Ring Module : Scan initialize PLL BNDY chain
+
+    FAPI_DBG("Exiting ...");
+
+fapi_try_exit:
+    return fapi2::current_err;
 
 }
