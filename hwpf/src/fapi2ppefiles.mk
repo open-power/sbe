6caa946dbd6302337b1874150b502c087a1fe381
Greg Still <stillgs@us.ibm.com>
2015-10-13 20:54:36 -0500

    Rearrange PPE FAPI2 directory structure to be more in-line with HWPF
    - Move files into new structure
    - Delete old, obsolete files and directories
    - Pre-import mirror of HWPF into importtemp
    - No additional function
    
    Change-Id: Iadad6eb05349f807acea362ec77d7f58273878b5
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21150
    Tested-by: Jenkins Server
    Reviewed-by: Santosh S. Puranik <santosh.puranik@in.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: Martin Peschke <mpeschke@de.ibm.com>

---
+++ hwpf/src/fapi2ppefiles.mk
@@ -0,0 +1,27 @@
+#  @file fapi2ppefiles.mk
+#
+#  @brief mk for including fapi2 object files
+#
+#  @page ChangeLogs Change Logs
+#  @section fapi2ppefiles.mk
+#  @verbatim
+#
+#
+# Change Log ******************************************************************
+# Flag     Defect/Feature  User        Date         Description
+# ------   --------------  ----------  ------------ -----------
+#
+# @endverbatim
+#
+##########################################################################
+# Object Files
+##########################################################################
+
+
+FAPI2-CPP-SOURCES +=  fapi2PlatAttributeService.C
+
+FAPI2-S-SOURCES =
+
+FAPI2LIB_OBJECTS += $(FAPI2-CPP-SOURCES:.C=.o) 
+FAPI2LIB_OBJECTS += $(FAPI2-S-SOURCES:.S=.o)
+
