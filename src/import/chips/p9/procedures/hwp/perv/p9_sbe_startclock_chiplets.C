9fb5747cd661aa08b0b38e995cc387bfa3183c86
Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
2015-12-10 07:11:29 +0100

    Level 1 HWP for p9_sbe_startclock_chiplets
    Change-Id: Iafbe97740c212fdd6fb9acddf67c1442ae43e298
    Original-Change-Id: Ibb429cae94e3a3efa0936f18b1810f1d735cd0b9
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/22599
    Tested-by: Jenkins Server
    Reviewed-by: Soma Bhanutej <soma.bhanu@in.ibm.com>
    Reviewed-by: SRINIVAS V. POLISETTY <srinivan@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Parvathi Rachakonda

--- src/import/chips/p9/procedures/hwp/perv/p9_sbe_startclock_chiplets.C
+++ src/import/chips/p9/procedures/hwp/perv/p9_sbe_startclock_chiplets.C
@@ -27,12 +27,12 @@
 ///
 /// @brief Start clock procedure for XBUS, OBUS, PCIe
 //------------------------------------------------------------------------------
-// *HWP HWP Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
-// *HWP HWP Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
-// *HWP FW Owner         : sunil kumar <skumar8j@in.ibm.com>
-// *HWP Team             : Perv
-// *HWP Level            : 1
-// *HWP Consumed by      : SBE
+// *HWP HW Owner        : Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner        : sunil kumar <skumar8j@in.ibm.com>
+// *HWP Team            : Perv
+// *HWP Level           : 1
+// *HWP Consumed by     : SBE
 //------------------------------------------------------------------------------
 
 
@@ -42,7 +42,7 @@
 
 
 fapi2::ReturnCode p9_sbe_startclock_chiplets(const
-        fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplets)
+        fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chiplets)
 {
     FAPI_DBG("Entering ...");
 
