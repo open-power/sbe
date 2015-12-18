56637476e6e9f719fb7fcbc261d518609157d871
Sachin Gupta <sgupta2m@in.ibm.com>
2015-12-18 01:41:08 -0600

    Enable CI Part 1
    Change-Id: I1d9d691e747595bd70e183444e965d3eff351c24
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/22885
    Tested-by: Jenkins Server
    Reviewed-by: Basabjit Sengupta <basengup@in.ibm.com>
    Reviewed-by: Amit J. Tendolkar <amit.tendolkar@in.ibm.com>

--- sbe/test/testIstepAuto.py
+++ sbe/test/testIstepAuto.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("targets/p9_nimbus/sbeTest" )
 import testUtil
+from sim_commands import *
 err = False
 
 EXPDATA = [0xc0,0xde,0xa1,0x01,
@@ -10,18 +11,23 @@ EXPDATA = [0xc0,0xde,0xa1,0x01,
 # MAIN Test Run Starts Here...
 #-------------------------------------------------
 def sbe_istep_func( major, minor ):
-    TESTDATA = [0,0,0,3,
-                0,0,0xA1,0x01,
-                0,major,0,minor ]
-    testUtil.runCycles( 10000000 )
-    testUtil.writeUsFifo( TESTDATA )
-    testUtil.writeEot( )
-    testUtil.readDsFifo( EXPDATA )
-    testUtil.readEot( )
-    if err:
-        print ("\nTest completed with error(s)")
-        #sys.exit(1)
-    else:
-        print ("\nTest completed with no errors")
+    try:
+        TESTDATA = [0,0,0,3,
+                     0,0,0xA1,0x01,
+                    0,major,0,minor ]
+        testUtil.runCycles( 10000000 )
+        testUtil.writeUsFifo( TESTDATA )
+        testUtil.writeEot( )
+        testUtil.readDsFifo( EXPDATA )
+        testUtil.readEot( )
+    except:
+        print ("\nTest completed with error(s). Raise error")
+        # TODO via RTC 142706
+        # Currently simics commands created using hooks always return
+        # success. Need to check from simics command a way to return
+        # Calling non existant command to return failure
+        run_command("Command Failed");
+        raise
+    print ("\nTest completed with no errors")
         #sys.exit(0);
 
