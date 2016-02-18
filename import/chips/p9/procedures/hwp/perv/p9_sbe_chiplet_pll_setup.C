7aeb0b87f28b30214ba1e9d6ddd3e4b533bc0d87
Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
2016-02-18 06:52:19 +0100

    Level 2 HWP for p9_sbe_chiplet_pll_setup
    According to ver111 IPL spread sheet
    
    Change-Id: Ibe0aafae4a77a135b7222dba5c2af45aa627fd48
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/24423
    Tested-by: Jenkins Server
    Reviewed-by: Soma Bhanutej <soma.bhanu@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Parvathi Rachakonda
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/24609
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- import/chips/p9/procedures/hwp/perv/p9_sbe_chiplet_pll_setup.C
+++ import/chips/p9/procedures/hwp/perv/p9_sbe_chiplet_pll_setup.C
@@ -7,7 +7,7 @@
 /*                                                                        */
 /* EKB Project                                                            */
 /*                                                                        */
-/* COPYRIGHT 2015                                                         */
+/* COPYRIGHT 2015,2016                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -33,26 +33,38 @@
 //## auto_generated
 #include "p9_sbe_chiplet_pll_setup.H"
 
-#include "p9_perv_scom_addresses.H"
+#include <p9_perv_scom_addresses.H>
+#include <p9_perv_scom_addresses_fld.H>
+#include <p9_sbe_common.H>
 
 
 enum P9_SBE_CHIPLET_PLL_SETUP_Private_Constants
 {
     NS_DELAY = 100000, // unit is nano seconds
-    //SIM_CYCLE_DELAY = 25000000 // unit is sim cycles
-    SIM_CYCLE_DELAY = 100000 // unit is sim cycles
+    SIM_CYCLE_DELAY = 100000, // unit is sim cycles
+    CLOCK_CMD = 0x1,
+    CLOCK_TYPES = 0x2,
+    DONT_STARTMASTER = 0x0,
+    DONT_STARTSLAVE = 0x0,
+    REGIONS = 0x001
 };
 
 static fapi2::ReturnCode p9_sbe_chiplet_pll_setup_function(
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet);
+
+static fapi2::ReturnCode p9_sbe_chiplet_pll_setup_mc_drop_bypass(
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet);
+
+static fapi2::ReturnCode p9_sbe_chiplet_pll_setup_pll_test_enable(
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet);
+
+static fapi2::ReturnCode p9_sbe_chiplet_pll_setup_strt_pci_nsl_drp_synclk_mux(
     const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet);
 
 fapi2::ReturnCode p9_sbe_chiplet_pll_setup(const
         fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
 {
     uint8_t l_read_attr = 0;
-    auto l_perv_functional_vector =
-        i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
-        (fapi2::TARGET_STATE_FUNCTIONAL);
     FAPI_DBG("Entering ...");
 
     FAPI_INF("Switch MC meshs to Nest mesh");
@@ -60,47 +72,59 @@ fapi2::ReturnCode p9_sbe_chiplet_pll_setup(const
 
     if ( l_read_attr )
     {
-        for (auto l_chplt_trgt : l_perv_functional_vector)
+        for (auto l_chplt_trgt : i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+             (fapi2::TARGET_FILTER_ALL_MC, fapi2::TARGET_STATE_FUNCTIONAL))
         {
-            uint8_t l_attr_chip_unit_pos = 0; //actual value is read in FAPI_ATTR_GET below
-            FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_chplt_trgt,
-                                   l_attr_chip_unit_pos));
+            FAPI_INF("Drop PDLY, DCC bypass");
+            FAPI_TRY(p9_sbe_chiplet_pll_setup_mc_drop_bypass(l_chplt_trgt));
+        }
 
-            if (!((l_attr_chip_unit_pos == 0x09 || l_attr_chip_unit_pos == 0x0A
-                   || l_attr_chip_unit_pos == 0x0B
-                   || l_attr_chip_unit_pos == 0x0C/* ObusChiplet */) ||
-                  (l_attr_chip_unit_pos == 0x0D || l_attr_chip_unit_pos == 0x0E
-                   || l_attr_chip_unit_pos == 0x0F/* PcieChiplet */) ||
-                  (l_attr_chip_unit_pos == 0x06/* XbusChiplet */)))
-            {
-                continue;
-            }
+        for (auto l_chplt_trgt : i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+             (fapi2::TARGET_FILTER_ALL_PCI, fapi2::TARGET_STATE_FUNCTIONAL))
+        {
+            FAPI_INF("call clock start stop module and drop syncclk muxsel");
+            FAPI_TRY(p9_sbe_chiplet_pll_setup_strt_pci_nsl_drp_synclk_mux(l_chplt_trgt));
+        }
 
-            FAPI_INF("p9_sbe_chiplet_pll_setup_function");
+        for (auto l_chplt_trgt : i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+             (static_cast<fapi2::TargetFilter>(fapi2::TARGET_FILTER_ALL_OBUS |
+                                               fapi2::TARGET_FILTER_XBUS), fapi2::TARGET_STATE_FUNCTIONAL))
+        {
+            FAPI_INF("release pll test enable for except pcie");
+            FAPI_TRY(p9_sbe_chiplet_pll_setup_pll_test_enable(l_chplt_trgt));
+        }
+
+        for (auto l_chplt_trgt : i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+             (static_cast<fapi2::TargetFilter>(fapi2::TARGET_FILTER_ALL_OBUS |
+                                               fapi2::TARGET_FILTER_ALL_PCI | fapi2::TARGET_FILTER_XBUS),
+              fapi2::TARGET_STATE_FUNCTIONAL))
+        {
             FAPI_TRY(p9_sbe_chiplet_pll_setup_function(l_chplt_trgt));
         }
     }
     else
     {
-        for (auto l_chplt_trgt : l_perv_functional_vector)
+        for (auto l_chplt_trgt : i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+             (fapi2::TARGET_FILTER_ALL_PCI, fapi2::TARGET_STATE_FUNCTIONAL))
         {
-            uint8_t l_attr_chip_unit_pos = 0; //actual value is read in FAPI_ATTR_GET below
-            FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_chplt_trgt,
-                                   l_attr_chip_unit_pos));
+            FAPI_INF("call clock start stop module and drop syncclk_muxsel");
+            FAPI_TRY(p9_sbe_chiplet_pll_setup_strt_pci_nsl_drp_synclk_mux(l_chplt_trgt));
+        }
 
-            if (!((l_attr_chip_unit_pos == 0x07
-                   || l_attr_chip_unit_pos == 0x08/* McChiplet */) ||
-                  (l_attr_chip_unit_pos == 0x09 || l_attr_chip_unit_pos == 0x0A
-                   || l_attr_chip_unit_pos == 0x0B
-                   || l_attr_chip_unit_pos == 0x0C/* ObusChiplet */) ||
-                  (l_attr_chip_unit_pos == 0x0D || l_attr_chip_unit_pos == 0x0E
-                   || l_attr_chip_unit_pos == 0x0F/* PcieChiplet */) ||
-                  (l_attr_chip_unit_pos == 0x06/* XbusChiplet */)))
-            {
-                continue;
-            }
+        for (auto l_chplt_trgt : i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+             (static_cast<fapi2::TargetFilter>(fapi2::TARGET_FILTER_ALL_MC |
+                                               fapi2::TARGET_FILTER_ALL_OBUS | fapi2::TARGET_FILTER_XBUS),
+              fapi2::TARGET_STATE_FUNCTIONAL))
+        {
+            FAPI_INF("release pll test enable for except pcie");
+            FAPI_TRY(p9_sbe_chiplet_pll_setup_pll_test_enable(l_chplt_trgt));
+        }
 
-            FAPI_INF("p9_sbe_chiplet_pll_setup_function");
+        for (auto l_chplt_trgt : i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+             (static_cast<fapi2::TargetFilter>(fapi2::TARGET_FILTER_ALL_MC |
+                                               fapi2::TARGET_FILTER_ALL_OBUS | fapi2::TARGET_FILTER_ALL_PCI |
+                                               fapi2::TARGET_FILTER_XBUS), fapi2::TARGET_STATE_FUNCTIONAL))
+        {
             FAPI_TRY(p9_sbe_chiplet_pll_setup_function(l_chplt_trgt));
         }
     }
@@ -121,32 +145,12 @@ static fapi2::ReturnCode p9_sbe_chiplet_pll_setup_function(
 {
     fapi2::buffer<uint64_t> l_read_reg;
     fapi2::buffer<uint64_t> l_data64;
-    uint8_t l_attr_chip_unit_pos = 0; //actual value is read in FAPI_ATTR_GET
-
     FAPI_DBG("Entering ...");
-
-    //-- Manual Update
-    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, i_target_chiplet,
-                           l_attr_chip_unit_pos));
-
-    if (l_attr_chip_unit_pos == 0x0D || l_attr_chip_unit_pos == 0x0E
-        || l_attr_chip_unit_pos == 0x0F/* PcieChiplet */)
-    {
-        FAPI_INF("Not Dropping PLL Test Enable for PCI PLL");
-    }
-    else
-    {
-        FAPI_INF("Drop PLL Test Enable");
-        //Setting NET_CTRL0 register value
-        l_data64.flush<1>();
-        l_data64.clearBit<3>();  //NET_CTRL0.PLL_TEST_EN = 0
-        FAPI_TRY(fapi2::putScom(i_target_chiplet, PERV_NET_CTRL0_WAND, l_data64));
-    }
 
     FAPI_INF("Drop PLL Reset");
     //Setting NET_CTRL0 register value
     l_data64.flush<1>();
-    l_data64.clearBit<4>();  //NET_CTRL0.PLL_RESET = 0
+    l_data64.clearBit<PERV_1_NET_CTRL0_PLL_RESET>();  //NET_CTRL0.PLL_RESET = 0
     FAPI_TRY(fapi2::putScom(i_target_chiplet, PERV_NET_CTRL0_WAND, l_data64));
 
     fapi2::delay(NS_DELAY, SIM_CYCLE_DELAY);
@@ -155,17 +159,16 @@ static fapi2::ReturnCode p9_sbe_chiplet_pll_setup_function(
     //Getting PLL_LOCK_REG register value
     FAPI_TRY(fapi2::getScom(i_target_chiplet, PERV_PLL_LOCK_REG,
                             l_read_reg)); //l_read_reg = PLL_LOCK_REG
-    
+
     FAPI_ASSERT(l_read_reg.getBit<0>(),
                 fapi2::PLL_LOCK_ERR()
                 .set_PLL_READ(l_read_reg),
                 "ERROR:PLL LOCK NOT SET");
-    
 
     FAPI_INF("Drop PLL Bypass");
     //Setting NET_CTRL0 register value
     l_data64.flush<1>();
-    l_data64.clearBit<5>();  //NET_CTRL0.PLL_BYPASS = 0
+    l_data64.clearBit<PERV_1_NET_CTRL0_PLL_BYPASS>();  //NET_CTRL0.PLL_BYPASS = 0
     FAPI_TRY(fapi2::putScom(i_target_chiplet, PERV_NET_CTRL0_WAND, l_data64));
 
     FAPI_INF("Reset PCB Slave error register");
@@ -180,3 +183,85 @@ fapi_try_exit:
 
 }
 
+/// @brief Drop PDLY bypass, DCC bypass
+///
+/// @param[in]     i_target_chiplet   Reference to TARGET_TYPE_PERV target
+/// @return  FAPI2_RC_SUCCESS if success, else error code.
+static fapi2::ReturnCode p9_sbe_chiplet_pll_setup_mc_drop_bypass(
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet)
+{
+    fapi2::buffer<uint64_t> l_data64;
+    FAPI_DBG("Entering ...");
+
+    FAPI_INF("Drop PDLY bypass");
+    //Setting NET_CTRL1 register value
+    l_data64.flush<1>();
+    //NET_CTRL1.CLK_PDLY_BYPASS_EN = 0
+    l_data64.clearBit<PERV_1_NET_CTRL1_CLK_PDLY_BYPASS_EN>();
+    FAPI_TRY(fapi2::putScom(i_target_chiplet, PERV_NET_CTRL1_WAND, l_data64));
+
+    FAPI_INF("Drop DCC bypass");
+    //Setting NET_CTRL1 register value
+    l_data64.flush<1>();
+    //NET_CTRL1.CLK_DCC_BYPASS_EN = 0
+    l_data64.clearBit<PERV_1_NET_CTRL1_CLK_DCC_BYPASS_EN>();
+    FAPI_TRY(fapi2::putScom(i_target_chiplet, PERV_NET_CTRL1_WAND, l_data64));
+
+    FAPI_DBG("Exiting ...");
+
+fapi_try_exit:
+    return fapi2::current_err;
+
+}
+
+/// @brief Release pll test enable except for pcie
+///
+/// @param[in]     i_target_chiplet   Reference to TARGET_TYPE_PERV target
+/// @return  FAPI2_RC_SUCCESS if success, else error code.
+static fapi2::ReturnCode p9_sbe_chiplet_pll_setup_pll_test_enable(
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet)
+{
+    fapi2::buffer<uint64_t> l_data64;
+    FAPI_DBG("Entering ...");
+
+    FAPI_INF("Release PLL test enable for except pcie");
+    //Setting NET_CTRL0 register value
+    l_data64.flush<1>();
+    l_data64.clearBit<PERV_1_NET_CTRL0_PLL_TEST_EN>();  //NET_CTRL0.PLL_TEST_EN = 0
+    FAPI_TRY(fapi2::putScom(i_target_chiplet, PERV_NET_CTRL0_WAND, l_data64));
+
+    FAPI_DBG("Exiting ...");
+
+fapi_try_exit:
+    return fapi2::current_err;
+
+}
+
+/// @brief start PLL clock region, NSL latches only , call module clock_start_stop
+/// Drop syncclk_muxsel
+///
+/// @param[in]     i_target_chiplet   Reference to TARGET_TYPE_PERV target
+/// @return  FAPI2_RC_SUCCESS if success, else error code.
+static fapi2::ReturnCode p9_sbe_chiplet_pll_setup_strt_pci_nsl_drp_synclk_mux(
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet)
+{
+    fapi2::buffer<uint64_t> l_data64;
+    FAPI_DBG("Entering ...");
+
+    FAPI_INF("call module clock start stop");
+    FAPI_TRY(p9_sbe_common_clock_start_stop(i_target_chiplet, CLOCK_CMD,
+                                            DONT_STARTSLAVE, DONT_STARTMASTER, REGIONS, CLOCK_TYPES));
+
+    FAPI_INF("Drop syncclk muxsel for pcie chiplet");
+    //Setting CPLT_CTRL0 register value
+    l_data64.flush<0>();
+    //CPLT_CTRL0.TC_UNIT_SYNCCLK_MUXSEL_DC = 0
+    l_data64.setBit<PERV_1_CPLT_CTRL0_TC_UNIT_SYNCCLK_MUXSEL_DC>();
+    FAPI_TRY(fapi2::putScom(i_target_chiplet, PERV_CPLT_CTRL0_CLEAR, l_data64));
+
+    FAPI_DBG("Exiting ...");
+
+fapi_try_exit:
+    return fapi2::current_err;
+
+}
