534db91fb2b79d46478459447e36dfde68dc5c7a
Yue Du <daviddu@us.ibm.com>
2016-02-29 22:39:41 -0600

    HWP-CACHE/CORE:istep4 procedures updates
    Change-Id: I8c1e08f5db655db54a91bb0ab4dab91b83f2eaef
    Original-Change-Id: I707a936f8124f997c338ce01db205b958716a8da
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/21489
    Tested-by: Jenkins Server
    Tested-by: Hostboot CI
    Tested-by: PPE CI
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/25113
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

---
+++ import/chips/p9/procedures/hwp/cache/p9_hcd_cache_dpll_initf.C
@@ -0,0 +1,72 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: chips/p9/procedures/hwp/cache/p9_hcd_cache_dpll_initf.C $     */
+/*                                                                        */
+/* IBM CONFIDENTIAL                                                       */
+/*                                                                        */
+/* EKB Project                                                            */
+/*                                                                        */
+/* COPYRIGHT 2015,2016                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* The source code for this program is not published or otherwise         */
+/* divested of its trade secrets, irrespective of what has been           */
+/* deposited with the U.S. Copyright Office.                              */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file  p9_hcd_cache_dpll_initf.C
+/// @brief Load DPLL ring for EX non-core
+///
+/// Procedure Summary:
+///   Load cache ring images from MVPD
+///   These rings must contain ALL chip customization data.
+///   This includes the following:  DPLL Power headers, and DTS
+///   Historically this was stored in MVPD keywords are #R, #G. Still stored in
+///     MVPD, but SBE image is customized with rings for booting cores
+
+// *HWP HWP Owner          : David Du       <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still     <stillgs@us.ibm.com>
+// *HWP FW Owner           : Sangeetha T S  <sangeet2@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : SBE:SGPE
+// *HWP Level              : 2
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include "p9_hcd_cache_dpll_initf.H"
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Procedure: Load DPLL ring for cache
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p9_hcd_cache_dpll_initf(
+    const fapi2::Target<fapi2::TARGET_TYPE_EQ>& i_target)
+{
+    FAPI_INF(">>p9_hcd_cache_dpll_initf");
+
+#ifndef P9_HCD_STOP_SKIP_SCAN
+
+    FAPI_DBG("Scanning Cache DPLL FUNC Rings");
+    FAPI_TRY(fapi2::putRing(i_target, EQ_DPLL_FUNC,
+                            fapi2::RING_MODE_HEADER_CHECK));
+
+fapi_try_exit:
+
+#endif
+
+    FAPI_INF("<<p9_hcd_cache_dpll_initf");
+    return fapi2::current_err;
+}
+
+
+
