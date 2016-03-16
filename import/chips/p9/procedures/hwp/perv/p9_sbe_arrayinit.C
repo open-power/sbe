ebde9074189751b4f98391a1db6f41691bdaa47f
Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
2016-03-16 13:35:33 +0100

    Level 2 HWPs for new IPL changes
    Change-Id: I0b9b8e80f34de7bd962b686f91ebc504cda1ff34
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22103
    Tested-by: Jenkins Server
    Tested-by: PPE CI
    Reviewed-by: Soma Bhanutej <soma.bhanu@in.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: PARVATHI RACHAKONDA <prachako@in.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22273
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- import/chips/p9/procedures/hwp/perv/p9_sbe_arrayinit.C
+++ import/chips/p9/procedures/hwp/perv/p9_sbe_arrayinit.C
@@ -7,7 +7,7 @@
 /*                                                                        */
 /* EKB Project                                                            */
 /*                                                                        */
-/* COPYRIGHT 2015                                                         */
+/* COPYRIGHT 2015,2016                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -33,58 +33,50 @@
 //## auto_generated
 #include "p9_sbe_arrayinit.H"
 
-#include "p9_perv_scom_addresses.H"
-#include "p9_perv_sbe_cmn.H"
+#include <p9_perv_scom_addresses.H>
+#include <p9_perv_scom_addresses_fld.H>
+#include <p9_perv_sbe_cmn.H>
 
 
 enum P9_SBE_ARRAYINIT_Private_Constants
 {
     LOOP_COUNTER = 0x0000000000042FFF,
-    REGIONS_EXCEPT_VITAL = 0x7FF,
-    REGIONS_FOR_PERV = 0x400,
+    REGIONS_EXCEPT_VITAL_AND_PLL = 0x7FE,
     SCAN_TYPES_EXCEPT_TIME_GPTR_REPR = 0xDCF,
     SELECT_EDRAM = 0x0,
     SELECT_SRAM = 0x1,
-    START_ABIST_MATCH_VALUE = 0x0000000F00000000
+    START_ABIST_MATCH_VALUE = 0x0000000F00000000,
+    SCAN0_LOOP_COUNT = 1
 };
 
 static fapi2::ReturnCode p9_sbe_arrayinit_scan0_and_arrayinit_module_function(
-    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet);
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet,
+    const fapi2::buffer<uint16_t> i_regions);
 
 fapi2::ReturnCode p9_sbe_arrayinit(const
                                    fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
 {
-    auto l_perv_functional_vector =
-        i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
-        (fapi2::TARGET_STATE_FUNCTIONAL);
-    FAPI_DBG("Entering ...");
+    fapi2::buffer<uint16_t> l_regions;
+    fapi2::buffer<uint32_t> l_attr_pg;
+    FAPI_INF("Entering ...");
 
-    for (auto l_chplt_trgt : l_perv_functional_vector)
+    for (auto l_chplt_trgt : i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>
+         (static_cast<fapi2::TargetFilter>(fapi2::TARGET_FILTER_ALL_MC |
+                                           fapi2::TARGET_FILTER_ALL_NEST | fapi2::TARGET_FILTER_ALL_OBUS |
+                                           fapi2::TARGET_FILTER_ALL_PCI | fapi2::TARGET_FILTER_XBUS),
+          fapi2::TARGET_STATE_FUNCTIONAL))
     {
-        uint8_t l_attr_chip_unit_pos = 0; //actual value is read in FAPI_ATTR_GET below
-        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_chplt_trgt,
-                               l_attr_chip_unit_pos));
+        FAPI_DBG("Region setup");
+        FAPI_TRY(p9_perv_sbe_cmn_regions_setup_16(l_chplt_trgt,
+                 REGIONS_EXCEPT_VITAL_AND_PLL, l_regions));
+        FAPI_DBG("l_regions value: %#018lX ", l_regions);
 
-        if (!((l_attr_chip_unit_pos == 0x07
-               || l_attr_chip_unit_pos == 0x08/* McChiplet */) ||
-              (l_attr_chip_unit_pos == 0x02 || l_attr_chip_unit_pos == 0x03
-               || l_attr_chip_unit_pos == 0x04
-               || l_attr_chip_unit_pos == 0x05/* NestChiplet */) ||
-              (l_attr_chip_unit_pos == 0x09 || l_attr_chip_unit_pos == 0x0A
-               || l_attr_chip_unit_pos == 0x0B
-               || l_attr_chip_unit_pos == 0x0C/* ObusChiplet */) ||
-              (l_attr_chip_unit_pos == 0x0D || l_attr_chip_unit_pos == 0x0E
-               || l_attr_chip_unit_pos == 0x0F/* PcieChiplet */) ||
-              (l_attr_chip_unit_pos == 0x06/* XbusChiplet */)))
-        {
-            continue;
-        }
-
-        FAPI_INF("Call proc_sbe_arryinit_scan0_and_arrayinit_module_function");
-        FAPI_TRY(p9_sbe_arrayinit_scan0_and_arrayinit_module_function(l_chplt_trgt));
+        FAPI_DBG("Call proc_sbe_arryinit_scan0_and_arrayinit_module_function");
+        FAPI_TRY(p9_sbe_arrayinit_scan0_and_arrayinit_module_function(l_chplt_trgt,
+                 l_regions));
     }
 
-    FAPI_DBG("Exiting ...");
+    FAPI_INF("Exiting ...");
 
 fapi_try_exit:
     return fapi2::current_err;
@@ -96,43 +88,52 @@ fapi_try_exit:
 ///
 ///
 /// @param[in]     i_target_chiplet   Reference to TARGET_TYPE_PERV target
+/// @param[in]     i_regions          region value settings
 /// @return  FAPI2_RC_SUCCESS if success, else error code.
 static fapi2::ReturnCode p9_sbe_arrayinit_scan0_and_arrayinit_module_function(
-    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet)
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet,
+    const fapi2::buffer<uint16_t> i_regions)
 {
     bool l_read_reg = false;
+    int l_timeout = SCAN0_LOOP_COUNT;
     fapi2::buffer<uint64_t> l_data64;
-    FAPI_DBG("Entering ...");
+    FAPI_INF("Entering ...");
 
-    FAPI_INF("Check for chiplet enable");
+    FAPI_DBG("Check for chiplet enable");
     //Getting NET_CTRL0 register value
     FAPI_TRY(fapi2::getScom(i_target_chiplet, PERV_NET_CTRL0, l_data64));
-    l_read_reg = l_data64.getBit<0>();  //l_read_reg = NET_CTRL0.CHIPLET_ENABLE
+    //l_read_reg = NET_CTRL0.CHIPLET_ENABLE
+    l_read_reg = l_data64.getBit<PERV_1_NET_CTRL0_CHIPLET_ENABLE>();
 
     if ( l_read_reg )
     {
-        FAPI_INF("run array_init module for all chiplet except TP, EC, EP");
-        FAPI_TRY(p9_perv_sbe_cmn_array_init_module(i_target_chiplet,
-                 REGIONS_EXCEPT_VITAL, LOOP_COUNTER, SELECT_SRAM, SELECT_EDRAM,
-                 START_ABIST_MATCH_VALUE));
+        FAPI_DBG("run array_init module for all chiplet except TP, EC, EP");
+        FAPI_TRY(p9_perv_sbe_cmn_array_init_module(i_target_chiplet, i_regions,
+                 LOOP_COUNTER, SELECT_SRAM, SELECT_EDRAM, START_ABIST_MATCH_VALUE));
 
-        FAPI_INF("Check  for SRAM ABIST done");
+        FAPI_DBG("Check  for SRAM ABIST done");
         //Getting CPLT_STAT0 register value
         FAPI_TRY(fapi2::getScom(i_target_chiplet, PERV_CPLT_STAT0, l_data64));
         //l_read_reg = CPLT_STAT0.SRAM_ABIST_DONE_DC
-        l_read_reg = l_data64.getBit<0>();
+        l_read_reg = l_data64.getBit<PERV_1_CPLT_STAT0_SRAM_ABIST_DONE_DC>();
 
         FAPI_ASSERT(l_read_reg,
                     fapi2::ABIST_DONE_ERR()
                     .set_READ_ABIST_DONE(l_read_reg),
                     "ERROR:ABIST DONE BIT NOT SET ");
 
-        FAPI_INF("run scan0 module for region except vital and scan types except GPTR TIME REPR all chiplets except TP, EC, EP");
-        FAPI_TRY(p9_perv_sbe_cmn_scan0_module(i_target_chiplet, REGIONS_EXCEPT_VITAL,
-                                              SCAN_TYPES_EXCEPT_TIME_GPTR_REPR));
+        while(l_timeout != 0)
+        {
+
+            FAPI_DBG("run scan0 module for region except vital and pll, scan types except GPTR TIME REPR all chiplets except TP, EC, EP");
+            FAPI_TRY(p9_perv_sbe_cmn_scan0_module(i_target_chiplet, i_regions,
+                                                  SCAN_TYPES_EXCEPT_TIME_GPTR_REPR));
+            FAPI_DBG("Loop Count :%d", l_timeout);
+            --l_timeout;
+        }
     }
 
-    FAPI_DBG("Exiting ...");
+    FAPI_INF("Exiting ...");
 
 fapi_try_exit:
     return fapi2::current_err;
