3cdef7c43e771c55ee9e6661ab2c79aa3a418797
Sunil.Kumar <skumar8j@in.ibm.com>
2016-04-05 23:25:52 -0500

    Level 1 Initf Procedure update for reconfig flow
    Change-Id: I16e16985615bd314ea760de1680bb4d8e6536247
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22930
    Tested-by: Jenkins Server
    Reviewed-by: SRINIVAS V. POLISETTY <srinivan@in.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: PARVATHI RACHAKONDA <prachako@in.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23091

---
+++ import/chips/p9/procedures/hwp/perv/p9_sbe_repr_initf.C
@@ -0,0 +1,44 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: chips/p9/procedures/hwp/perv/p9_sbe_repr_initf.C $            */
+/*                                                                        */
+/* IBM CONFIDENTIAL                                                       */
+/*                                                                        */
+/* EKB Project                                                            */
+/*                                                                        */
+/* COPYRIGHT 2016                                                         */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* The source code for this program is not published or otherwise         */
+/* divested of its trade secrets, irrespective of what has been           */
+/* deposited with the U.S. Copyright Office.                              */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @file  p9_sbe_repr_initf.C
+///
+/// @brief Load Repair rings for all enabled chiplets
+//------------------------------------------------------------------------------
+// *HWP HW Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner        : Sunil Kumar <skumar8j@in.ibm.com>
+// *HWP Team            : Perv
+// *HWP Level           : 1
+// *HWP Consumed by     : SBE
+//------------------------------------------------------------------------------
+
+#include "p9_sbe_repr_initf.H"
+
+fapi2::ReturnCode p9_sbe_repr_initf(const
+                                    fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
+{
+
+    FAPI_INF("Entering ...");
+
+    FAPI_INF("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
