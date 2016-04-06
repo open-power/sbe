441337e4711499af4fe00ca97b109d54c7d3bddb
Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
2016-04-06 07:58:49 +0200

    Level 1 HWP for p9_sbe_clock_test2
    Change-Id: I198cdd5995e37ad9bc230024f31a998ee06d073f
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22932
    Tested-by: Jenkins Server
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: PARVATHI RACHAKONDA <prachako@in.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23090

---
+++ import/chips/p9/procedures/hwp/perv/p9_sbe_clock_test2.C
@@ -0,0 +1,47 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: chips/p9/procedures/hwp/perv/p9_sbe_clock_test2.C $           */
+/*                                                                        */
+/* IBM CONFIDENTIAL                                                       */
+/*                                                                        */
+/* EKB Project                                                            */
+/*                                                                        */
+/* COPYRIGHT 2016                                                         */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* The source code for this program is not published or otherwise         */
+/* divested of its trade secrets, irrespective of what has been           */
+/* deposited with the U.S. Copyright Office.                              */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @file  p9_sbe_clock_test2.C
+///
+/// @brief sbe_clock_test2 for enabling osc-check
+//------------------------------------------------------------------------------
+// *HWP HW Owner        : Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner        : Sunil Kumar <skumarj8@in.ibm.com>
+// *HWP Team            : Perv
+// *HWP Level           : 1
+// *HWP Consumed by     : SBE
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_sbe_clock_test2.H"
+
+
+
+fapi2::ReturnCode p9_sbe_clock_test2(const
+                                     fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chiplet)
+{
+    FAPI_INF("Entering ...");
+
+    FAPI_INF("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
