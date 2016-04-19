c8b555b055f85a68e69a2db516be3e1a93aee712
Sangeetha T S <sangeet2@in.ibm.com>
2016-04-19 07:00:28 -0500

    Adding the Hooks and supporting files for commit and applypatch
    Change-Id: Id6ea19f7c5acd6345d403fd3575d660f14647cd2
    RTC:149657
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23406
    Tested-by: Jenkins Server
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- env.bash
+++ env.bash
@@ -1,3 +1,27 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: env.bash $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2015,2016
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
 ROOTDIR=.
 export SBEROOT=`pwd`
 
@@ -15,3 +39,27 @@ if [ -n "${SANDBOXROOT}" ]; then
 fi
 
 export DEFAULT_MACHINE=nimbus
+
+# The following ENV variables are required by the pre/post commit hooks
+export PROJECT_NAME=sbe
+export TOOLSDIR=$SBEROOT/tools/hookScripts
+export HOOKSDIR=$SBEROOT/.git/hooks
+export IMPORT_REL_PATH=import
+export IMPORT_DIR=$SBEROOT/$IMPORT_REL_PATH
+export LICENSE=$SBEROOT/LICENSE_PROLOG # Copyright license file for PPE
+export PATH=${PATH}:$TOOLSDIR # Update PATH
+
+# Fetch the gerrit server; Required by the pre/post commit hooks
+if [ -e $HOME/.ssh/config ]; then
+if [ -e $TOOLSDIR/gerrit-hostname ]; then
+    echo "Searching for Gerrit Host..."
+    eval $($TOOLSDIR/gerrit-hostname)
+fi
+fi
+
+# Copy the git hooks to .git/hooks folder
+if [ -e .git/hooks ]; then
+if [ -e $TOOLSDIR/setupgithooks.sh ]; then
+    $TOOLSDIR/setupgithooks.sh
+fi
+fi
---
+++ sbe/build/citest/copyright-check.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: sbe/build/citest/copyright-check.sh $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2016
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+#
+#   Front end to addCopyright.pl - script to check for copyright block during
+#   Gerrit checkin.
+#
+
+export WORKSPACE_DIR=`pwd`
+export ADDCOPYRIGHT=${WORKSPACE_DIR}/tools/hookScripts/addCopyright.pl
+
+##  run git show to get a list of checked in files
+CHECKINFILES=`git show --pretty=format: --name-only -n1 | tr '\n' ' '`
+##  use git log to determine the year of the commit.
+##     Since commits have their copyright updated at the time they are
+##     committed, a commit might have a copyright date in its prolog of
+##     last year.  Set the DATE_OVERRIDE variable to the 'validate' to allow
+##     slightly-old prologs (ie. ones corresponding to the date in the msg).
+export DATE_OVERRIDE=`git log -n1 --date=short | grep "Date" | sed "s/Date: *//" | sed "s/-.*//"`
+
+echo "========================================================================"
+
+echo "  Checking Copyright blocks for checked-in files:"
+echo "  $CHECKINFILES"
+echo
+$ADDCOPYRIGHT validate  $CHECKINFILES --copyright-check
+
+if [ $? -eq 0 ]; then
+    echo "Copyright Check passed OK, $?"
+    exit 0
+else
+    echo "ERROR: $?"
+    exit 1
+fi
