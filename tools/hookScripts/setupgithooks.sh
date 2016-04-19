92c052e92a53ac010bc045eff50dbcf9ff5018be
Sangeetha T S <sangeet2@in.ibm.com>
2016-04-19 07:00:28 -0500

    Adding the Hooks and supporting files for commit and applypatch
    Change-Id: Id6ea19f7c5acd6345d403fd3575d660f14647cd2
    RTC:149657
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23406
    Tested-by: Jenkins Server
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

---
+++ tools/hookScripts/setupgithooks.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: tools/hookScripts/setupgithooks.sh $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2016
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+# Each developer runs this from the git_repo base dir, where it will copy the
+# needed scripts into .git/hooks/ directory and make them executable.
+
+if [ -d $HOOKSDIR ]
+then
+
+    # Get hooks from Gerrit, if needed.
+    if [ ! -f $HOOKSDIR/commit-msg ]
+    then
+        echo "Copying Gerrit hooks..."
+        scp -p -q $GERRIT_SRV:hooks/commit-msg $HOOKSDIR
+    fi
+
+    # Copy custom pre/post commit hooks from tools directory.
+    if [ -f "$TOOLSDIR/pre-commit" -a \
+         -f "$TOOLSDIR/post-commit" ]
+    then
+        echo "Copying pre/post commit hooks..."
+
+        cp $TOOLSDIR/pre-commit  $HOOKSDIR/
+        cp $TOOLSDIR/pre-commit  $HOOKSDIR/pre-applypatch
+        cp $TOOLSDIR/post-commit $HOOKSDIR/
+
+        chmod u+x $HOOKSDIR/pre-commit
+        chmod u+x $HOOKSDIR/pre-applypatch
+        chmod u+x $HOOKSDIR/post-commit
+
+    else
+        echo "Cannot find or access pre or post commit scripts"
+        exit 1
+    fi
+
+else
+    echo "Cannot find or access .git/hooks directory"
+    exit 1
+fi
+
+exit 0
