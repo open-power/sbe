a488e446cdd8e75b39e5a84ddbdddcd2f1e2424b
Santosh Puranik <santosh.puranik@in.ibm.com>
2016-04-27 12:02:24 -0500

    Machine check interrupt handler
    Added an interrupt handler for PPE machine check to gracefully handle SCOM
    failures.
    
    Change-Id: I75711ea9817a95e678e365efec9c12243c5e8b46
    RTC: 148524
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23735
    Tested-by: Jenkins Server
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: AMIT J. TENDOLKAR <amit.tendolkar@in.ibm.com>

--- sbe/sbefw/pk_app_cfg.h
+++ sbe/sbefw/pk_app_cfg.h
@@ -1,3 +1,27 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: sbe/sbefw/pk_app_cfg.h $                                      */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2016                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
 /*
  * @file: ppe/sbe/sbefw/pk_app_cfg.h
  *
@@ -106,5 +130,10 @@
  *
  */
 #define PK_TRACE_TIMER_OUTPUT 0
+
+#ifdef PPE42_MACHINE_CHECK_HANDLER
+#undef PPE42_MACHINE_CHECK_HANDLER
+#endif
+#define PPE42_MACHINE_CHECK_HANDLER SBE_MACHINE_CHECK_HANDLER
 
 #endif /*__PK_APP_CFG_H__*/
