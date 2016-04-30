6f4c4d80d2c52ac569fb69408b30f396be647327
Joe McGill <jmcgill@us.ibm.com>
2016-04-30 15:08:36 -0500

    add core/cache initfiles
      Add SCAN/SCOM initfiles for core/core common/CME/L2/L3/NCU
    
      Update initf procedures to call generated SCAN HWPs directly
        Use for Cronus only testing of initfiles prior to scan via image support
    
      Add SCOM initfiles, do not invoke yet to stage PPE commits
    
    Change-Id: I23882f3b31ca0fc93b6b0981975f73de4eec0ec1
    Original-Change-Id: I5b65ea980586a3cd0a4c0c0180473dec78234cba
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23910
    Tested-by: Jenkins Server
    Tested-by: PPE CI
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Tested-by: Thi N. Tran <thi@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/initfiles/p9_l2_scom.C
@@ -0,0 +1,261 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/initfiles/p9_l2_scom.C $   */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include "p9_l2_scom.H"
+#include <stdint.h>
+#include <stddef.h>
+#include <fapi2.H>
+
+using namespace fapi2;
+
+constexpr auto literal_0b0001 = 0b0001;
+constexpr auto literal_0b0100 = 0b0100;
+constexpr auto literal_0 = 0;
+constexpr auto literal_0x001 = 0x001;
+constexpr auto literal_1 = 1;
+constexpr auto literal_2 = 2;
+constexpr auto literal_0b0000 = 0b0000;
+
+fapi2::ReturnCode p9_l2_scom(const fapi2::Target<fapi2::TARGET_TYPE_EX>& TGT0,
+                             const fapi2::Target<fapi2::TARGET_TYPE_SYSTEM>& TGT1)
+{
+    fapi2::ReturnCode l_rc = 0;
+
+    do
+    {
+        fapi2::buffer<uint64_t> l_scom_buffer;
+        fapi2::ATTR_PROC_FABRIC_ADDR_BAR_MODE_Type l_TGT1_ATTR_PROC_FABRIC_ADDR_BAR_MODE;
+        l_rc = FAPI_ATTR_GET(fapi2::ATTR_PROC_FABRIC_ADDR_BAR_MODE, TGT1, l_TGT1_ATTR_PROC_FABRIC_ADDR_BAR_MODE);
+
+        if (l_rc)
+        {
+            FAPI_ERR("ERROR executing: FAPI_ATTR_GET (ATTR_PROC_FABRIC_ADDR_BAR_MODE)");
+            break;
+        }
+
+        fapi2::ATTR_SYSTEM_IPL_PHASE_Type l_TGT1_ATTR_SYSTEM_IPL_PHASE;
+        l_rc = FAPI_ATTR_GET(fapi2::ATTR_SYSTEM_IPL_PHASE, TGT1, l_TGT1_ATTR_SYSTEM_IPL_PHASE);
+
+        if (l_rc)
+        {
+            FAPI_ERR("ERROR executing: FAPI_ATTR_GET (ATTR_SYSTEM_IPL_PHASE)");
+            break;
+        }
+
+        {
+            l_rc = fapi2::getScom( TGT0, 0x1001080aull, l_scom_buffer );
+
+            if (l_rc)
+            {
+                FAPI_ERR("ERROR executing: getScom (0x1001080aull)");
+                break;
+            }
+
+            {
+                if ((l_TGT1_ATTR_PROC_FABRIC_ADDR_BAR_MODE == fapi2::ENUM_ATTR_PROC_FABRIC_ADDR_BAR_MODE_SMALL_SYSTEM))
+                {
+                    constexpr auto l_scom_buffer_ON = 0x1;
+                    l_scom_buffer.insert<uint64_t> (l_scom_buffer_ON, 23, 1, 63 );
+                }
+                else if ((l_TGT1_ATTR_PROC_FABRIC_ADDR_BAR_MODE == fapi2::ENUM_ATTR_PROC_FABRIC_ADDR_BAR_MODE_LARGE_SYSTEM))
+                {
+                    constexpr auto l_scom_buffer_OFF = 0x0;
+                    l_scom_buffer.insert<uint64_t> (l_scom_buffer_OFF, 23, 1, 63 );
+                }
+            }
+
+            {
+                if ((l_TGT1_ATTR_SYSTEM_IPL_PHASE == ENUM_ATTR_SYSTEM_IPL_PHASE_HB_RUNTIME))
+                {
+                    constexpr auto l_scom_buffer_ON = 0x1;
+                    l_scom_buffer.insert<uint64_t> (l_scom_buffer_ON, 15, 1, 63 );
+                }
+            }
+
+            l_rc = fapi2::putScom(TGT0, 0x1001080aull, l_scom_buffer);
+
+            if (l_rc)
+            {
+                FAPI_ERR("ERROR executing: putScom (0x1001080aull)");
+                break;
+            }
+        }
+
+        {
+            l_rc = fapi2::getScom( TGT0, 0x1001080bull, l_scom_buffer );
+
+            if (l_rc)
+            {
+                FAPI_ERR("ERROR executing: getScom (0x1001080bull)");
+                break;
+            }
+
+            {
+                l_scom_buffer.insert<uint64_t> (literal_0b0001, 4, 4, 60 );
+            }
+
+            {
+                l_scom_buffer.insert<uint64_t> (literal_0b0100, 8, 4, 60 );
+            }
+
+            l_rc = fapi2::putScom(TGT0, 0x1001080bull, l_scom_buffer);
+
+            if (l_rc)
+            {
+                FAPI_ERR("ERROR executing: putScom (0x1001080bull)");
+                break;
+            }
+        }
+
+        fapi2::ATTR_PROC_EPS_READ_CYCLES_Type l_TGT1_ATTR_PROC_EPS_READ_CYCLES;
+        l_rc = FAPI_ATTR_GET(fapi2::ATTR_PROC_EPS_READ_CYCLES, TGT1, l_TGT1_ATTR_PROC_EPS_READ_CYCLES);
+
+        if (l_rc)
+        {
+            FAPI_ERR("ERROR executing: FAPI_ATTR_GET (ATTR_PROC_EPS_READ_CYCLES)");
+            break;
+        }
+
+        {
+            l_rc = fapi2::getScom( TGT0, 0x10010810ull, l_scom_buffer );
+
+            if (l_rc)
+            {
+                FAPI_ERR("ERROR executing: getScom (0x10010810ull)");
+                break;
+            }
+
+            {
+                if ((l_TGT1_ATTR_PROC_EPS_READ_CYCLES[literal_0] != literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (l_TGT1_ATTR_PROC_EPS_READ_CYCLES[literal_0], 0, 12, 52 );
+                }
+                else if ((l_TGT1_ATTR_PROC_EPS_READ_CYCLES[literal_0] == literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (literal_0x001, 0, 12, 52 );
+                }
+            }
+
+            {
+                if ((l_TGT1_ATTR_PROC_EPS_READ_CYCLES[literal_1] != literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (l_TGT1_ATTR_PROC_EPS_READ_CYCLES[literal_1], 12, 12, 52 );
+                }
+                else if ((l_TGT1_ATTR_PROC_EPS_READ_CYCLES[literal_1] == literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (literal_0x001, 12, 12, 52 );
+                }
+            }
+
+            {
+                if ((l_TGT1_ATTR_PROC_EPS_READ_CYCLES[literal_2] != literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (l_TGT1_ATTR_PROC_EPS_READ_CYCLES[literal_2], 24, 12, 52 );
+                }
+                else if ((l_TGT1_ATTR_PROC_EPS_READ_CYCLES[literal_2] == literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (literal_0x001, 24, 12, 52 );
+                }
+            }
+
+            l_rc = fapi2::putScom(TGT0, 0x10010810ull, l_scom_buffer);
+
+            if (l_rc)
+            {
+                FAPI_ERR("ERROR executing: putScom (0x10010810ull)");
+                break;
+            }
+        }
+
+        fapi2::ATTR_PROC_EPS_WRITE_CYCLES_Type l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES;
+        l_rc = FAPI_ATTR_GET(fapi2::ATTR_PROC_EPS_WRITE_CYCLES, TGT1, l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES);
+
+        if (l_rc)
+        {
+            FAPI_ERR("ERROR executing: FAPI_ATTR_GET (ATTR_PROC_EPS_WRITE_CYCLES)");
+            break;
+        }
+
+        {
+            l_rc = fapi2::getScom( TGT0, 0x10010811ull, l_scom_buffer );
+
+            if (l_rc)
+            {
+                FAPI_ERR("ERROR executing: getScom (0x10010811ull)");
+                break;
+            }
+
+            {
+                if ((l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES[literal_0] != literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES[literal_0], 0, 12, 52 );
+                }
+                else if ((l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES[literal_0] == literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (literal_0x001, 0, 12, 52 );
+                }
+            }
+
+            {
+                if ((l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES[literal_1] != literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES[literal_1], 12, 12, 52 );
+                }
+                else if ((l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES[literal_1] == literal_0))
+                {
+                    l_scom_buffer.insert<uint64_t> (literal_0x001, 12, 12, 52 );
+                }
+            }
+
+            {
+                constexpr auto l_scom_buffer_OFF = 0x0;
+                l_scom_buffer.insert<uint64_t> (l_scom_buffer_OFF, 29, 1, 63 );
+            }
+
+            {
+                l_scom_buffer.insert<uint64_t> (literal_0b0000, 30, 4, 60 );
+            }
+
+            {
+                l_scom_buffer.insert<uint64_t> (literal_0b0001, 24, 4, 60 );
+            }
+
+            {
+                constexpr auto l_scom_buffer_MODE1 = 0x0;
+                l_scom_buffer.insert<uint64_t> (l_scom_buffer_MODE1, 28, 1, 63 );
+            }
+
+            l_rc = fapi2::putScom(TGT0, 0x10010811ull, l_scom_buffer);
+
+            if (l_rc)
+            {
+                FAPI_ERR("ERROR executing: putScom (0x10010811ull)");
+                break;
+            }
+        }
+    }
+    while (0);
+
+    return l_rc;
+}
