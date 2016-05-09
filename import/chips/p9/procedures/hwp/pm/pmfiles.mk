36bb0204ebddf1417f133d6709227c4bfd6103ea
Sachin Gupta <sgupta2m@in.ibm.com>
2016-05-09 08:04:48 -0500

    Enable p9_block_wakeup_intr
    Change-Id: I20387e156ec9ea2e103b3c0476f330fdcad4c5ae
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/24249
    Tested-by: Jenkins Server
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- import/chips/p9/procedures/hwp/pm/pmfiles.mk
+++ import/chips/p9/procedures/hwp/pm/pmfiles.mk
@@ -11,6 +11,7 @@ PM-CPP-SOURCES +=p9_pm_ocb_indir_setup_circular.C
 PM-CPP-SOURCES +=p9_pm_ocb_indir_setup_linear.C
 PM-CPP-SOURCES +=p9_pm_ocb_init.C
 PM-CPP-SOURCES +=p9_pm_utils.C
+PM-CPP-SOURCES +=p9_block_wakeup_intr.C
 PM-C-SOURCES =
 PM-S-SOURCES =
 
