eb4cb9fdb8b256c5c897fd42f8fa19ea932e6611
Sachin Gupta <sgupta2m@in.ibm.com>
2016-05-31 11:28:09 -0500

    Move prevasive procedures to SEEPROM
    Change-Id: Ifc930b6b27f73e9dc44a5c03037e7c8a0192364e
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/25190
    Tested-by: Jenkins Server
    Reviewed-by: AMIT J. TENDOLKAR <amit.tendolkar@in.ibm.com>

--- import/chips/p9/procedures/hwp/nest/nestfiles.mk
+++ import/chips/p9/procedures/hwp/nest/nestfiles.mk
@@ -17,6 +17,16 @@ NEST-CPP-SOURCES +=p9_pba_setup.C
 NEST-CPP-SOURCES +=p9_adu_access.C
 NEST-CPP-SOURCES +=p9_adu_setup.C
 NEST-CPP-SOURCES +=p9_adu_coherent_utils.C
+# TODO via RTC 152424
+# p9_ram_core.C will go to runtime makefile. Currently this procedure is in
+# perv directory. As we are putting perv procedures currently in SEEPROM.
+# So compiling p9_ram_core.C in nest makefile.
+NEST-CPP-SOURCES +=p9_ram_core.C
+# TODO via RTC 152424
+# swicth gear proecdures are in perv directory. But these procedures needs to
+# be executed from PIBMEM. So compiling these is nest makefile.
+NEST-CPP-SOURCES +=p9_sbe_gear_switcher.C
+NEST-CPP-SOURCES +=p9_sbe_tp_switch_gears.C
 NEST-C-SOURCES =
 NEST-S-SOURCES =
 
