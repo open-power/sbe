a12adc93d57ab5f7e74fecd47594a1cc8cd2113b
Sachin Gupta <sgupta2m@in.ibm.com>
2016-06-06 05:31:34 -0500

    Removed P9_HCD_STOP_SKIP_FLUSH/P9_HCD_STOP_SKIP_FLUSH compile flags
    Change-Id: I760c3238fbcab87bf345c578dbc0aeae76cb7fa6
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/25388
    Tested-by: Jenkins Server
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>
    Reviewed-by: AMIT J. TENDOLKAR <amit.tendolkar@in.ibm.com>

--- sbe/image/img_defs.mk
+++ sbe/image/img_defs.mk
@@ -1,3 +1,27 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: sbe/image/img_defs.mk $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2015,2016
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
 #  Make header for SBE image build
 #
 # Settings to for SBE image file build
@@ -320,9 +344,7 @@ GCC-CFLAGS += -fdata-sections
 endif
 
 ## Enable compiler flags for istep4
-GCC-CFLAGS += -DP9_HCD_STOP_SKIP_FLUSH
 GCC-CFLAGS += -DP9_HCD_STOP_SKIP_SCAN
-GCC-CFLAGS += -DP9_HCD_STOP_SKIP_ARRAYINIT
 
 CFLAGS =
 PPE-CFLAGS = $(CFLAGS) -c $(GCC-CFLAGS) $(PIPE-CFLAGS) $(GCC-O-LEVEL) $(INCLUDES)
