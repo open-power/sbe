c4b31fce136be2996800f5625d254f424640c388
Prasad Bg Ranganath <prasadbgr@in.ibm.com>
2016-07-07 05:58:36 -0500

    Putring:Fix EX scan region conversion support
    Change-Id: I13163ad7acf18ed38307fdcefaa30ed3223d75a0
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/26698
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/26709
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- import/chips/p9/utils/p9_putRingUtils.C
+++ import/chips/p9/utils/p9_putRingUtils.C
@@ -416,7 +416,7 @@ fapi2::ReturnCode setupScanRegion(const fapi2::Target<fapi2::TARGET_TYPE_ALL>&
         l_chiplet = (l_chipletID << 24);
     }
 
-    if (fapi2::TARGET_TYPE_EQ & i_target.getTargetType())
+    if (fapi2::TARGET_TYPE_EX & i_target.getTargetType())
     {
         // this gives position of ex (0 or 1)
         uint32_t l_ex_number = i_target.getTargetNumber();
