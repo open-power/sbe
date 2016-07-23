067fae0102047de80cdd9953b7d11fd4fa7939c4
Joe McGill <jmcgill@us.ibm.com>
2016-07-23 06:54:00 -0500

    scan HWP updates
      tested via Cronus platform putring implementation
    
      p9_sbe_attr_setup
      p9_setup_sbe_config
        adjust mailbox write/read logic to properly handle cache contained mode
    
      p9_sbe_gptr_time_initf
        remove unused MC mc_iom[01|23]_time rings
        add OBUS1/2 scans
        add PCI pci[0|1|2]_pll_gptr rings
        add N2 n2_psi_gptr ring
    
      p9_sbe_repr_initf
        remove unused MC mc_iom[01|23]_repr rings
        add OBUS1/2 scans
    
      p9_sbe_nest_initf
        skip MC iom[01|23]_fure scans which require DETERMINISTIC_TEST_EN
    
      p9_sbe_io_initf
        skip PCI pci[0|1|2]_fure scans which require DETERMINISTIC_TEST_EN
        remove DETERMINISTIC_TEST_EN application for XB
    
      p9_hcd_cache_initf
        remove explicit initfile invocation/ring caching in wrapper
        correct putring targeting
        add EX ex_l2_mode ring scan
    
      p9_hcd_core_initf
        remove explicit initfile invocation/ring caching in wrapper
        add EC ec_mode ring scan
    
      add DBG/ERR trace for all putRing calls
      remove unused *gptr_time_repr_initf HWPs and wrappers
    
    Change-Id: Iaeb6c60b552721c5867f1f14fea0beb8313dab0f
    Original-Change-Id: If1f8e9f5b327a6ab4f9b5271c53616ad20163b93
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/27400
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Prachi Gupta <pragupta@us.ibm.com>
    Reviewed-by: Kevin F. Reick <reick@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/30606
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- src/import/chips/p9/procedures/hwp/perv/pervfiles.mk
+++ src/import/chips/p9/procedures/hwp/perv/pervfiles.mk
@@ -38,7 +38,6 @@ PERV-CPP-SOURCES +=p9_sbe_chiplet_pll_initf.C
 PERV-CPP-SOURCES +=p9_sbe_chiplet_pll_setup.C
 PERV-CPP-SOURCES +=p9_sbe_chiplet_reset.C
 PERV-CPP-SOURCES +=p9_sbe_enable_seeprom.C
-PERV-CPP-SOURCES +=p9_sbe_gptr_time_repr_initf.C
 PERV-CPP-SOURCES +=p9_sbe_lpc_init.C
 PERV-CPP-SOURCES +=p9_sbe_nest_enable_ridi.C
 PERV-CPP-SOURCES +=p9_sbe_nest_initf.C
