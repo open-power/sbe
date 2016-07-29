ecca9de5aef67319455b2357e7e0b7429f07b055
Doug Gilbert <dgilbert@us.ibm.com>
2016-07-29 14:34:32 -0500

    PPE42 PK lib locaton changes
    Change-Id: I292a3e823073233eafb95f549cd4ccb1e7c9294a
    RTC: 157965
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/27653
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/28292
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- import/chips/p9/procedures/ppe/pk/ppe42/pkppe42files.mk
+++ import/chips/p9/procedures/ppe/pk/ppe42/pkppe42files.mk
@@ -50,21 +50,22 @@ PPE42-C-SOURCES = ppe42_core.c \
 		  ppe42_init.c \
 		  ppe42_irq_core.c\
 		  ppe42_gcc.c\
-                  ppe42_scom.c
-				   
+		  ppe42_scom.c\
+		  eabi.c\
+		  math.c
+
 PPE42-S-SOURCES =  ppe42_boot.S \
 		   ppe42_exceptions.S\
 		   div64.S\
 		   ppe42_timebase.S
 
-PPE42-TIMER-C-SOURCES = 
-PPE42-TIMER-S-SOURCES = 
+PPE42-TIMER-C-SOURCES =
+PPE42-TIMER-S-SOURCES =
 
 PPE42-THREAD-C-SOURCES =
 PPE42-THREAD-S-SOURCES = ppe42_thread_init.S
 
 PPE42_THREAD_OBJECTS= $(PPE42-THREAD-S-SOURCES:.S=.o)
-PPE42_OBJECTS = $(PPE42-C-SOURCES:.c=.o) $(PPE42-S-SOURCES:.S=.o)
-
-
+PPE42_OBJECTS = $(PPE42-C-SOURCES:.c=.o)
+PPE42_OBJECTS += $(PPE42-S-SOURCES:.S=.o)
 
