2041f00058aa99a76d526878f1e363c6a0f6b98e
Shakeeb <shakeebbk@in.ibm.com>
2016-08-26 02:12:35 -0500

    SBE code restructure: moving citest
    Change-Id: I7f74353e75208d7321566d5079e0e8c71cd1ae69
    RTC:159709
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/28829
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: AMIT J. TENDOLKAR <amit.tendolkar@in.ibm.com>

--- sbe/test/citest/copyright-check.sh
+++ sbe/test/citest/copyright-check.sh
@@ -2,12 +2,11 @@
 # IBM_PROLOG_BEGIN_TAG
 # This is an automatically generated prolog.
 #
-# $Source: sbe/build/citest/copyright-check.sh $
+# $Source: sbe/test/citest/copyright-check.sh $
 #
 # OpenPOWER sbe Project
 #
 # Contributors Listed Below - COPYRIGHT 2016
-# [+] International Business Machines Corp.
 #
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
