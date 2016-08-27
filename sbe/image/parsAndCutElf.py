6a2c5f719d206d9bb07f571f89a67fee40acbb9e
Shakeeb <shakeebbk@in.ibm.com>
2016-08-27 10:04:03 -0500

    SBE code restructure: moving obj directory
    Change-Id: Iec889b4bbcf895d5f7e794f45eb0c9d5cfa8c624
    RTC:159709
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/28874
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- sbe/image/parsAndCutElf.py
+++ sbe/image/parsAndCutElf.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: sbe/image/parsAndCutElf.py $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2016
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
 import os
 import subprocess
 import re
@@ -7,9 +31,9 @@ import sys
 
 # Workaround to cut SBE image from elf image.
 def parserElf():
-    cmd = "readelf -S ../obj/sbe_seeprom.out"
+    cmd = "readelf -S ../../obj/sbe_seeprom.out"
     firstSection = ".header"
-    cmd1 = "nm ../obj/sbe_seeprom.out | grep  _sbe_image_size"
+    cmd1 = "nm ../../obj/sbe_seeprom.out | grep  _sbe_image_size"
     output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     i = 0;
     for line in output.stdout:
@@ -36,7 +60,7 @@ def parserElf():
         exit(-1)
 
     # cut the image
-    cmd1 = "dd skip=" + str(startSize) + " count=" + str(endSize) + " if=../obj/sbe_seeprom.out of=../obj/sbe_seeprom.bin bs=1"
+    cmd1 = "dd skip=" + str(startSize) + " count=" + str(endSize) + " if=../../obj/sbe_seeprom.out of=../../obj/sbe_seeprom.bin bs=1"
     rc = os.system(cmd1)
     if ( rc ):
        print "ERROR running %s: %d "%( cmd1, rc )
