f17fc5d29a53aa4d519264459671507df73d67ea
nagurram-in <nagendra.g@in.ibm.com>
2016-11-15 01:19:20 -0600

    L1 - Fastarray on FSP
    Change-Id: Ib507075cef3cc71f38de5a32cd1e7f71718ebfd7
    Original-Change-Id: I7cde3e16038b67497dc828fa700fd4d017063730
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/32635
    Reviewed-by: Johannes Koesters <koesters@de.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Shakeeb A. Pasha B K <shakeebbk@in.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/35874

---
+++ src/import/chips/p9/procedures/hwp/perv/p9_sbe_fastarray_cleanup.C
@@ -0,0 +1,56 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/perv/p9_sbe_fastarray_cleanup.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2017                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @file  p9_sbe_fastarray_cleanup.C
+///
+/// @brief Cleanup sub-procedure for p9_fastarray procedure
+//------------------------------------------------------------------------------
+// *HWP HW Owner        : Joachim Fenkes <fenkes@de.ibm.com>
+// *HWP HW Backup Owner : Nick Klazynski <jklazyns@us.ibm.com>
+// *HWP FW Owner        : Nagendra Gurram <nagendra.g@in.ibm.com>
+// *HWP Team            : Chip
+// *HWP Level           : 1
+// *HWP Consumed by     : SBE
+//------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------------
+#include <p9_sbe_fastarray_cleanup.H>
+
+/**
+ * @brief Cleanup sub-procedure for p9_fastarray procedure
+ * @param[in] i_target_chiplet The chiplet to clean up
+ * @return FAPI2_RC_SUCCESS if success, else error code.
+ */
+fapi2::ReturnCode p9_sbe_fastarray_cleanup(
+    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet)
+{
+    FAPI_INF("Start");
+    FAPI_INF("End");
+    return fapi2::current_err;
+}
+
+
