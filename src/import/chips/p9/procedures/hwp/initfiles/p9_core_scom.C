ca354cab56376dcfdae97ba84812dcb444610f8b
Nick Klazynski <jklazyns@us.ibm.com>
2016-12-16 14:44:35 -0600

    Add new core workarounds, defect inline in the initfile
    Unmask tc_fir_xstop for non-cache contained runs
    
    Change-Id: Ied34464771d8468718f2d86e48b214b707590fed
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/34011
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/34569
    Reviewed-by: Hostboot Team <hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- src/import/chips/p9/procedures/hwp/initfiles/p9_core_scom.C
+++ src/import/chips/p9/procedures/hwp/initfiles/p9_core_scom.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER sbe Project                                                  */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2016                             */
+/* Contributors Listed Below - COPYRIGHT 2016,2017                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -29,7 +29,7 @@
 
 using namespace fapi2;
 
-constexpr uint64_t literal_0x4301D70000AB7696 = 0x4301D70000AB7696;
+constexpr uint64_t literal_0x0301D70000AB7696 = 0x0301D70000AB7696;
 constexpr uint64_t literal_0x0000000000000000 = 0x0000000000000000;
 constexpr uint64_t literal_0xA858009775100008 = 0xA858009775100008;
 
@@ -40,7 +40,7 @@ fapi2::ReturnCode p9_core_scom(const fapi2::Target<fapi2::TARGET_TYPE_CORE>& TGT
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x20010a43ull, l_scom_buffer ));
 
-            l_scom_buffer.insert<0, 64, 0, uint64_t>(literal_0x4301D70000AB7696 );
+            l_scom_buffer.insert<0, 64, 0, uint64_t>(literal_0x0301D70000AB7696 );
             FAPI_TRY(fapi2::putScom(TGT0, 0x20010a43ull, l_scom_buffer));
         }
         {
