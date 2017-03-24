88d80d62c7f2792b69480951afef2617fa1d4532
spashabk-in <shakeebbk@in.ibm.com>
2017-03-24 04:45:15 -0500

    Modify op distribute tool
    Change-Id: I8dcc64103f5085ba7c2c04280231c8bef1a6c880
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/38400
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- src/build/sbeOpDistribute.py
+++ src/build/sbeOpDistribute.py
@@ -35,9 +35,9 @@ def usage():
 
 def run_system_cmd(cmd):
     print 'Cmd:<'+cmd+'>'
-#    if(os.system(cmd)):
-#        print "ERROR running cmd:<"+cmd+">"
-#        exit(1)
+    if(os.system(cmd)):
+        print "ERROR running cmd:<"+cmd+">"
+        exit(1)
 
 def main(argv):
     try:
@@ -75,28 +75,13 @@ def main(argv):
             host_dir = str(arg)
             assert os.path.exists(arg), "Did not find the file at,"+str(arg)
 
-    SEEPROM_IMAGE           = 'sbe_seeprom.bin'
-    SEEPROM_HDR_BIN         = 'sbe_seeprom.hdr.bin'
     SBE_BINARIES_PATH       = staging_dir+'/sbe_binaries'
-    HB_BUILD_IMAGES         = staging_dir+'/hostboot_build_images'
-    SBE_OUT_BINARY          = staging_dir+'/hostboot_binaries/nimbus_sbe.img'
-    HW_REF_IMAGE            = staging_dir+'/hostboot_binaries/'+CHIPID+'.ref_image.bin'
-    IPL_BUILD_TOOL          = img_dir+'/p9_ipl_build'
-    BUILD_PREFIX_HEADER     = 'buildPrefixHeader.pl'
-    ECC_TOOL                = host_dir+'/usr/bin/ecc'
+    SEEPROM_IMAGE           = 'sbe_seeprom.bin'
 
     # Create binaries folder
     run_system_cmd('mkdir -p '+SBE_BINARIES_PATH)
     for ecLevel in p9n_EC:
         # Copy sbe raw binary to binaries folder
         run_system_cmd('cp '+img_dir+'/'+'sbe_seeprom_'+DD_level[ecLevel]+'.bin'+' '+SBE_BINARIES_PATH+'/'+CHIPID+'_'+ecLevel+'.'+SEEPROM_IMAGE)
-        # Add HW ref image
-        run_system_cmd(IPL_BUILD_TOOL+' '+SBE_BINARIES_PATH+'/'+CHIPID+'_'+ecLevel+'.'+SEEPROM_IMAGE+' '+HW_REF_IMAGE+' 0x'+ecLevel)
-        # Add build header
-        run_system_cmd(BUILD_PREFIX_HEADER+' --inputFile '+SBE_BINARIES_PATH+'/'+CHIPID+'_'+ecLevel+'.'+SEEPROM_IMAGE+' --outputFile '+SBE_BINARIES_PATH+'/'+CHIPID+'_'+ecLevel+'.'+SEEPROM_HDR_BIN)
-        # buildSbePart.pl
-        run_system_cmd(HB_BUILD_IMAGES+'/buildSbePart.pl --sbeOutBin '+SBE_OUT_BINARY+'  --ecImg_'+ecLevel+' '+SBE_BINARIES_PATH+'/'+CHIPID+'_'+ecLevel+'.'+SEEPROM_HDR_BIN)
-        run_system_cmd('dd if='+SBE_OUT_BINARY+' of='+SBE_OUT_BINARY+'.256K ibs=256K conv=sync')
-        run_system_cmd(ECC_TOOL+' --inject '+SBE_OUT_BINARY+'.256K --output '+SBE_OUT_BINARY+'.ecc --p8')
 if __name__ == "__main__":
     main(sys.argv)
