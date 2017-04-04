b98bddb354ab62397204f19fc6bd97a73cc479f8
Luke Murray <murrayl@us.ibm.com>
2017-04-04 17:11:31 -0500

    Adding epsilon divider variable to cache initfiles
    There are several dials in the caches that change what value
    they are set to based on the epsilon divider dial setting.
    To simplify, I've added a variable that controls all of these
    dial settings.
    
    Change-Id: Ife6a875f8f0fcc0543be609f9e4032ac64cfb808
    Original-Change-Id: I8f7030bd56900a228475bac5d890a30db8c4dc18
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/38842
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: ALEXANDER M. TAFT <amtaft@us.ibm.com>
    Reviewed-by: Jenny Huynh <jhuynh@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/initfiles/p9_l2_scom.C
+++ src/import/chips/p9/procedures/hwp/initfiles/p9_l2_scom.C
@@ -43,6 +43,7 @@ fapi2::ReturnCode p9_l2_scom(const fapi2::Target<fapi2::TARGET_TYPE_EX>& TGT0,
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_FABRIC_PUMP_MODE, TGT1, l_TGT1_ATTR_PROC_FABRIC_PUMP_MODE));
         fapi2::ATTR_SYSTEM_IPL_PHASE_Type l_TGT1_ATTR_SYSTEM_IPL_PHASE;
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_SYSTEM_IPL_PHASE, TGT1, l_TGT1_ATTR_SYSTEM_IPL_PHASE));
+        uint64_t l_def_L2_EPS_DIVIDE = literal_1;
         fapi2::ATTR_PROC_EPS_READ_CYCLES_T0_Type l_TGT1_ATTR_PROC_EPS_READ_CYCLES_T0;
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_EPS_READ_CYCLES_T0, TGT1, l_TGT1_ATTR_PROC_EPS_READ_CYCLES_T0));
         fapi2::ATTR_PROC_EPS_READ_CYCLES_T1_Type l_TGT1_ATTR_PROC_EPS_READ_CYCLES_T1;
@@ -95,20 +96,25 @@ fapi2::ReturnCode p9_l2_scom(const fapi2::Target<fapi2::TARGET_TYPE_EX>& TGT0,
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x10010810ull, l_scom_buffer ));
 
-            l_scom_buffer.insert<0, 12, 52, uint64_t>(((l_TGT1_ATTR_PROC_EPS_READ_CYCLES_T0 / literal_8) + literal_1) );
-            l_scom_buffer.insert<12, 12, 52, uint64_t>(((l_TGT1_ATTR_PROC_EPS_READ_CYCLES_T1 / literal_8) + literal_1) );
-            l_scom_buffer.insert<24, 12, 52, uint64_t>(((l_TGT1_ATTR_PROC_EPS_READ_CYCLES_T2 / literal_8) + literal_1) );
+            l_scom_buffer.insert<0, 12, 52, uint64_t>((((l_TGT1_ATTR_PROC_EPS_READ_CYCLES_T0 / literal_8) / l_def_L2_EPS_DIVIDE) +
+                    literal_1) );
+            l_scom_buffer.insert<12, 12, 52, uint64_t>((((l_TGT1_ATTR_PROC_EPS_READ_CYCLES_T1 / literal_8) / l_def_L2_EPS_DIVIDE) +
+                    literal_1) );
+            l_scom_buffer.insert<24, 12, 52, uint64_t>((((l_TGT1_ATTR_PROC_EPS_READ_CYCLES_T2 / literal_8) / l_def_L2_EPS_DIVIDE) +
+                    literal_1) );
             FAPI_TRY(fapi2::putScom(TGT0, 0x10010810ull, l_scom_buffer));
         }
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x10010811ull, l_scom_buffer ));
 
-            l_scom_buffer.insert<0, 12, 52, uint64_t>(((l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES_T1 / literal_8) + literal_1) );
-            l_scom_buffer.insert<12, 12, 52, uint64_t>(((l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES_T2 / literal_8) + literal_1) );
+            l_scom_buffer.insert<0, 12, 52, uint64_t>((((l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES_T1 / literal_8) / l_def_L2_EPS_DIVIDE) +
+                    literal_1) );
+            l_scom_buffer.insert<12, 12, 52, uint64_t>((((l_TGT1_ATTR_PROC_EPS_WRITE_CYCLES_T2 / literal_8) / l_def_L2_EPS_DIVIDE) +
+                    literal_1) );
             constexpr auto l_EXP_L2_L2MISC_L2CERRS_EPS_CNT_USE_L2_DIVIDER_EN_OFF = 0x0;
             l_scom_buffer.insert<29, 1, 63, uint64_t>(l_EXP_L2_L2MISC_L2CERRS_EPS_CNT_USE_L2_DIVIDER_EN_OFF );
             l_scom_buffer.insert<30, 4, 60, uint64_t>(literal_0b0000 );
-            l_scom_buffer.insert<24, 4, 60, uint64_t>(literal_0b0001 );
+            l_scom_buffer.insert<24, 4, 60, uint64_t>(l_def_L2_EPS_DIVIDE );
             constexpr auto l_EXP_L2_L2MISC_L2CERRS_EPS_MODE_SEL_MODE1 = 0x0;
             l_scom_buffer.insert<28, 1, 63, uint64_t>(l_EXP_L2_L2MISC_L2CERRS_EPS_MODE_SEL_MODE1 );
             FAPI_TRY(fapi2::putScom(TGT0, 0x10010811ull, l_scom_buffer));
