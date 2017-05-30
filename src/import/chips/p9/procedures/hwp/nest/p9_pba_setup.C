ae8c48308c09e4489689320abfd49289c9bc368a
Joe McGill <jmcgill@us.ibm.com>
2017-05-30 11:34:43 -0500

    update owner comments in ADU, PBA, TOD HWPs
    Change-Id: Ic3182c3e8e1d8917b779f1189340d24b2de57865
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/41119
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/41122
    Reviewed-by: Hostboot Team <hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- src/import/chips/p9/procedures/hwp/nest/p9_pba_setup.C
+++ src/import/chips/p9/procedures/hwp/nest/p9_pba_setup.C
@@ -28,7 +28,7 @@
 /// @file p9_pba_setup.C
 /// @brief Setup the registers for a read/write to the PBA
 ///
-// *HWP HWP Owner: Christina Graves clgraves@us.ibm.com
+// *HWP HWP Owner: Joe McGill jmcgill@us.ibm.com
 // *HWP FW Owner: Thi Tran thi@us.ibm.com
 // *HWP Team: Nest
 // *HWP Level: 3
