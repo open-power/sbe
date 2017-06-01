f848e2ea369b83c46bf8a9b12574984cc4905455
Joachim Fenkes <fenkes@de.ibm.com>
2017-06-01 17:39:52 +0200

    p9_sbe_chiplet_pll_initf: Level 3
    Change-Id: I041f9d00fd146b909018e2dc1c303a4a9aa80af0
    Original-Change-Id: I59e223e3f68fa2707686080fb040434dd9f43e4e
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/41245
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/perv/p9_sbe_chiplet_pll_initf.C
+++ src/import/chips/p9/procedures/hwp/perv/p9_sbe_chiplet_pll_initf.C
@@ -31,7 +31,7 @@
 // *HWP HW Backup Owner : srinivas naga <srinivan@in.ibm.com>
 // *HWP FW Owner        : sunil kumar <skumar8j@in.ibm.com>
 // *HWP Team            : Perv
-// *HWP Level           : 2
+// *HWP Level           : 3
 // *HWP Consumed by     : SBE
 //------------------------------------------------------------------------------
 
