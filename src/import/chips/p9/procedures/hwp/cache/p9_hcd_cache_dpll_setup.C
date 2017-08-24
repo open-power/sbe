6f30cb16de63c252252c2ee14d0d3c19cdd2f130
Joachim Fenkes <fenkes@de.ibm.com>
2017-08-24 14:21:37 +0200

    {nest,cache}_pll_setup: Properly clear PLL unlock indication
    p9_sbe_npll_setup used a bad target to clear the PLL unlock indication
        (luckily the misguided writes hit benign registers)
    
    p9_hcd_cache_dpll_setup didn't clear the unlock indicator at all
    
    CMVC-Prereq: 1035048
    
    Change-Id: Ia77e8f168e00eba9effb9b3cf0bbb7df2e814749
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/45100
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Soma Bhanutej <soma.bhanu@in.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/45102
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- src/import/chips/p9/procedures/hwp/cache/p9_hcd_cache_dpll_setup.C
+++ src/import/chips/p9/procedures/hwp/cache/p9_hcd_cache_dpll_setup.C
@@ -243,6 +243,12 @@ p9_hcd_cache_dpll_setup(
         FAPI_TRY(getScom(i_target, EQ_OPCG_ALIGN, l_data64));
         l_data64.insertFromRight<47, 5>(0x3);
         FAPI_TRY(putScom(i_target, EQ_OPCG_ALIGN, l_data64));
+
+        FAPI_DBG("Clear PLL unlock errors in PCB slave, unmask FIR propagation");
+        FAPI_TRY(putScom(i_target, EQ_ERROR_REG, 0xFFFFFFFFFFFFFFFF));
+        FAPI_TRY(getScom(i_target, EQ_SLAVE_CONFIG_REG, l_data64));
+        l_data64.clearBit<12>();
+        FAPI_TRY(putScom(i_target, EQ_SLAVE_CONFIG_REG, l_data64));
     }
 
     FAPI_DBG("Drop skew/duty cycle adjust func_clksel via NET_CTRL0[22]");
