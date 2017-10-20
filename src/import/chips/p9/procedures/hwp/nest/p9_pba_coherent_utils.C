e15c80ad9743e5d5d9d6da585f0c598d00832930
Thi Tran <thi@us.ibm.com>
2017-10-20 14:28:00 -0500

    Need to clear OCB3 errors before a PBA operation
    Change-Id: Iaa7246b82bc490222162ef74011db78bc8631e23
    CQ:SW405593
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/48647
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/48649
    Reviewed-by: Hostboot Team <hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- src/import/chips/p9/procedures/hwp/nest/p9_pba_coherent_utils.C
+++ src/import/chips/p9/procedures/hwp/nest/p9_pba_coherent_utils.C
@@ -199,11 +199,14 @@ extern "C"
 
         //Write the OCB3 Status Control Register
         //Configure linear stream mode (auto-increment +8 with each data register read/write)
-        //set bit 4 and unset bit 5 of OCB3 Status Control Register
-        ocb_status_ctl_data.flush<0>().setBit<5>();
+        // Set bits 0:1 to clear PULL_READ_UNDERFLOW and PUSH_WRITE_OVERFLOW
+        // Set bit 5 to clear stream type (enables linear mode)
+        // Set bits 6:15 to clear other errors
+        ocb_status_ctl_data.flush<0>().setBit<0, 2>().setBit<5, 11>();
         FAPI_TRY(fapi2::putScom(i_target, PU_OCB_PIB_OCBCSR3_CLEAR,
                                 ocb_status_ctl_data),
                  "Error writing to the OCB3 Status Control Register with and mask");
+        // Set bit 4 to enable stream mode
         ocb_status_ctl_data.flush<0>().setBit<4>();
         FAPI_TRY(fapi2::putScom(i_target, PU_OCB_PIB_OCBCSR3_OR,
                                 ocb_status_ctl_data),
