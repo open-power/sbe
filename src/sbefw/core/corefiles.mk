1c7f2eb205f464ac09f11afbd35002ab78b43c62
spashabk-in <shakeebbk@in.ibm.com>
2017-12-06 00:12:10 -0600

    [SBE-code-re-org][3] Merge core folder makefiles
    Remove separate seeprom makefile from core folder
    
    Change-Id: I5693d948d931cb4770dea1cb9ec6e45e3fb9b592
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/50553
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

---
+++ src/sbefw/core/corefiles.mk
@@ -0,0 +1,74 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/sbefw/core/corefiles.mk $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2015,2018
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+COREPIBMEM-CPP-SOURCES = sbemain.C
+COREPIBMEM-CPP-SOURCES += sbeirq.C
+COREPIBMEM-CPP-SOURCES += sbecmdreceiver.C
+COREPIBMEM-CPP-SOURCES += sbecmdprocessor.C
+COREPIBMEM-CPP-SOURCES += sbecmdparser.C
+COREPIBMEM-CPP-SOURCES += sbecmdscomaccess.C
+COREPIBMEM-CPP-SOURCES += sbecmdiplcontrol.C
+COREPIBMEM-CPP-SOURCES += pool.C
+COREPIBMEM-CPP-SOURCES += sbecmdgeneric.C
+COREPIBMEM-CPP-SOURCES += sbeFifoMsgUtils.C
+COREPIBMEM-CPP-SOURCES += sbecmdmemaccess.C
+COREPIBMEM-CPP-SOURCES += sbeHostUtils.C
+COREPIBMEM-CPP-SOURCES += sbecmdcntrldmt.C
+COREPIBMEM-CPP-SOURCES += sbecmdsram.C
+COREPIBMEM-CPP-SOURCES += sberegaccess.C
+COREPIBMEM-CPP-SOURCES += sbecmdcntlinst.C
+COREPIBMEM-CPP-SOURCES += sbecmdregaccess.C
+COREPIBMEM-CPP-SOURCES += sbeFFDC.C
+COREPIBMEM-CPP-SOURCES += sbecmdringaccess.C
+COREPIBMEM-CPP-SOURCES += sbescom.C
+COREPIBMEM-CPP-SOURCES += sbecmdmpipl.C
+COREPIBMEM-CPP-SOURCES += sbefapiutil.C
+COREPIBMEM-CPP-SOURCES += sbeutil.C
+COREPIBMEM-CPP-SOURCES += sbecmdtracearray.C
+COREPIBMEM-CPP-SOURCES += sbeTimerSvc.C
+COREPIBMEM-CPP-SOURCES += sbecmdCntrlTimer.C
+COREPIBMEM-CPP-SOURCES += sbeHostMsg.C
+COREPIBMEM-CPP-SOURCES += sbeSpMsg.C
+COREPIBMEM-CPP-SOURCES += sbeglobals.C
+COREPIBMEM-CPP-SOURCES += sbeMemAccessInterface.C
+COREPIBMEM-CPP-SOURCES += sbeSecureMemRegionManager.C
+COREPIBMEM-CPP-SOURCES += sbeSecurity.C
+COREPIBMEM-CPP-SOURCES += chipop_handler.C
+
+COREPIBMEM-C-SOURCES =
+COREPIBMEM-S-SOURCES =
+
+COREPIBMEM_OBJECTS = $(COREPIBMEM-C-SOURCES:.c=.o) $(COREPIBMEM-CPP-SOURCES:.C=.o) $(COREPIBMEM-S-SOURCES:.S=.o)
+
+# seeprom objects
+CORESEEPROM-CPP-SOURCES = sbecmdgeneric.C
+CORESEEPROM-CPP-SOURCES += sbecmdmpipl.C
+CORESEEPROM-CPP-SOURCES += sbecmdmemaccess.C
+CORESEEPROM-CPP-SOURCES += sbeSecureMemRegionManager.C
+CORESEEPROM-CPP-SOURCES += sbecmdiplcontrol.C
+CORESEEPROM-CPP-SOURCES += sbecmdfastarray.C
+
+CORESEEPROM-C-SOURCES =
+CORESEEPROM-S-SOURCES =
+
+CORESEEPROM_OBJECTS = $(CORESEEPROM-C-SOURCES:.c=.o) $(CORESEEPROM-CPP-SOURCES:.C=.o) $(CORESEEPROM-S-SOURCES:.S=.o)
