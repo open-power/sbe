71ffbf448c4e7b57fafeb334afa27347f5620cfc
spashabk-in <shakeebbk@in.ibm.com>
2017-12-06 05:19:18 -0600

    [SBE-code-re-org][4] Adding app common makefiles
    Addition of app/common directory and associated makefiles
    
    Change-Id: I9cd107965b5712ee03b32d3939828281a6cadf83
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/50569
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

---
+++ src/sbefw/app/common/appcommonfiles.mk
@@ -0,0 +1,36 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/sbefw/app/common/appcommonfiles.mk $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2015,2017
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+APPCOMMONPIBMEM-CPP-SOURCES = sbeappcommon.C
+
+APPCOMMONPIBMEM-C-SOURCES =
+APPCOMMONPIBMEM-S-SOURCES =
+
+APPCOMMONPIBMEM_OBJECTS = $(APPCOMMONPIBMEM-C-SOURCES:.c=.o) $(APPCOMMONPIBMEM-CPP-SOURCES:.C=.o) $(APPCOMMONPIBMEM-S-SOURCES:.S=.o)
+
+APPCOMMONSEEPROM-CPP-SOURCES = sbeappcommon.C
+
+APPCOMMONSEEPROM-C-SOURCES =
+APPCOMMONSEEPROM-S-SOURCES =
+
+APPCOMMONSEEPROM_OBJECTS = $(APPCOMMONSEEPROM-C-SOURCES:.c=.o) $(APPCOMMONSEEPROM-CPP-SOURCES:.C=.o) $(APPCOMMONSEEPROM-S-SOURCES:.S=.o)
