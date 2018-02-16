cce76062ef2fc25aa421db4d98892b5ea5b5afae
Joachim Fenkes <fenkes@de.ibm.com>
2018-02-16 10:23:10 +0100

    hwpErrors: Use wildcard instead of explicit list
    The makefile was including all XMLs but one (that was unused) anyway.
    Switch to wildcard so we don't have to explicitly touch hwpErrors.mk
    every time we add an error XML file.
    
    Change-Id: I127dcb75ab3ecb0dadaf73e06d6f6fd3e8294524
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/54212
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- src/import/chips/p9/procedures/xml/error_info/hwpErrors.mk
+++ src/import/chips/p9/procedures/xml/error_info/hwpErrors.mk
@@ -5,7 +5,7 @@
 #
 # OpenPOWER sbe Project
 #
-# Contributors Listed Below - COPYRIGHT 2015,2017
+# Contributors Listed Below - COPYRIGHT 2015,2018
 # [+] International Business Machines Corp.
 #
 #
@@ -33,51 +33,4 @@
 PERV_CURR_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
 # This variable name must not change
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_perv_sbe_cmn_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_arrayinit_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_chiplet_init_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_pll_setup_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_common_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_fabricinit_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_gear_switcher_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_nest_startclocks_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_npll_setup_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_tp_arrayinit_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_tp_chiplet_init3_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_mcs_setup_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_hcd_cache_dpll_setup_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_common_poweronoff_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_hcd_cache_startclocks_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_hcd_core_startclocks_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_pba_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_load_bootloader_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_thread_control_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_startclock_chiplets_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_select_ex_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_check_master_stop15_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_pm_ocb_indir_access_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_pm_ocb_init_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_block_wakeup_intr_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_ram_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_tp_switch_gears_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_adu_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_npll_initf_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_chiplet_pll_initf_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_scominit_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_plat_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_hcd_core_stopclocks_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_hcd_cache_stopclocks_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_hcd_l2_stopclocks_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_l2_flush_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_l3_flush_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_check_quiesce_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_pm_pfet_control_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_hcd_cache_scominit_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_tracearray_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_hcd_cache_initf_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_quad_power_off_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_fastarray_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_suspend_io_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_ppe_utils.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_suspend_powman_errors.xml
-ERROR_XML_FILES += $(PERV_CURR_DIR)/p9_sbe_ring_errors.xml
+ERROR_XML_FILES += $(wildcard $(PERV_CURR_DIR)/*.xml)
