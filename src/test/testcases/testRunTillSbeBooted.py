c10e17d5baea61727cc04ce197eea3f7e5eae534
spashabk-in <shakeebbk@in.ibm.com>
2018-03-27 04:55:59 -0500

    Run simics intially till SBE is booted
    Change-Id: Iab0eb98231bb1f66944cbb5f990f80ab9e827e49
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/56297
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

---
+++ src/test/testcases/testRunTillSbeBooted.py
@@ -0,0 +1,45 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/test/testcases/testRunTillSbeBooted.py $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2018
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+import os
+import sys
+sys.path.append("targets/p9_nimbus/sbeTest" )
+import testUtil
+err = False
+
+import testScomUtil
+
+from sim_commands import *
+lbus = conf.p9Proc0.proc_lbus_map
+
+while True:
+    try:
+        if testUtil.read(lbus, 0x2824, 4)[0] & 0x80 :
+            print "SBE is booted, continue"
+            break
+        else:
+            print "SBE is still not booted"
+
+    except:
+        print "."
+    testUtil.runCycles( 10000000 )
