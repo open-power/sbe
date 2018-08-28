3974b9facebec55cd795b235647a89c7070a5d74
Yue Du <daviddu@us.ibm.com>
2018-08-28 08:35:42 -0500

    P10 Powman: initial common file port and qme image structures
    The following change is not included in this commit as being hack
    
    To build qme: ekb build qme_p10dd1
    need to add/change these lines to ekb build tool files
    
    modified tools/build/ekb.dir/mflags.env.mk
    Using new compiler from a temp test dir(Need to formalize this)
    PPE_TOOL_PATH ?= $(CTEPATH)/tools/ppetools/test
    
    modified tools/build/image.dir/cflags.env.mk
    Change to 42x from 42 for new ppe instructions
    ASFLAGS = -mppe42x
    PPE_CFLAGS += -mcpu=ppe42x
    
    modified tools/build/image.dir/ppe.env.mk
    Base source dir for new qme target
    QME_SRCDIR=$(ROOTPATH)/chips/p10/procedures/ppe/qme
    
    modified tools/build/ekb.dir/dirs.env.mk
    For makefile search
    MAKEFILE_PATH += $(ROOTPATH)/chips/p10/procedures/ppe/qme
    
    Change-Id: I552ecbe9fd3581c695e8ccad6399152d160da7d7
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/70521
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/79801
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

---
+++ src/import/chips/p10/common/ppe/pk/ppe42/ppe42_irq.h
@@ -0,0 +1,248 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/common/ppe/pk/ppe42/ppe42_irq.h $        */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2018,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#ifndef __PPE42_IRQ_H__
+#define __PPE42_IRQ_H__
+
+/// \file ppe42_irq.h
+/// \brief PPE42 interrupt handling for PK
+///
+/// Interrupt handling protocols and interrupt controller programming are
+/// inherently non-portable, however PK defines APIs that may be useful among
+/// different machines.
+///
+
+
+// Define pseudo-IRQ numbers for PPE42 built-in interrupts.  These numbers
+// will appear in bits 16:23 of SPRG0 (__PkKernelContext) when the handlers
+// are active
+
+#define PPE42_EXC_MACHINE_CHECK         0x50
+#define PPE42_EXC_DATA_STORAGE          0x53
+#define PPE42_EXC_INSTRUCTION_STORAGE   0x54
+#define PPE42_EXC_ALIGNMENT             0x56
+#define PPE42_EXC_PROGRAM               0x57
+#define PPE42_IRQ_DEC                   0x58
+#define PPE42_IRQ_FIT                   0x59
+#define PPE42_IRQ_WATCHDOG              0x5A
+
+
+//  Unhandled exceptions default to a kernel panic, but the application can
+//  override these definition.  Note that the exception area only allocates 32
+//  bytes (8 instructions) to an unhandled exception, so any redefinition
+//  would most likely be a branch to an application-defined handler.
+
+// Empty PPE FFDC Handler plugged here, dependent platform needs to plug it in,
+// if specific registers are required to be saved-off during the interupt
+// execution
+// by Default it will not generate any branch instruction in the pk interrupt
+// vector table
+#ifndef PPE_CAPTURE_INTERRUPT_FFDC
+    #define PPE_CAPTURE_INTERRUPT_FFDC
+#endif
+
+#ifndef PPE42_MACHINE_CHECK_HANDLER
+    #define PPE42_MACHINE_CHECK_HANDLER PK_PANIC( PPE42_MACHINE_CHECK_PANIC)
+#endif
+
+#ifndef PPE42_DATA_STORAGE_HANDLER
+    #define PPE42_DATA_STORAGE_HANDLER  PK_PANIC(PPE42_DATA_STORAGE_PANIC)
+#endif
+
+#ifndef PPE42_INSTRUCTION_STORAGE_HANDLER
+#define PPE42_INSTRUCTION_STORAGE_HANDLER \
+    PK_PANIC(PPE42_INSTRUCTION_STORAGE_PANIC)
+#endif
+
+#ifndef PPE42_ALIGNMENT_HANDLER
+    #define PPE42_ALIGNMENT_HANDLER     PK_PANIC(PPE42_DATA_ALIGNMENT_PANIC)
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////
+//  PK API
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef __ASSEMBLER__
+
+/// An IRQ handler takes 2 arguments:
+/// \arg \c arg - Private handler data installed by \c ssx_irq_setup() or
+///                   \c ssx_irq_handler_set().
+/// \arg \c irq     - The IRQ id; to enable a generic handler to manipulate
+///                   its own interrupt status .
+
+typedef void (*PkIrqHandler)(void* arg, PkIrqId irq);
+
+/// Declare a subroutine as an IRQ handler
+
+#define PK_IRQ_HANDLER(f) void f(void* arg, PkIrqId irq)
+
+int pk_irq_setup(PkIrqId irq,
+                 int      polarity,
+                 int      trigger);
+
+int pk_irq_handler_set(PkIrqId      irq,
+                       PkIrqHandler handler,
+                       void*         arg);
+
+void pk_irq_enable(PkIrqId irq);
+void pk_irq_disable(PkIrqId irq);
+void pk_irq_statusclear(PkIrqId irq);
+
+PK_IRQ_HANDLER(__ppe42_default_irq_handler);
+PK_IRQ_HANDLER(__ppe42_phantom_irq_handler);
+
+
+int
+ppe42_fit_setup(PkIrqHandler handler, void* arg);
+
+
+///  The address of the optional FIT interrupt handler
+
+UNLESS__PPE42_IRQ_CORE_C__(extern)
+volatile
+PkIrqHandler __ppe42_fit_routine;
+
+
+/// The private data of the optional FIT interrupt handler
+
+UNLESS__PPE42_IRQ_CORE_C__(extern)
+volatile
+void* __ppe42_fit_arg;
+
+
+int
+ppe42_watchdog_setup(int tcr_wp, int tcr_wrc,
+                     PkIrqHandler handler, void* arg);
+
+
+///  The address of the optional Watchdog interrupt handler
+
+UNLESS__PPE42_IRQ_CORE_C__(extern)
+volatile
+PkIrqHandler __ppe42_watchdog_routine;
+
+
+/// The private data of the optional Watchdog interrupt handler
+
+UNLESS__PPE42_IRQ_CORE_C__(extern)
+volatile
+void* __ppe42_watchdog_arg;
+
+
+int
+ppe42_debug_setup(PkIrqHandler handler, void* arg);
+
+
+///  The address of the optional Debug interrupt handler
+
+UNLESS__PPE42_IRQ_CORE_C__(extern)
+volatile
+PkIrqHandler __ppe42_debug_routine;
+
+
+/// The private data of the optional Watchdog interrupt handler
+
+UNLESS__PPE42_IRQ_CORE_C__(extern)
+volatile
+void* __ppe42_debug_arg;
+
+#endif  /* __ASSEMBLER__ */
+
+//  It's hard to be portable and get all of the definitions and headers in the
+//  correct order.  We need to bring in the system IRQ header here.
+
+#ifdef HWMACRO_GPE
+    #include "gpe_irq.h"
+#else
+    #ifdef HWMACRO_STD
+        #include "std_irq.h"
+    #endif
+#endif
+
+/// \page ppe42_irq_macros_page PPE42 PK IRQ Assembler Macros
+///
+///
+
+#ifndef __ASSEMBLER__
+
+
+///  This structure holds the interrupt handler routine addresses and private
+///  data.  Assembler code assumes the given structure layout, so any changes
+///  to this structure will need to be reflected down into the interrupt
+///  dispatch assembler code.
+
+typedef struct
+{
+    PkIrqHandler handler;
+    void*         arg;
+} Ppe42IrqHandler;
+
+
+#ifdef STATIC_IRQ_TABLE
+
+#define IRQ_HANDLER(func, arg) \
+    {func, arg},
+
+#define IRQ_HANDLER_DEFAULT \
+    {__ppe42_default_irq_handler, 0},
+
+#define EXTERNAL_IRQ_TABLE_END \
+    {__ppe42_phantom_irq_handler, 0}\
+    };
+
+#define EXTERNAL_IRQ_TABLE_START \
+    Ppe42IrqHandler __ppe42_irq_handlers[EXTERNAL_IRQS + 1] __attribute__((section (".sdata"))) = \
+            {
+
+#else
+
+#define EXTERNAL_IRQ_TABLE_START
+
+#define IRQ_HANDLER(func, arg)
+
+#define IRQ_HANDLER_DEFAULT
+
+#define EXTERNAL_IRQ_TABLE_END
+
+#endif /*STATIC_IRQ_TABLE*/
+
+/// Interrupt handlers for real (implemented interrupts) plus one for the phantom interrupt handler
+extern Ppe42IrqHandler __ppe42_irq_handlers[EXTERNAL_IRQS + 1] __attribute__((section (".sdata")));
+
+
+/// The 'phantom interrupt' handler
+///
+/// A 'phantom' interrupt occurs when the interrupt handling code in the
+/// kernel is entered, but no interrupt is found pending in the controller.
+/// This is considered a serious bug, as it indictates a short window
+/// condition where a level-sensitive interrupt has been asserted and then
+/// quickly deasserted before it can be handled.
+
+UNLESS__PPE42_IRQ_CORE_C__(extern)
+Ppe42IrqHandler __ppe42_phantom_irq;
+
+#endif  /* __ASSEMBLER__ */
+
+#endif  /* __PPE42_IRQ_H__ */
---
+++ src/import/chips/p10/common/ppe/pk/ppe42/ppe42_irq_init.c
@@ -0,0 +1,81 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/common/ppe/pk/ppe42/ppe42_irq_init.c $   */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2018,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//-----------------------------------------------------------------------------
+// *! (C) Copyright International Business Machines Corp. 2014
+// *! All Rights Reserved -- Property of IBM
+// *! *** IBM Confidential ***
+//-----------------------------------------------------------------------------
+
+/// \file ppe42_irq_init.c
+/// \brief PPE42 IRQ initialization routines
+///
+/// The entry points in this file are routines that are typically used during
+/// initialization, and their code space could be deallocated and recovered if
+/// no longer needed by the application after initialization.
+
+#include "pk.h"
+
+/// Set up a PPE42 Fixed Interval Timer (FIT) handler
+///
+/// See the PK specification for full details on setting up a FIT handler.
+///
+/// Return values other then PK_OK (0) are errors; see \ref pk_errors
+///
+/// \retval 0 Successful completion
+///
+
+int
+ppe42_fit_setup(PkIrqHandler handler, void* arg)
+{
+    PkMachineContext ctx;
+    Ppe42TCR tcr;
+
+    pk_critical_section_enter(&ctx);
+
+    tcr.value = mfspr(SPRN_TCR);
+
+    if (handler)
+    {
+
+        tcr.fields.fp  = 0;
+        tcr.fields.fie = 1;
+
+        __ppe42_fit_routine = handler;
+        __ppe42_fit_arg = arg;
+
+    }
+    else
+    {
+
+        tcr.fields.fie = 0;
+    }
+
+    mtspr(SPRN_TCR, tcr.value);
+    sync();
+
+    pk_critical_section_exit(&ctx);
+
+    return PK_OK;
+}
