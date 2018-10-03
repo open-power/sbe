a273362351bf5ec03ccb25ebf1b66b94cbcfd645
Raja Das <rajadas2@in.ibm.com>
2018-10-02 23:29:19 -0500

    Added newline termination after putting in build info into Hash
    At the top of the hash generated file, the file/build info is
    getting appended with the first hash line. Terminated the build
    info with a newline termination.
    
    Change-Id: Id002f2591e166f73703cb5a7121da1a7ac33a365
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/66905
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>

--- src/tools/trace/ppetracepp.C
+++ src/tools/trace/ppetracepp.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER sbe Project                                                  */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2016                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2018                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -921,7 +921,7 @@ if (!hashtab.empty())
     time_t tt = time(NULL);
     sprintf(buf, "%s", asctime(localtime(&tt)));
     buf[strlen(buf)-1] = '\0'; // chop off extra newline
-	fprintf(TRC, "#FSP_TRACE_v2|||%s|||BUILD:%.120s", buf, pwd.c_str());
+	fprintf(TRC, "#FSP_TRACE_v2|||%s|||BUILD:%.120s\n", buf, pwd.c_str());
 
 	string srch_str = "||";
 	srch_str += source;
