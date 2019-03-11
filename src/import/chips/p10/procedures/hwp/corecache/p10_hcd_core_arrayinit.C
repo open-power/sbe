ec389e4158fb67e8629722d9dfc4527c3b6acdfd
Yue Du <daviddu@us.ibm.com>
2019-03-10 23:05:09 -0500

    CORECACHE: IStep4 HWP Level 1 Delivery
    Change-Id: Iad9d79ba35cc2a04181d7f7b3fabc000fb02d5f2
    Original-Change-Id: If454c005b661c46025dca97e840fd39f0cecb4f6
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/73111
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_core_arrayinit.C
@@ -0,0 +1,69 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/corecache/p10_hcd_core_arrayinit.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+
+
+///
+/// @file  p10_hcd_core_arrayinit.C
+/// @brief
+///
+
+
+// *HWP HWP Owner          : David Du         <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still       <stillgs@us.ibm.com>
+// *HWP FW Owner           : Prem Shanker Jha <premjha2@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : SBE:QME
+// *HWP Level              : 1
+
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include "p10_hcd_core_arrayinit.H"
+
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+// Procedure: p10_hcd_core_arrayinit
+//------------------------------------------------------------------------------
+
+
+fapi2::ReturnCode
+p10_hcd_core_arrayinit(
+    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST > & i_target)
+{
+    FAPI_INF(">>p10_hcd_core_arrayinit");
+
+
+
+    FAPI_INF("<<p10_hcd_core_arrayinit");
+    return fapi2::current_err;
+}
