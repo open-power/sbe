3c4a1a55e8756ea4a7745e8c7ce8747facd0c9c7
Yue Du <daviddu@us.ibm.com>
2019-03-13 21:02:31 -0500

    CORECACHE: startclocks and stopclocks
    - Add multicast groups to wrappers
    - Remove polling of CLOCK_STAT_[SL/NSL/ARY] registers as they presently don't
      change values on 16u01.  Applicable only the QME (SIMICS) for now.
    
    Change-Id: I64636fd3bfce657a5fa6b432114015a81c5a0a90
    Original-Change-Id: I497be977b256ad70f91fea1ea6ebb5f8c402623c
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/73296
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/corecache/p10_hcd_cache_startclocks.C
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_cache_startclocks.C
@@ -36,7 +36,7 @@
 // *HWP FW Owner           : Prem Shanker Jha <premjha2@in.ibm.com>
 // *HWP Team               : PM
 // *HWP Consumed by        : SBE:QME
-// *HWP Level              : 1
+// *HWP Level              : 2
 
 
 //------------------------------------------------------------------------------
@@ -44,11 +44,20 @@
 //------------------------------------------------------------------------------
 
 #include "p10_hcd_cache_startclocks.H"
-
+#include "p10_hcd_corecache_clock_control.H"
+#include "p10_hcd_common.H"
 
 //------------------------------------------------------------------------------
 // Constant Definitions
 //------------------------------------------------------------------------------
+
+enum P10_HCD_CACHE_STARTCLOCKS_CONSTANTS
+{
+    HCD_L3_CLK_SYNC_DONE_POLL_TIMEOUT_HW_NS        = 100000,  // 10^5ns = 100us timeout
+    HCD_L3_CLK_SYNC_DONE_POLL_DELAY_HW_NS          = 1000,    // 1us poll loop delay
+    HCD_L3_CLK_SYNC_DONE_POLL_DELAY_SIM_CYCLE      = 32000,   // 32k sim cycle delay
+};
+
 
 
 //------------------------------------------------------------------------------
@@ -58,12 +67,64 @@
 
 fapi2::ReturnCode
 p10_hcd_cache_startclocks(
-    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST > & i_target)
+    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > & i_target,
+    uint32_t i_regions)
 {
+// @todo RTC 207921
+//     fapi2::Target < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > eq_target =
+//         i_target.getParent < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST > ();
+    fapi2::Target < fapi2::TARGET_TYPE_EQ > eq_target =
+        i_target.getParent < fapi2::TARGET_TYPE_EQ > ();
+
+    fapi2::buffer<uint64_t> l_data64  = 0;
+#ifndef EQ_SKEW_ADJUST_DISABLE
+    uint32_t                l_timeout = 0;
+#endif
     FAPI_INF(">>p10_hcd_cache_startclocks");
 
+    FAPI_DBG("Enable L3 Skewadjust via CPMS_CGCSR_[0:L3_CLK_SYNC_DONE_ENABLE]");
+    FAPI_TRY( putScom( i_target, G_QME_CPMS_CGCSR_OR, MASK_SET(0) ) );
 
+#ifndef EQ_SKEW_ADJUST_DISABLE
+    FAPI_DBG("Check L3 Skewadjust Sync Done via CPMS_CGCSR[32:L3_CLK_SYNC_DONE_DONE]");
+    l_timeout = HCD_L3_CLK_SYNC_DONE_POLL_TIMEOUT_HW_NS /
+                HCD_L3_CLK_SYNC_DONE_POLL_DELAY_HW_NS;
+
+    do
+    {
+        FAPI_TRY( getScom( i_target, G_QME_CPMS_CGCSR, l_data64 ) );
+
+        //use multicastAND to check 1
+        if( DATA_GET(32) == 1 )
+        {
+            break;
+        }
+
+        fapi2::delay(HCD_L3_CLK_SYNC_DONE_POLL_DELAY_HW_NS,
+                     HCD_L3_CLK_SYNC_DONE_POLL_DELAY_SIM_CYCLE);
+    }
+    while( (--l_timeout) != 0 );
+
+    FAPI_ASSERT((l_timeout != 0),
+                fapi2::L3_CLK_SYNC_DONE_TIMEOUT()
+                .set_L3_CLK_SYNC_DONE_POLL_TIMEOUT_HW_NS(HCD_L3_CLK_SYNC_DONE_POLL_TIMEOUT_HW_NS)
+                .set_CPMS_CGCSR(l_data64)
+                .set_CORE_TARGET(i_target),
+                "L3 Clock Sync Done Timeout");
+#endif
+
+    FAPI_TRY( p10_hcd_corecache_clock_control(eq_target, i_regions, HCD_CLK_START ) );
+
+    FAPI_DBG("Disable L3 Regional Fences via CPLT_CTRL1[9-12:L3_FENCES]");
+    FAPI_TRY( putScom( eq_target, G_CPLT_CTRL1_CLR, MASK_H32(i_regions) ) );
+
+    FAPI_DBG("Enable L3 Regional PSCOMs via CPLT_CTRL3[9-12:L3_REGIONS]");
+    FAPI_TRY( putScom( eq_target, G_CPLT_CTRL3_OR, MASK_H32(i_regions) ) );
+
+fapi_try_exit:
 
     FAPI_INF("<<p10_hcd_cache_startclocks");
+
     return fapi2::current_err;
+
 }
