4053e68253272498b66f42c624d638c09a93ff03
Yue Du <daviddu@us.ibm.com>
2019-03-13 21:02:31 -0500

    CORECACHE: startclocks and stopclocks
    - Add multicast groups to wrappers
    - Remove polling of CLOCK_STAT_[SL/NSL/ARY] registers as they presently don't
      change values on 16u01.  Applicable only the QME (SIMICS) for now.
    
    Change-Id: I26998495a58baf65e8fd5634682b289fc00aa27a
    Original-Change-Id: I497be977b256ad70f91fea1ea6ebb5f8c402623c
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/73296
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/corecache/p10_hcd_core_startclocks.C
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_core_startclocks.C
@@ -36,7 +36,7 @@
 // *HWP FW Owner           : Prem Shanker Jha <premjha2@in.ibm.com>
 // *HWP Team               : PM
 // *HWP Consumed by        : SBE:QME
-// *HWP Level              : 1
+// *HWP Level              : 2
 
 
 //------------------------------------------------------------------------------
@@ -44,26 +44,86 @@
 //------------------------------------------------------------------------------
 
 #include "p10_hcd_core_startclocks.H"
-
+#include "p10_hcd_corecache_clock_control.H"
+#include "p10_hcd_common.H"
 
 //------------------------------------------------------------------------------
 // Constant Definitions
 //------------------------------------------------------------------------------
 
+enum P10_HCD_CORE_STARTCLOCKS_CONSTANTS
+{
+    HCD_ECL2_CLK_SYNC_DONE_POLL_TIMEOUT_HW_NS        = 100000, // 10^5ns = 100us timeout
+    HCD_ECL2_CLK_SYNC_DONE_POLL_DELAY_HW_NS          = 1000,   // 1us poll loop delay
+    HCD_ECL2_CLK_SYNC_DONE_POLL_DELAY_SIM_CYCLE      = 32000,  // 32k sim cycle delay
+};
 
 //------------------------------------------------------------------------------
 // Procedure: p10_hcd_core_startclocks
 //------------------------------------------------------------------------------
 
-
 fapi2::ReturnCode
 p10_hcd_core_startclocks(
-    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST > & i_target)
+    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > & i_target,
+    uint32_t i_regions)
 {
+// @todo RTC 207921
+//     fapi2::Target < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > eq_target =
+//         i_target.getParent < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST > ();
+    fapi2::Target < fapi2::TARGET_TYPE_EQ > eq_target =
+        i_target.getParent < fapi2::TARGET_TYPE_EQ > ();
+
+    fapi2::buffer<uint64_t> l_data64  = 0;
+
     FAPI_INF(">>p10_hcd_core_startclocks");
 
+    //TODO RTC 207122
+    //Joachim to provide fapi2 means of read regionSelect from target instead of parameter
+    //same applies to all procedures.
+#ifndef EQ_SKEW_ADJUST_DISABLE
+    uint32_t l_timeout = 0;
+    FAPI_DBG("Enable ECL2 Skewadjust via CPMS_CGCSR[1:CL2_CLK_SYNC_DONE_ENABLE]");
+    FAPI_TRY( putScom( i_target, G_QME_CPMS_CGCSR_OR, MASK_SET(1) ) );
 
+    FAPI_DBG("Check ECL2 Skewadjust Sync Done via CPMS_CGCSR[33:CL2_CLK_SYNC_DONE_DONE]");
+    l_timeout = HCD_ECL2_CLK_SYNC_DONE_POLL_TIMEOUT_HW_NS /
+                HCD_ECL2_CLK_SYNC_DONE_POLL_DELAY_HW_NS;
+
+    do
+    {
+        FAPI_TRY( getScom( i_target, G_QME_CPMS_CGCSR, l_data64 ) );
+
+        // use multicastAND to check 1
+        if( DATA_GET(33) == 1 )
+        {
+            break;
+        }
+
+        fapi2::delay(HCD_ECL2_CLK_SYNC_DONE_POLL_DELAY_HW_NS,
+                     HCD_ECL2_CLK_SYNC_DONE_POLL_DELAY_SIM_CYCLE);
+    }
+    while( (--l_timeout) != 0 );
+
+    FAPI_ASSERT((l_timeout != 0),
+                fapi2::ECL2_CLK_SYNC_DONE_TIMEOUT()
+                .set_ECL2_CLK_SYNC_DONE_POLL_TIMEOUT_HW_NS(HCD_ECL2_CLK_SYNC_DONE_POLL_TIMEOUT_HW_NS)
+                .set_CPMS_CGCSR(l_data64)
+                .set_CORE_TARGET(i_target),
+                "ECL2 Clock Sync Done Timeout");
+#endif
+
+    FAPI_TRY( p10_hcd_corecache_clock_control(eq_target, i_regions, HCD_CLK_START ) );
+
+    FAPI_DBG("Disable ECL2 Regional Fences via CPLT_CTRL1[5-8:ECL2_FENCES]");
+    FAPI_TRY( putScom( eq_target, G_CPLT_CTRL1_CLR, MASK_H32(i_regions) ) );
+
+    FAPI_DBG("Enable ECL2 Regional PSCOMs via CPLT_CTRL3[5-8:ECL2_REGIONS]");
+    FAPI_TRY( putScom( eq_target, G_CPLT_CTRL3_OR, MASK_H32(i_regions) ) );
+
+fapi_try_exit:
 
     FAPI_INF("<<p10_hcd_core_startclocks");
+
     return fapi2::current_err;
+
 }
