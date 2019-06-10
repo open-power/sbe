3805d3d5c7b350cdd3751390ca476ca770eb3a79
Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
2019-06-10 06:09:12 -0400

    P10_sbe_tp_switch_gears updates
    * Read and compare magic number,
    * Setup bit-rate divisor and delay into SPI masters
    
    Change-Id: I3d2d83573d28ca86c5c7a89c8f7483973df45eb1
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/78634
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Reviewed-by: DEEPA NANDANAN <dnandana@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/78636
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/import/chips/p10/procedures/hwp/perv/p10_sbe_tp_switch_gears.C
+++ src/import/chips/p10/procedures/hwp/perv/p10_sbe_tp_switch_gears.C
@@ -39,20 +39,18 @@
 #include <p9_perv_scom_addresses.H>
 #include <p9_perv_scom_addresses_fld.H>
 
+#define SEEPROM_START 0xFF800000
 
 enum P10_SBE_TP_SWITCH_GEARS_Private_Constants
 {
-    BACKUP_SEEPROM_MAGIC_NUM_ADDRESS = 0xD8A9029000000000, // Magic number value from Backup SEEPROM
-    BUS_STATUS_BUSY_POLL_COUNT = 256,
     MAGIC_NUMBER = 0x584950205345504D,
-    NORMAL_SEEPROM_MAGIC_NUM_ADDRESS = 0xD8A9009000000000, // Magic number value from SEEPROM
     SCAN_RATIO_4TO1 = 0x3
 };
 
 fapi2::ReturnCode p10_sbe_tp_switch_gears(const
         fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
 {
-    fapi2::buffer<uint64_t> l_data64;
+    fapi2::buffer<uint64_t> l_data64, l_read_reg;
     fapi2::buffer<uint64_t> l_opcg_align;
     fapi2::buffer<uint32_t> l_attr_freq_pau_mhz;
     fapi2::buffer<uint16_t> sck_clock_divider;
@@ -71,30 +69,30 @@ fapi2::ReturnCode p10_sbe_tp_switch_gears(const
         sck_clock_divider = ((l_attr_freq_pau_mhz / 40) - 1 );
         FAPI_DBG("sck clock divider calculation : %#018lX", sck_clock_divider);
 
-        FAPI_TRY(fapi2::getScom(i_target_chip, PU_SPIPSS_ADC_STATUS_REG, l_data64));
+        FAPI_TRY(fapi2::getScom(i_target_chip, 0x000C0003, l_data64));
         l_data64.insert< 0, 12, 4 >(sck_clock_divider);
         l_data64.insertFromRight< 12, 4 >(0x4);
-        FAPI_TRY(fapi2::putScom(i_target_chip, PU_SPIPSS_ADC_STATUS_REG, l_data64));
+        FAPI_TRY(fapi2::putScom(i_target_chip, 0x000C0003, l_data64));
 
-        FAPI_TRY(fapi2::getScom(i_target_chip, PU_SPIPSS_ADC_RDATA_REG3, l_data64));
+        FAPI_TRY(fapi2::getScom(i_target_chip, 0x000C0023, l_data64));
         l_data64.insert< 0, 12, 4 >(sck_clock_divider);
         l_data64.insertFromRight< 12, 4 >(0x4);
-        FAPI_TRY(fapi2::putScom(i_target_chip, PU_SPIPSS_ADC_RDATA_REG3, l_data64));
+        FAPI_TRY(fapi2::putScom(i_target_chip, 0x000C0023, l_data64));
 
-        FAPI_TRY(fapi2::getScom(i_target_chip, PU_SPIPSS_P2S_STATUS_REG, l_data64));
+        FAPI_TRY(fapi2::getScom(i_target_chip, 0x000C0043, l_data64));
         l_data64.insert< 0, 12, 4 >(sck_clock_divider);
         l_data64.insertFromRight< 12, 4 >(0x4);
-        FAPI_TRY(fapi2::putScom(i_target_chip, PU_SPIPSS_P2S_STATUS_REG, l_data64));
+        FAPI_TRY(fapi2::putScom(i_target_chip, 0x000C0043, l_data64));
 
-        FAPI_TRY(fapi2::getScom(i_target_chip, 0x00070063, l_data64));
+        FAPI_TRY(fapi2::getScom(i_target_chip, 0x000C0063, l_data64));
         l_data64.insert< 0, 12, 4 >(sck_clock_divider);
         l_data64.insertFromRight< 12, 4 >(0x4);
-        FAPI_TRY(fapi2::putScom(i_target_chip, 0x00070063, l_data64));
+        FAPI_TRY(fapi2::putScom(i_target_chip, 0x000C0063, l_data64));
 
-        FAPI_TRY(fapi2::getScom(i_target_chip, 0x00070083, l_data64));
+        FAPI_TRY(fapi2::getScom(i_target_chip, 0x000C0083, l_data64));
         l_data64.insert< 0, 12, 4 >(sck_clock_divider);
         l_data64.insertFromRight< 12, 4 >(0x4);
-        FAPI_TRY(fapi2::putScom(i_target_chip, 0x00070083, l_data64));
+        FAPI_TRY(fapi2::putScom(i_target_chip, 0x000C0083, l_data64));
 
         FAPI_DBG("Write new clock divider / delay value back to scratch reg");
         FAPI_TRY(fapi2::getScom(i_target_chip, PERV_SCRATCH_REGISTER_2_SCOM, l_data64));
@@ -102,9 +100,11 @@ fapi2::ReturnCode p10_sbe_tp_switch_gears(const
         l_data64.insertFromRight< 12, 4 >(0x4);
         FAPI_TRY(fapi2::putScom(i_target_chip, PERV_SCRATCH_REGISTER_2_SCOM, l_data64));
 
+#ifdef __PPE__
         FAPI_DBG("read magic number from seeprom, compare value");
-        // TODO - to read and check magic number - might change, calling p9 check as it is
+        // To read and check magic number
         FAPI_TRY(p10_sbe_tp_switch_gears_check_magicnumber(i_target_chip));
+#endif
 
         // adjust scan ratio
         FAPI_DBG("Adjust scan rate to 4:1");
@@ -132,69 +132,16 @@ fapi2::ReturnCode p10_sbe_tp_switch_gears_check_magicnumber(
     const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
 {
     fapi2::buffer<uint64_t> l_read_reg;
-    fapi2::buffer<uint8_t> l_read_attr = 0;
-    int l_timeout = 0;
-    fapi2::buffer<uint64_t> l_data64;
+
     FAPI_INF("p10_sbe_tp_switch_gears_check_magicnumber: Entering ...");
 
-    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_BACKUP_SEEPROM_SELECT, i_target_chip,
-                           l_read_attr));
-
-    if ( l_read_attr.getBit<7>() == 1 )
-    {
-        FAPI_DBG("Read magic number from Backup SEEPROM");
-        //Setting CONTROL_REGISTER_B register value
-        //PIB.CONTROL_REGISTER_B = BACKUP_SEEPROM_MAGIC_NUM_ADDRESS
-        FAPI_TRY(fapi2::putScom(i_target_chip, PU_CONTROL_REGISTER_B,
-                                BACKUP_SEEPROM_MAGIC_NUM_ADDRESS));
-    }
-    else
-    {
-        FAPI_DBG("Read magic number from SEEPROM");
-        //Setting CONTROL_REGISTER_B register value
-        //PIB.CONTROL_REGISTER_B = NORMAL_SEEPROM_MAGIC_NUM_ADDRESS
-        FAPI_TRY(fapi2::putScom(i_target_chip, PU_CONTROL_REGISTER_B,
-                                NORMAL_SEEPROM_MAGIC_NUM_ADDRESS));
-    }
-
-    FAPI_DBG("Poll for stop command completion");
-    l_timeout = BUS_STATUS_BUSY_POLL_COUNT;
-
-    //UNTIL STATUS_REGISTER_B.BUS_STATUS_BUSY_0 == 0
-    while (l_timeout != 0)
-    {
-        //Getting STATUS_REGISTER_B register value
-        FAPI_TRY(fapi2::getScom(i_target_chip, PU_STATUS_REGISTER_B, l_data64));
-        //bool l_poll_data = PIB.STATUS_REGISTER_B.BUS_STATUS_BUSY_0
-        bool l_poll_data = l_data64.getBit<44>();
-
-        if (l_poll_data == 0)
-        {
-            break;
-        }
-
-        --l_timeout;
-    }
-
-    FAPI_DBG("Loop Count :%d", l_timeout);
-
-    FAPI_ASSERT(l_timeout > 0,
-                fapi2::I2C_BUS_STATUS_BUSY()
-                .set_STATUS_REGISTER_B(l_data64)
-                .set_LOOP_COUNT(BUS_STATUS_BUSY_POLL_COUNT)
-                .set_BACKUP_SEEPROM_ATTR(l_read_attr),
-                "ERROR:BUS_STSTUS_BUSY_0 NOT SET TO 0");
-
-    FAPI_DBG("Reading the value of DATA0TO7_REGISTER_B");
-    //Getting DATA0TO7_REGISTER_B register value
-    FAPI_TRY(fapi2::getScom(i_target_chip, PU_DATA0TO7_REGISTER_B,
-                            l_read_reg)); //l_read_reg = PIB.DATA0TO7_REGISTER_B
+    // Read SEEPROM start address FF800000 for magic number
+    l_read_reg = *reinterpret_cast<volatile uint64_t*>(SEEPROM_START);
 
     FAPI_ASSERT(l_read_reg == MAGIC_NUMBER,
                 fapi2::MAGIC_NUMBER_NOT_VALID()
-                .set_DATA0TO7_REGISTER_B(l_read_reg)
-                .set_MAGIC_NUMBER_VALUE(MAGIC_NUMBER)
-                .set_BACKUP_SEEPROM_ATTR(l_read_attr),
+                .set_SEEPROM_START_ADDR(l_read_reg)
+                .set_MAGIC_NUMBER_VALUE(MAGIC_NUMBER),
                 "ERROR: Magic number not matching");
 
     FAPI_INF("p10_sbe_tp_switch_gears_check_magicnumber: Exiting ...");
