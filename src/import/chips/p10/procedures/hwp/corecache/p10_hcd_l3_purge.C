ce69a9a70a6ea761ee3fc13e6cb1cb6fee040ae7
Yue Du <daviddu@us.ibm.com>
2019-06-24 09:44:00 -0500

    QME: Stop11 Entry
    1) Support Stop11 Entry on QME
    2) Support Fit Timer Inject Stop11 based on flag[STOP11_REQ]
    3) Support RUNN and Contained Mode attributes
       (use delay instead of poll for pfet/rvid/skewadjust)
    4) Support QME Broadside Scan on QME with attribute
    5) minor bug fixes on catchup/abort targets and pls and stop5
    
    Change-Id: Id0e116828b62ac038ee499867b088440dfac78ba
    Original-Change-Id: I896467ec3bd96b6d07cc3e375000c1782fb46728
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/79423
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Prem Shanker Jha <premjha2@in.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/103229
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_l3_purge.C
@@ -0,0 +1,113 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/corecache/p10_hcd_l3_purge.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019,2020                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+
+
+///
+/// @file  p10_hcd_l3_purge.C
+/// @brief Purge the L3 cache
+///
+
+
+// *HWP HWP Owner          : David Du         <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still       <stillgs@us.ibm.com>
+// *HWP FW Owner           : Prem Shanker Jha <premjha2@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : SBE:QME
+// *HWP Level              : 2
+
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include "p10_hcd_l3_purge.H"
+#include "p10_hcd_common.H"
+
+#include "p10_scom_c.H"
+using namespace scomt::c;
+
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+enum P10_HCD_L3_PURGE_CONSTANTS
+{
+    HCD_L3_PURGE_DONE_POLL_TIMEOUT_HW_NS    = 100000, // 10^5ns = 100us timeout
+    HCD_L3_PURGE_DONE_POLL_DELAY_HW_NS      = 1000,   // 1us poll loop delay
+    HCD_L3_PURGE_DONE_POLL_DELAY_SIM_CYCLE  = 32000,  // 32k sim cycle delay
+};
+
+//------------------------------------------------------------------------------
+// Procedure: p10_hcd_l3_purge
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_hcd_l3_purge(
+    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_OR > & i_target)
+{
+    fapi2::buffer<uint64_t> l_scomData = 0;
+    uint32_t                l_timeout  = 0;
+
+    FAPI_INF(">>p10_hcd_l3_purge");
+
+    FAPI_DBG("Assert L3_PURGE_REQ via PM_PURGE_REG[0]");
+    FAPI_TRY( HCD_PUTSCOM_C( i_target, L3_MISC_L3CERRS_PM_PURGE_REG, SCOM_1BIT(0) ) );
+
+    FAPI_DBG("Wait for L3_PURGE_REQ to drop via PM_PURGE_REG[0]");
+    l_timeout = HCD_L3_PURGE_DONE_POLL_TIMEOUT_HW_NS /
+                HCD_L3_PURGE_DONE_POLL_DELAY_HW_NS;
+
+    do
+    {
+
+        FAPI_TRY( HCD_GETSCOM_C( i_target, L3_MISC_L3CERRS_PM_PURGE_REG, l_scomData ) );
+
+        // use multicastOR to check 0
+        if( SCOM_GET(0) == 0 )
+        {
+            break;
+        }
+
+        fapi2::delay(HCD_L3_PURGE_DONE_POLL_DELAY_HW_NS,
+                     HCD_L3_PURGE_DONE_POLL_DELAY_SIM_CYCLE);
+    }
+    while( (--l_timeout) != 0 );
+
+    FAPI_ASSERT((l_timeout != 0),
+                fapi2::L3_PURGE_DONE_TIMEOUT()
+                .set_L3_PURGE_DONE_POLL_TIMEOUT_HW_NS(HCD_L3_PURGE_DONE_POLL_TIMEOUT_HW_NS)
+                .set_PM_PURGE_REG(l_scomData)
+                .set_CORE_TARGET(i_target),
+                "L3 Purge Done Timeout");
+
+fapi_try_exit:
+
+    FAPI_INF("<<p10_hcd_l3_purge");
+
+    return fapi2::current_err;
+
+}
---
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_powerbus_purge.C
@@ -0,0 +1,123 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/corecache/p10_hcd_powerbus_purge.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019,2020                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+
+
+///
+/// @file  p10_hcd_powerbus_purge.C
+/// @brief Purge the PowerBus interface
+///
+
+
+// *HWP HWP Owner          : David Du         <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still       <stillgs@us.ibm.com>
+// *HWP FW Owner           : Prem Shanker Jha <premjha2@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : SBE:QME
+// *HWP Level              : 2
+
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include "p10_hcd_powerbus_purge.H"
+#include "p10_hcd_common.H"
+
+#ifdef __PPE_QME
+    #include "p10_ppe_c.H"
+    using namespace scomt::ppe_c;
+#else
+    #include "p10_scom_c.H"
+    using namespace scomt::c;
+#endif
+
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+enum P10_HCD_POWERBUS_PURGE_CONSTANTS
+{
+    HCD_POWERBUS_PURGE_DONE_POLL_TIMEOUT_HW_NS    = 100000, // 10^5ns = 100us timeout
+    HCD_POWERBUS_PURGE_DONE_POLL_DELAY_HW_NS      = 1000,   // 1us poll loop delay
+    HCD_POWERBUS_PURGE_DONE_POLL_DELAY_SIM_CYCLE  = 32000,  // 32k sim cycle delay
+};
+
+//------------------------------------------------------------------------------
+// Procedure: p10_hcd_powerbus_purge
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_hcd_powerbus_purge(
+    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > & i_target)
+{
+    fapi2::buffer<buffer_t> l_mmioData = 0;
+    uint32_t                l_timeout  = 0;
+    uint32_t                l_powerbus_purge_done = 0;
+
+    FAPI_INF(">>p10_hcd_powerbus_purge");
+
+    FAPI_DBG("Assert PB_PURGE_REQ via PCR_SCSR[12]");
+    FAPI_TRY( HCD_PUTMMIO_C( i_target, QME_SCSR_WO_OR, MMIO_LOAD32H( BIT32(12) ) ) );
+
+    FAPI_DBG("Wait for PB_PURGE_DONE via PCR_SCSR[44]");
+    l_timeout = HCD_POWERBUS_PURGE_DONE_POLL_TIMEOUT_HW_NS /
+                HCD_POWERBUS_PURGE_DONE_POLL_DELAY_HW_NS;
+
+    do
+    {
+
+        FAPI_TRY( HCD_GETMMIO_C( i_target, MMIO_LOWADDR(QME_SCSR), l_mmioData ) );
+
+        // use multicastAND to check 1
+        MMIO_GET32L(l_powerbus_purge_done);
+
+        if( ( l_powerbus_purge_done & BIT64SH(44) ) == BIT64SH(44) )
+        {
+            break;
+        }
+
+        fapi2::delay(HCD_POWERBUS_PURGE_DONE_POLL_DELAY_HW_NS,
+                     HCD_POWERBUS_PURGE_DONE_POLL_DELAY_SIM_CYCLE);
+    }
+    while( (--l_timeout) != 0 );
+
+    FAPI_ASSERT((l_timeout != 0),
+                fapi2::POWERBUS_PURGE_DONE_TIMEOUT()
+                .set_POWERBUS_PURGE_DONE_POLL_TIMEOUT_HW_NS(HCD_POWERBUS_PURGE_DONE_POLL_TIMEOUT_HW_NS)
+                .set_QME_SCSR(l_mmioData)
+                .set_CORE_TARGET(i_target),
+                "PowerBus Purge Done Timeout");
+
+    // Note: Do not drop Powerbus Purge until L3 becomes available again.
+
+fapi_try_exit:
+
+    FAPI_INF("<<p10_hcd_powerbus_purge");
+
+    return fapi2::current_err;
+
+}
