9c3ed681fdaa11e05393e3a00b0d7edde6ceb31e
Douglas Holtsinger <Douglas.Holtsinger@ibm.com>
2019-07-08 11:32:06 -0400

    p10_sbe_core_spr_setup
    Implemented SPR setup of URMOR/HRMOR and PSSCR
    
    Change-Id: I431d50d807396a448b5e434b1b3c28d1b38384f7
    RTC:209629
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/79958
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/79962
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/import/chips/p10/procedures/hwp/core/p10_sbe_core_spr_setup.C
+++ src/import/chips/p10/procedures/hwp/core/p10_sbe_core_spr_setup.C
@@ -28,7 +28,7 @@
 ///
 /// @brief Initializes core SPRs prior to thread execution start
 ///
-/// *HWP HW Maintainer: Nicholas Landi <nlandi@ibm.com>
+/// *HWP HW Maintainer: Douglas Holtsinger <Douglas.Holtsinger@ibm.com>
 /// *HWP FW Maintainer: Raja Das <rajadas2@in.ibm.com>
 /// *HWP Consumed by  : SBE
 ///----------------------------------------------------------------------------
@@ -38,6 +38,22 @@
 //------------------------------------------------------------------------------
 #include <p10_sbe_core_spr_setup.H>
 #include <p10_scom_c.H>
+#include <p10_ram_core.H>
+
+// RAM constants
+const uint8_t THREAD_0 = 0;
+const uint8_t THREAD_1 = 1;
+const uint8_t THREAD_2 = 2;
+const uint8_t THREAD_3 = 3;
+const uint32_t MSR_SPR_NUMBER   = 2001;
+const uint32_t PSSCR_SPR_NUMBER = 855;
+const uint64_t HOSTBOOT_PSSCR_VALUE = 0x00000000003F00FFULL;
+
+// MSR Secure / S bit
+//    0 -> The thread is not in Secure state. It may not access Secure memory. The thread is not in ultravisor state.
+//    1 -> The thread is in Secure state. If MSRHV=1 and MSRPR=0, the thread is in ultravisor state; otherwise the
+//    value does not affect privilege.
+const uint8_t MSR_S_BITPOS = 41;
 
 //------------------------------------------------------------------------------
 // Function definitions
@@ -53,17 +69,18 @@
 /// @param[in]  i_fused_core_mode Boolean indicator of fused core configuration
 ///                               status (true=fused, false=normal)
 /// @param[out] o_core_targets    Set of master core targets to process
+/// @param[out] o_master_core_num Master core number
 /// @return fapi::ReturnCode. FAPI2_RC_SUCCESS if success, else error code.
 ///
 fapi2::ReturnCode p10_sbe_core_spr_setup_select_cores(
     const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target,
     bool i_fused_core_mode,
-    std::vector<fapi2::Target<fapi2::TARGET_TYPE_CORE>>& o_core_targets)
+    std::vector<fapi2::Target<fapi2::TARGET_TYPE_CORE>>& o_core_targets,
+    fapi2::ATTR_MASTER_CORE_Type& o_master_core_num)
 {
     FAPI_DBG("Entering...");
 
-    fapi2::ATTR_MASTER_CORE_Type l_master_core_num;
-    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_MASTER_CORE, i_target, l_master_core_num),
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_MASTER_CORE, i_target, o_master_core_num),
              "Error from FAPI_ATTR_GET (ATTR_MASTER_CORE)");
 
     for (auto l_core_target : i_target.getChildren<fapi2::TARGET_TYPE_CORE>())
@@ -72,7 +89,7 @@ fapi2::ReturnCode p10_sbe_core_spr_setup_select_cores(
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_core_target, l_core_num),
                  "Error from FAPI_ATTR_GET (ATTR_CHIP_UNIT_POS)");
 
-        if (l_core_num == l_master_core_num)
+        if (l_core_num == o_master_core_num)
         {
             o_core_targets.push_back(l_core_target);
             break;
@@ -82,14 +99,14 @@ fapi2::ReturnCode p10_sbe_core_spr_setup_select_cores(
     FAPI_ASSERT(o_core_targets.size() == 1,
                 fapi2::P10_SBE_CORE_SPR_SETUP_MASTER_CORE_NOT_FOUND()
                 .set_CHIP_TARGET(i_target)
-                .set_MASTER_CORE_NUM(l_master_core_num)
+                .set_MASTER_CORE_NUM(o_master_core_num)
                 .set_FUSED_MODE(i_fused_core_mode),
                 "Error finding the master core target!");
 
     if (i_fused_core_mode)
     {
         fapi2::ATTR_MASTER_CORE_Type l_partner_core_num =
-            (l_master_core_num % 2) ? (l_master_core_num - 1) : (l_master_core_num + 1);
+            (o_master_core_num % 2) ? (o_master_core_num - 1) : (o_master_core_num + 1);
 
         for (auto l_core_target : i_target.getChildren<fapi2::TARGET_TYPE_CORE>())
         {
@@ -107,7 +124,7 @@ fapi2::ReturnCode p10_sbe_core_spr_setup_select_cores(
         FAPI_ASSERT(o_core_targets.size() == 2,
                     fapi2::P10_SBE_CORE_SPR_SETUP_MASTER_FUSED_CORE_PARTNER_NOT_FOUND()
                     .set_CHIP_TARGET(i_target)
-                    .set_MASTER_CORE_NUM(l_master_core_num)
+                    .set_MASTER_CORE_NUM(o_master_core_num)
                     .set_FUSED_MODE(i_fused_core_mode),
                     "Error finding the master fused core partner target!");
     }
@@ -119,27 +136,95 @@ fapi_try_exit:
 }
 
 ///
-/// @brief Program SPRs on set of master core targets
+/// @brief Program SPRs on set of master core targets.
 ///
-/// @param[in]  i_targets  Set of core targets to initialize
+/// @param[in]  i_core_targets    -- Set of core targets to initialize.
+/// @param[in]  i_master_core_num -- Master core number
 ///
 /// @return fapi::ReturnCode. FAPI2_RC_SUCCESS if success, else error code.
 ///
 fapi2::ReturnCode p10_sbe_core_spr_setup_program_sprs(
-    const std::vector<fapi2::Target<fapi2::TARGET_TYPE_CORE>>& i_core_targets)
+    const std::vector<fapi2::Target<fapi2::TARGET_TYPE_CORE>>& i_core_targets,
+    const fapi2::ATTR_MASTER_CORE_Type& i_master_core_num)
 {
     using namespace scomt::c;
+    uint64_t l_master_hrmor = 0;
+    uint64_t l_bootloader_offset = 0;
+    const fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> FAPI_SYSTEM;
+    fapi2::buffer<uint64_t> l_scsr_or = 0;
 
     FAPI_DBG("Entering...");
 
-    // assert PM_EXIT via SCSR WOR address
-    fapi2::buffer<uint64_t> l_scsr_or = 0;
     l_scsr_or.setBit<QME_SCSR_ASSERT_PM_EXIT>();
+
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_SBE_BOOTLOADER_OFFSET,
+                           FAPI_SYSTEM,
+                           l_bootloader_offset),
+             "Error from FAPI_ATTR_GET (ATTR_SBE_BOOTLOADER_OFFSET)");
+
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_SBE_MASTER_HRMOR_ADDRESS,
+                           FAPI_SYSTEM,
+                           l_master_hrmor),
+             "Error from FAPI_ATTR_GET (ATTR_SBE_MASTER_HRMOR_ADDRESS)");
 
     for (auto l_core_target : i_core_targets)
     {
+
+        RamCore l_ram_t1(l_core_target, THREAD_1);
+        RamCore l_ram_t2(l_core_target, THREAD_2);
+        RamCore l_ram_t3(l_core_target, THREAD_3);
+
+        fapi2::buffer<uint64_t> l_data = l_master_hrmor;
+        fapi2::buffer<uint64_t> l_msr = 0;
+
+        uint8_t l_core_unit_pos = 0;
+
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS,
+                               l_core_target,
+                               l_core_unit_pos),
+                 "Error from FAPI_ATTR_GET (ATTR_CHIP_UNIT_POS)!");
+
+        if (l_core_unit_pos != i_master_core_num)
+        {
+            l_data -= l_bootloader_offset;
+        }
+
+        // assert PM_EXIT via SCSR WOR address
         FAPI_TRY(fapi2::putScom(l_core_target, QME_SCSR_SCOM2, l_scsr_or),
                  "Error from putScom (QME_SCSR_SCOM2)");
+
+        // get MSR to determine if need to set URMOR
+        FAPI_TRY(l_ram_t2.get_reg(REG_SPR, MSR_SPR_NUMBER, &l_msr),
+                 "Error ramming MSR (T2)!");
+
+        // set URMOR to the HRMOR value if SMF is enabled
+        if(l_msr.getBit <MSR_S_BITPOS>()) // SMF is on
+        {
+            PREP_QME_RMOR(l_core_target);
+            FAPI_TRY(PUT_QME_RMOR(l_core_target,
+                                  l_data.setBit<QME_RMOR_URMOR_SELECT>()),
+                     "Error during PUT_QME_RMOR for URMOR");
+            FAPI_DBG("Wrote URMOR with 0x%016lX", (uint64_t)l_data );
+        }
+
+        // set HRMOR -- core level SPR
+        // Must not set bit 12 in HRMOR.  Only applies to URMOR.
+        l_data.clearBit<12>();
+        FAPI_TRY(PUT_QME_RMOR(l_core_target,
+                              l_data.clearBit<QME_RMOR_URMOR_SELECT>()),
+                 "Error during PUT_QME_RMOR for HRMOR");
+        FAPI_DBG("Wrote HRMOR with 0x%016lX", (uint64_t)l_data);
+
+        // set PSSCR via thread specific instances
+        l_data = HOSTBOOT_PSSCR_VALUE;
+        FAPI_DBG("PSSCR with 0x%16llX", (uint64_t)l_data);
+        FAPI_TRY(l_ram_t1.put_reg(REG_SPR, PSSCR_SPR_NUMBER, &l_data),
+                 "Error ramming PSSCR (T1)!");
+        FAPI_TRY(l_ram_t2.put_reg(REG_SPR, PSSCR_SPR_NUMBER, &l_data),
+                 "Error ramming PSSCR (T2)!");
+        FAPI_TRY(l_ram_t3.put_reg(REG_SPR, PSSCR_SPR_NUMBER, &l_data),
+                 "Error ramming PSSCR (T3)!");
+
     }
 
 fapi_try_exit:
@@ -157,9 +242,10 @@ fapi2::ReturnCode p10_sbe_core_spr_setup(
     fapi2::ATTR_PROC_SBE_MASTER_CHIP_Type l_is_master_sbe;
     fapi2::ATTR_FUSED_CORE_MODE_Type l_fused_core_mode;
     std::vector<fapi2::Target<fapi2::TARGET_TYPE_CORE>> l_core_targets;
+    fapi2::ATTR_MASTER_CORE_Type l_master_core_num;
 
     FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_SBE_MASTER_CHIP, i_target, l_is_master_sbe),
-             "Error from FAPI_ATTR_GET (ATTR_RPROC_SBE_MASTER_CHIP)");
+             "Error from FAPI_ATTR_GET (ATTR_PROC_SBE_MASTER_CHIP)");
     FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_FUSED_CORE_MODE, fapi2::Target<fapi2::TARGET_TYPE_SYSTEM>(), l_fused_core_mode),
              "Error from FAPI_ATTR_GET (ATTR_FUSED_CORE_MODE)");
 
@@ -170,10 +256,11 @@ fapi2::ReturnCode p10_sbe_core_spr_setup(
 
     FAPI_TRY(p10_sbe_core_spr_setup_select_cores(i_target,
              l_fused_core_mode,
-             l_core_targets),
+             l_core_targets,
+             l_master_core_num),
              "Error from p10_sbe_core_spr_setup_select_cores");
 
-    FAPI_TRY(p10_sbe_core_spr_setup_program_sprs(l_core_targets),
+    FAPI_TRY(p10_sbe_core_spr_setup_program_sprs(l_core_targets, l_master_core_num),
              "Error from p10_sbe_core_spr_setup_program_sprs");
 
 fapi_try_exit:
