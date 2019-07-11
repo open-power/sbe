54eb7072a01937ea8f35e8e4c9d21e1bd19ee549
Ben Gass <bgass@us.ibm.com>
2019-07-11 19:13:18 -0400

    ridi enable updates
    - add p10_enable_ridi function to p10_perv_sbe_cmn
    - add p10_chiplet_enable_ridi to enable MC/PCIe
    - add p10_iosh_enable_ridi to enable IOHS
    - update p10_sbe_nest_enable_ridi to use p10_enable_ridi
    
    Change-Id: Ie9eb42a7bc28d334dfc4795d1eead76e0625729a
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/80321
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/80391
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/import/chips/p10/procedures/hwp/perv/p10_sbe_nest_enable_ridi.C
+++ src/import/chips/p10/procedures/hwp/perv/p10_sbe_nest_enable_ridi.C
@@ -37,9 +37,7 @@
 #include "p9_perv_scom_addresses.H"
 #include "p9_perv_scom_addresses_fld.H"
 #include <target_filters.H>
-
-static fapi2::ReturnCode p10_sbe_nest_enable_ridi_setup(
-    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet);
+#include <p10_perv_sbe_cmn.H>
 
 static fapi2::ReturnCode p10_sbe_nest_enable_ridi_tp_enable_ridi(const
         fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip);
@@ -61,7 +59,7 @@ fapi2::ReturnCode p10_sbe_nest_enable_ridi(const
 
     for (auto& targ : l_perv_nest)
     {
-        FAPI_TRY(p10_sbe_nest_enable_ridi_setup(targ));
+        FAPI_TRY(p10_enable_ridi(targ));
     }
 
     FAPI_DBG("p10_sbe_nest_enable_ridi: Exiting ...");
@@ -89,37 +87,6 @@ static fapi2::ReturnCode p10_sbe_nest_enable_ridi_tp_enable_ridi(const
     FAPI_TRY(fapi2::putScom(i_target_chip, PERV_ROOT_CTRL1_SET_SCOM, l_data64));
 
     FAPI_INF("p10_sbe_nest_enable_ridi_tp_enable_ridi: Exiting ...");
-
-fapi_try_exit:
-    return fapi2::current_err;
-}
-
-/// @brief Enable Drivers/Recievers of Nest chiplet
-///
-/// @param[in]     i_target_chiplet   Reference to TARGET_TYPE_PERV target
-/// @return  FAPI2_RC_SUCCESS if success, else error code.
-static fapi2::ReturnCode p10_sbe_nest_enable_ridi_setup(
-    const fapi2::Target<fapi2::TARGET_TYPE_PERV>& i_target_chiplet)
-{
-    bool l_read_reg = false;
-    fapi2::buffer<uint64_t> l_data64;
-    FAPI_DBG("p10_sbe_nest_enable_ridi_setup: Entering ...");
-
-    FAPI_INF("Check for chiplet enable");
-    FAPI_TRY(fapi2::getScom(i_target_chiplet, PERV_NET_CTRL0, l_data64));
-    l_read_reg = l_data64.getBit<0>();
-
-    if ( l_read_reg )
-    {
-        FAPI_INF("Enable Recievers, Drivers DI1 & DI2");
-        l_data64.flush<0>();
-        l_data64.setBit<19>();
-        l_data64.setBit<20>();
-        l_data64.setBit<21>();
-        FAPI_TRY(fapi2::putScom(i_target_chiplet, PERV_NET_CTRL0_WOR, l_data64));
-    }
-
-    FAPI_DBG("p10_sbe_nest_enable_ridi_setup: Exiting ...");
 
 fapi_try_exit:
     return fapi2::current_err;
