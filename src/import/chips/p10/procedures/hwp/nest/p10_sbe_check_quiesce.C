2216002997fe1aa9a302bc2d3b5f1a12a1980b0e
rajerpp1 <rajerpp1@in.ibm.com>
2019-10-22 01:17:52 -0500

    Added sub procedure to check quiesce state for NX
    SW477416
    This sub procedure will be called by 'p10_sbe_check_quiesce' procedure.
    
    Unit Test Enviornment:
        Model       : /sfs/star27/p10vbu/p10_e10021_vbu_sc_021u03a_01/
        Checkpoint  : nmz/chkpt/jmcgill_hb_after_istep_s4.chkpt
    Unit Test Result:
        Successfully executed the wrapper function.
    
    Change-Id: Ifaf1344d533090ac51dd5f63273a43ade3b7611d
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/85667
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Thi N Tran <thi@us.ibm.com>
    Reviewed-by: Manish K. Chowdhary <manichow@in.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/95503
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/import/chips/p10/procedures/hwp/nest/p10_sbe_check_quiesce.C
+++ src/import/chips/p10/procedures/hwp/nest/p10_sbe_check_quiesce.C
@@ -39,6 +39,7 @@
 //--------------------------------------------------------------------------
 #include <p10_sbe_check_quiesce.H>
 #include <p10_scom_proc.H>
+#include <p10_scom_nmmu.H>
 
 //-----------------------------------------------------------------------------------
 // Constant definitions
@@ -49,30 +50,204 @@ const uint32_t C_NUM_TRIES_QUIESCE_STATE = 10000;
 const uint32_t C_DELAY_NS_396      = 396000000 / C_NUM_TRIES_QUIESCE_STATE; //396 ms
 const uint32_t C_DELAY_CYCLES_396 = 792000000 / C_NUM_TRIES_QUIESCE_STATE; //2GHz * 396 ms
 
-//--------------------------------------------------------------------------
-//  HWP entry point
-//--------------------------------------------------------------------------
-fapi2::ReturnCode p10_sbe_check_quiesce(
-    const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+using namespace scomt;
+using namespace scomt::proc;
+using namespace scomt::nmmu;
+//---------------------------------------------------------------------------------
+// NOTE: description in header
+//---------------------------------------------------------------------------------
+fapi2::ReturnCode p10_nx_check_quiesce(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
 {
-    FAPI_DBG("p10_sbe_check_quiesce: Entering..");
+    FAPI_DBG("p10_nx_check_quiesce: Entering....");
+    fapi2::buffer<uint64_t> l_dma_status_reg_data(0);
+    fapi2::buffer<uint64_t> l_data(0);
 
-    // SBE will check quiesce state for all units on the powerbus on its chip
-    // TODO (SW477416): Implement HW Procedure for checking the quiesce state of
-    // CAPP, PHB, NPU, VAS and NX.
+    //If (DMA Status Register[HMI Ative])
+    FAPI_TRY(GET_NX_DMA_SU_STATUS(i_target, l_dma_status_reg_data));
 
-    FAPI_TRY(p10_psihb_check_quiesce(i_target), "Error from p10_psihb_check_quiesce");
+    // If HMI has been signaled, wait for power bus interface to go idle.
+    if (GET_NX_DMA_SU_STATUS_HMI_ACTIVE(l_dma_status_reg_data))
+    {
+        for (uint32_t i = 0; i < C_NUM_TRIES_QUIESCE_STATE; i++)
+        {
+            FAPI_TRY(GET_FBC_CQ_WRAP_NXCQ_SCOM_NX_PB_ERR_RPT_0(i_target, l_data));
 
-    // TODO (SW477416): Implement HW Procedure for checking the quiesce state of INTP.
+            if (GET_FBC_CQ_WRAP_NXCQ_SCOM_NX_PB_ERR_RPT_0_NX_PBI_WRITE_IDLE(l_data))
+            {
+                break;
+            }
 
-    //We also need to clean up any active special wakeups, and redirect
-    //special wakeups to the SGPE
-    // TODO (SW477416):
+            FAPI_TRY(fapi2::delay(C_DELAY_NS_396, C_DELAY_CYCLES_396));
+        }
+
+        FAPI_ASSERT(GET_FBC_CQ_WRAP_NXCQ_SCOM_NX_PB_ERR_RPT_0_NX_PBI_WRITE_IDLE(l_data),
+                    fapi2::P10_NX_PBI_WRITE_IDLE_TIMEOUT()
+                    .set_TARGET(i_target)
+                    .set_NX_PB_ERR_RPT_0(l_data), "PBI Write Idle never happened");
+    }
+
+    //Any CRB kill must be complete before issuing the following sequence
+    FAPI_TRY(GET_NX_DMA_SU_CRB_KILL_REQ(i_target, l_data));
+
+    if (GET_NX_DMA_SU_CRB_KILL_REQ_ENABLE(l_data))
+    {
+        //Wait for completing the kill request.
+        for (uint32_t i = 0; i < C_NUM_TRIES_QUIESCE_STATE; i++)
+        {
+            FAPI_TRY(GET_NX_DMA_SU_CRB_KILL_REQ(i_target, l_data));
+
+            if (GET_NX_DMA_SU_CRB_KILL_REQ_DONE(l_data))
+            {
+                break;
+            }
+
+            FAPI_TRY(fapi2::delay(C_DELAY_NS_396, C_DELAY_CYCLES_396));
+        }
+
+        FAPI_ASSERT(GET_NX_DMA_SU_CRB_KILL_REQ_DONE(l_data),
+                    fapi2::P10_NX_CRB_KILL_DONE_TIMEOUT()
+                    .set_TARGET(i_target)
+                    .set_NX_DMA_SU_CRB_KILL_REQ(l_data), "CRB kills were not complete");
+    }
+
+    //Stop UMAC from fetching new CRBs
+    FAPI_TRY(GET_NX_PBI_UMAC_STATUS_CONTROL(i_target, l_data));
+    CLEAR_NX_PBI_UMAC_STATUS_CONTROL_CRB_READS_ENBL(l_data);
+    FAPI_TRY(PUT_NX_PBI_UMAC_STATUS_CONTROL(i_target, l_data));
+
+    for (uint32_t i = 0; i < C_NUM_TRIES_QUIESCE_STATE; i++)
+    {
+        FAPI_TRY(GET_NX_PBI_UMAC_STATUS_CONTROL(i_target, l_data));
+
+        if(GET_NX_PBI_UMAC_STATUS_CONTROL_CRB_READS_HALTED(l_data))
+        {
+            break;
+        }
+
+        FAPI_TRY(fapi2::delay(C_DELAY_NS_396, C_DELAY_CYCLES_396));
+    }
+
+    FAPI_ASSERT(GET_NX_PBI_UMAC_STATUS_CONTROL_CRB_READS_HALTED(l_data),
+                fapi2::P10_NX_STOP_UMAC_FETCHING_NEW_CRBS_TIMEOUT()
+                .set_TARGET(i_target)
+                .set_NX_PBI_UMAC_STATUS_CONTROL(l_data),
+                "UMAC was not stopped from fetching new CRBs");
+
+    //Wait for UMAC dispatch slots to drain of CRBs
+    for (uint32_t i = 0; i < C_NUM_TRIES_QUIESCE_STATE; i++)
+    {
+        FAPI_TRY(GET_NX_PBI_UMAC_STATUS_CONTROL(i_target, l_data));
+
+        if(GET_NX_PBI_UMAC_STATUS_CONTROL_UMAC_QUIESCED(l_data))
+        {
+            break;
+        }
+
+        FAPI_TRY(fapi2::delay(C_DELAY_NS_396, C_DELAY_CYCLES_396));
+    }
+
+    FAPI_ASSERT(GET_NX_PBI_UMAC_STATUS_CONTROL_UMAC_QUIESCED(l_data),
+                fapi2::P10_NX_UMAC_DISPATCH_SLOTS_TO_DRAIN_CRBS_TIMEOUT()
+                .set_TARGET(i_target)
+                .set_NX_PBI_UMAC_STATUS_CONTROL(l_data),
+                "UMAC was not done dispatching slots to drain of CRBs");
+
+    //Wait for DMA channels to drain
+    for (uint32_t i = 0; i < C_NUM_TRIES_QUIESCE_STATE; i++)
+    {
+        FAPI_TRY(GET_NX_DMA_SU_STATUS(i_target, l_dma_status_reg_data));
+
+        if(GET_NX_DMA_SU_STATUS_DMA_CH0_IDLE(l_dma_status_reg_data)
+           && GET_NX_DMA_SU_STATUS_DMA_CH1_IDLE(l_dma_status_reg_data)
+           && GET_NX_DMA_SU_STATUS_DMA_CH2_IDLE(l_dma_status_reg_data)
+           && GET_NX_DMA_SU_STATUS_DMA_CH3_IDLE(l_dma_status_reg_data)
+           && GET_NX_DMA_SU_STATUS_DMA_CH4_IDLE(l_dma_status_reg_data))
+        {
+            break;
+        }
+
+        fapi2::delay(C_DELAY_NS_396, C_DELAY_CYCLES_396);
+    }
+
+    FAPI_ASSERT((GET_NX_DMA_SU_STATUS_DMA_CH0_IDLE(l_dma_status_reg_data)
+                 && GET_NX_DMA_SU_STATUS_DMA_CH1_IDLE(l_dma_status_reg_data)
+                 && GET_NX_DMA_SU_STATUS_DMA_CH2_IDLE(l_dma_status_reg_data)
+                 && GET_NX_DMA_SU_STATUS_DMA_CH3_IDLE(l_dma_status_reg_data)
+                 && GET_NX_DMA_SU_STATUS_DMA_CH4_IDLE(l_dma_status_reg_data)),
+                fapi2::P10_WAIT_FOR_DMA_CHANNELS_TO_DRAIN_TIMEOUT()
+                .set_TARGET(i_target)
+                .set_NX_DMA_SU_STATUS(l_dma_status_reg_data),
+                "DMA channels were not drained");
+
+    //Wait for ERAT to be idle. Should be trivially idle because of the above.
+    for (uint32_t i = 0; i < C_NUM_TRIES_QUIESCE_STATE; i++)
+    {
+        FAPI_TRY(GET_NX_PBI_SHIM_ERAT_STATUS_CONTROL(i_target, l_data));
+
+        if (GET_NX_PBI_SHIM_ERAT_STATUS_CONTROL_ERAT_IDLE(l_data))
+        {
+            break;
+        }
+
+        FAPI_TRY(fapi2::delay(C_DELAY_NS_396, C_DELAY_CYCLES_396));
+    }
+
+    FAPI_ASSERT(GET_NX_PBI_SHIM_ERAT_STATUS_CONTROL_ERAT_IDLE(l_data),
+                fapi2::P10_WAIT_FOR_ERAT_IDLE()
+                .set_TARGET(i_target)
+                .set_NX_PBI_SHIM_ERAT_STATUS_CONTROL(l_data),
+                "ERAT was not idle");
+
+    //Wait for PBI master machines to be idle
+    for (uint32_t i = 0; i < C_NUM_TRIES_QUIESCE_STATE; i++)
+    {
+        FAPI_TRY(GET_NX_DMA_SU_STATUS(i_target, l_dma_status_reg_data));
+
+        if (GET_NX_DMA_SU_STATUS_PBI_IDLE(l_dma_status_reg_data))
+        {
+            break;
+        }
+
+        FAPI_TRY(fapi2::delay(C_DELAY_NS_396, C_DELAY_CYCLES_396));
+    }
+
+    FAPI_ASSERT(GET_NX_DMA_SU_STATUS_PBI_IDLE(l_dma_status_reg_data),
+                fapi2::P10_PBI_MASTER_MACHINES_IDLE_TIMEOUT()
+                .set_TARGET(i_target)
+                .set_NX_DMA_SU_STATUS(l_dma_status_reg_data),
+                "PBI Master Machines are not idle");
+
+    // If this procedure is followed, then usually if UMAC Status and
+    // Control Register[Quiesce Request] is written to 1 then hardware will
+    // trivially respond UMAC Status and Control Register[Quiesce Achieved] = 1
+    FAPI_TRY(GET_NX_PBI_UMAC_STATUS_CONTROL(i_target, l_data));
+    SET_NX_PBI_UMAC_STATUS_CONTROL_QUIESCE_REQUEST(l_data);
+    CLEAR_NX_PBI_UMAC_STATUS_CONTROL_QUIESCE_ACHEIVED(l_data);
+    CLEAR_NX_PBI_UMAC_STATUS_CONTROL_QUIESCE_FAILED(l_data);
+    FAPI_TRY(PUT_NX_PBI_UMAC_STATUS_CONTROL(i_target, l_data));
+
+    // Poll UMAC Control register status register
+    for (uint32_t i = 0; i < C_NUM_TRIES_QUIESCE_STATE; i++)
+    {
+        FAPI_TRY(GET_NX_PBI_UMAC_STATUS_CONTROL(i_target, l_data));
+
+        if (!GET_NX_PBI_UMAC_STATUS_CONTROL_QUIESCE_FAILED(l_data))
+        {
+            break;
+        }
+
+        FAPI_TRY(fapi2::delay(C_DELAY_NS_396, C_DELAY_CYCLES_396));
+    }
+
+
+    FAPI_ASSERT(!GET_NX_PBI_UMAC_STATUS_CONTROL_QUIESCE_FAILED(l_data),
+                fapi2::P10_UMAC_QUIESCE_FAILED()
+                .set_TARGET(i_target)
+                .set_NX_PBI_UMAC_STATUS_CONTROL(l_data),
+                "UMAC status control quiesce failed");
 
 fapi_try_exit:
-    // TODO (SW477416): If the quiesce fails then checkstop the system
-
-    FAPI_DBG("p10_sbe_check_quiesce: Exiting..");
+    FAPI_DBG("p10_nx_check_quiesce: Exiting....");
     return fapi2::current_err;
 }
 
@@ -81,9 +256,6 @@ fapi_try_exit:
 //---------------------------------------------------------------------------------
 fapi2::ReturnCode p10_psihb_check_quiesce(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
 {
-    using namespace scomt;
-    using namespace scomt::proc;
-
     FAPI_DBG("p10_psihb_check_quiesce: Entering...");
     fapi2::buffer<uint64_t> l_psihb_data(0);
     const uint32_t c_error_mask_disable_all = 0xFFFul;
@@ -102,6 +274,7 @@ fapi2::ReturnCode p10_psihb_check_quiesce(const fapi2::Target<fapi2::TARGET_TYPE
     FAPI_TRY(PUT_TP_TPBR_PSIHB_ERROR_MASK_REG(i_target, l_psihb_data));
 
     //Poll PSIHBCR bit 20 - inbound queue empty to be 0b0 for quiesce state
+    //Poll PSIHBCR bit 20 - inbound queue empty to be 0b0 for quiesce state
     for (uint32_t i = 0; i < C_NUM_TRIES_QUIESCE_STATE; i++)
     {
         FAPI_TRY(GET_TP_TPBR_PSIHB_STATUS_CTL_REG(i_target, l_psihb_data));
@@ -117,7 +290,7 @@ fapi2::ReturnCode p10_psihb_check_quiesce(const fapi2::Target<fapi2::TARGET_TYPE
     FAPI_ASSERT(!GET_TP_TPBR_PSIHB_STATUS_CTL_REG_FSP_INBOUND_ACTIVE(l_psihb_data),
                 fapi2::P10_PSIHBCR_INBOUND_QUEUE_NOT_EMPTY()
                 .set_TARGET(i_target)
-                .set_DATA(l_psihb_data), "PSIHBCR inbound queue not empty");
+                .set_TPBR_PSIHB_STATUS_CTL_REG(l_psihb_data), "PSIHBCR inbound queue not empty");
 
     // Disable TCE access by clearing the secure boot register - can't be done
     // later because it's a security hole
@@ -127,5 +300,33 @@ fapi2::ReturnCode p10_psihb_check_quiesce(const fapi2::Target<fapi2::TARGET_TYPE
 
 fapi_try_exit:
     FAPI_DBG("p10_psihb_check_quiesce: Exiting...");
+    return fapi2::current_err;
+}
+
+//--------------------------------------------------------------------------
+//  HWP entry point
+//--------------------------------------------------------------------------
+fapi2::ReturnCode p10_sbe_check_quiesce(
+    const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    FAPI_DBG("p10_sbe_check_quiesce: Entering..");
+
+    // SBE will check quiesce state for all units on the powerbus on its chip
+    // TODO (SW477416): Implement HW Procedure for checking the quiesce state of
+    // CAPP, PHB, NPU and VAS.
+
+    FAPI_TRY(p10_nx_check_quiesce(i_target), "Error from p10_nx_check_quiesce");
+    FAPI_TRY(p10_psihb_check_quiesce(i_target), "Error from p10_psihb_check_quiesce");
+
+    // TODO (SW477416): Implement HW Procedure for checking the quiesce state of INTP.
+
+    //We also need to clean up any active special wakeups, and redirect
+    //special wakeups to the SGPE
+    // TODO (SW477416):
+
+fapi_try_exit:
+    // TODO (SW477416): If the quiesce fails then checkstop the system
+
+    FAPI_DBG("p10_sbe_check_quiesce: Exiting..");
     return fapi2::current_err;
 }
