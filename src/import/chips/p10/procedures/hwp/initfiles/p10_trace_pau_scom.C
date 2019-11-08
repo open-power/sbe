c1d953e652df345bee8a5f81ff83567e8856daef
Nicholas Landi <nlandi@ibm.com>
2019-11-08 09:43:06 -0500

    p10_trace initifiles
    This commit serves to add trace scominit files for mirroring into other
    repositories.
    
    Change-Id: I085974c9cde4b8e663de842ce38e40e77baf7cf0
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/86708
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Thi N Tran <thi@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/87009
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/initfiles/p10_trace_pau_scom.C
@@ -0,0 +1,72 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/initfiles/p10_trace_pau_scom.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include "p10_trace_pau_scom.H"
+#include <stdint.h>
+#include <stddef.h>
+#include <fapi2.H>
+
+using namespace fapi2;
+
+constexpr uint64_t literal_0b1 = 0b1;
+
+fapi2::ReturnCode p10_trace_pau_scom(const fapi2::Target<fapi2::TARGET_TYPE_PAUC>& TGT0,
+                                     const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& TGT1)
+{
+    {
+        fapi2::ATTR_EC_Type   l_chip_ec;
+        fapi2::ATTR_NAME_Type l_chip_id;
+        FAPI_TRY(FAPI_ATTR_GET_PRIVILEGED(fapi2::ATTR_NAME, TGT1, l_chip_id));
+        FAPI_TRY(FAPI_ATTR_GET_PRIVILEGED(fapi2::ATTR_EC, TGT1, l_chip_ec));
+        fapi2::buffer<uint64_t> l_scom_buffer;
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x10010402ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<23, 1, 63, uint64_t>(literal_0b1 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x10010402ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x10010442ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<23, 1, 63, uint64_t>(literal_0b1 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x10010442ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x10010482ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<23, 1, 63, uint64_t>(literal_0b1 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x10010482ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x100107c0ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<13, 1, 63, uint64_t>(literal_0b1 );
+            l_scom_buffer.insert<26, 1, 63, uint64_t>(literal_0b1 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x100107c0ull, l_scom_buffer));
+        }
+
+    };
+fapi_try_exit:
+    return fapi2::current_err;
+}
