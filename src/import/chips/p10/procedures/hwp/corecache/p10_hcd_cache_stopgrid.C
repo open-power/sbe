2d5e4227661102f5a827dbccdafe21b24aab2994
Yue Du <daviddu@us.ibm.com>
2019-11-21 11:40:34 -0600

    VBU: EQ_STOPCLOCKS
    Set #3 - Adds EQ clockstops selection to p10_stopclocks and its wrapper.
    Set #6 - Change target to 'eq' when calling p10_hcd_eq_stopclocks.C
             from wrapper and p10_stopclocks.C
    
    Change-Id: Icd4cd51313424d82aefd8184aa614d2e0451a752
    Original-Change-Id: Iff2ced9df21603a23909e3fb7201bdd4e96065d9
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/87586
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Dev-Ready: Brent Wieman <bwieman@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Reviewed-by: Thi N Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/103835
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_cache_stopgrid.C
@@ -0,0 +1,131 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/corecache/p10_hcd_cache_stopgrid.C $ */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019,2020                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+
+
+///
+/// @file  p10_hcd_cache_stopgrid.C
+/// @brief
+///
+
+
+// *HWP HWP Owner          : David Du         <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still       <stillgs@us.ibm.com>
+// *HWP FW Owner           : Prem Shanker Jha <premjha2@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : SBE:QME
+// *HWP Level              : 2
+
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include "p10_hcd_cache_stopgrid.H"
+#include "p10_hcd_common.H"
+
+#ifdef __PPE_QME
+    #include "p10_ppe_c.H"
+    using namespace scomt::ppe_c;
+#else
+    #include "p10_scom_c.H"
+    using namespace scomt::c;
+#endif
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+enum P10_HCD_CACHE_STOPGRID_CONSTANTS
+{
+    HCD_L3_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS        = 10000,   // 10^4ns = 10us timeout
+    HCD_L3_CLK_SYNC_DROP_POLL_DELAY_HW_NS          = 100,     // 100ns poll loop delay
+    HCD_L3_CLK_SYNC_DROP_POLL_DELAY_SIM_CYCLE      = 3200,    // 3.2k sim cycle delay
+};
+
+//------------------------------------------------------------------------------
+// Procedure: p10_hcd_cache_stopgrid
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_hcd_cache_stopgrid(
+    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > & i_target)
+{
+    fapi2::buffer<buffer_t> l_mmioData = 0;
+#ifndef EQ_SKEW_ADJUST_DISABLE
+    uint32_t                l_timeout  = 0;
+    fapi2::Target < fapi2::TARGET_TYPE_SYSTEM > l_sys;
+    fapi2::ATTR_RUNN_MODE_Type                  l_attr_runn_mode;
+    FAPI_TRY( FAPI_ATTR_GET( fapi2::ATTR_RUNN_MODE, l_sys, l_attr_runn_mode ) );
+#endif
+
+    FAPI_INF(">>p10_hcd_cache_stopgrid");
+
+    FAPI_DBG("Disable L3 Skewadjust via CPMS_CGCSR_[0:L3_CLK_SYNC_ENABLE]");
+    FAPI_TRY( HCD_PUTMMIO_C( i_target, CPMS_CGCSR_WO_CLEAR, MMIO_1BIT(0) ) );
+
+#ifndef EQ_SKEW_ADJUST_DISABLE
+
+    FAPI_DBG("Check L3 Skewadjust Removed via CPMS_CGCSR[32:L3_CLK_SYNC_DONE]");
+    l_timeout = HCD_L3_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS /
+                HCD_L3_CLK_SYNC_DROP_POLL_DELAY_HW_NS;
+
+    do
+    {
+        if (!l_attr_runn_mode)
+        {
+            FAPI_TRY( HCD_GETMMIO_C( i_target, MMIO_LOWADDR(CPMS_CGCSR), l_mmioData ) );
+
+            // use multicastOR to check 0
+            if( MMIO_GET(MMIO_LOWBIT(32)) == 0 )
+            {
+                break;
+            }
+        }
+
+        fapi2::delay(HCD_L3_CLK_SYNC_DROP_POLL_DELAY_HW_NS,
+                     HCD_L3_CLK_SYNC_DROP_POLL_DELAY_SIM_CYCLE);
+    }
+    while( (--l_timeout) != 0 );
+
+    FAPI_ASSERT((l_timeout != 0),
+                fapi2::L3_CLK_SYNC_DROP_TIMEOUT()
+                .set_L3_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS(HCD_L3_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS)
+                .set_CPMS_CGCSR(l_mmioData)
+                .set_CORE_TARGET(i_target),
+                "L3 Clock Sync Drop Timeout");
+
+#endif
+
+    FAPI_DBG("Switch glsmux to refclk to save clock grid power via CPMS_CGCSR[11]");
+    FAPI_TRY( HCD_PUTMMIO_C( i_target, CPMS_CGCSR_WO_CLEAR, MMIO_1BIT(11) ) );
+
+fapi_try_exit:
+
+    FAPI_INF("<<p10_hcd_cache_stopgrid");
+
+    return fapi2::current_err;
+
+}
