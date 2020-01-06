e88b61c9e5db9e50b78524283cf1034a6a592aae
Prasad Bg Ranganath <prasadbgr@in.ibm.com>
2020-01-06 06:56:35 -0600

    P10:SBE Putring tor fixes
    Change-Id: I7569591b11634b04f0b5d984b8812233fa23a990
    Original-Change-Id: I4ebc867a21eac8044d2737297c20819bf73f94a6
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/89236
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: Prem Shanker Jha <premjha2@in.ibm.com>
    Reviewed-by: Gregory S Still <stillgs@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/91287
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

---
+++ src/import/chips/p10/utils/utils.mk
@@ -0,0 +1,38 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/import/chips/p10/utils/utils.mk $
+#
+# OpenPOWER sbe Project
+#
+# Contributors Listed Below - COPYRIGHT 2020
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+#  @file utils.mk
+#
+#  @brief mk for including Utils object files
+#
+##########################################################################
+# Object Files
+##########################################################################
+
+UTILS-CPP-SOURCES = p10_plat_ring_traverse.C
+UTILS-CPP-SOURCES += p10_putRingUtils.C
+PM-C-SOURCES =
+PM-S-SOURCES =
+
+UTILS_OBJECTS = $(UTILS-CPP-SOURCES:.C=.o)
