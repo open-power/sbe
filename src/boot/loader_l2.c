42d3f1a6f2c33966268fd9547aa160f8a3476744
Srikantha Meesala <srikantha@in.ibm.com>
2020-02-10 22:26:28 -0600

    Removed re-init PIBMEM area from seeprom loader(L2loader)
    -- Measurement L1 loader will zero init PIBMEM
    
    Change-Id: Id6fb90ade56c81675291f2d57983a1efcf2d7710
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/91415
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>

--- src/boot/loader_l2.c
+++ src/boot/loader_l2.c
@@ -47,37 +47,7 @@ int32_t l2_loader()
     uint64_t loadValue = (uint64_t)(SBE_CODE_BOOT_PIBMEM_L2_LOADER_MSG)<<32;
     PPE_STVD(0x50009, loadValue);
     P9XipHeader *hdr = getXipHdr();
-
     uint64_t *pibMemAddr  = (uint64_t *)g_pibMemAddr;
-    uint64_t *tempPibMemAddr  = (uint64_t *)g_pibMemAddr;
-    uint64_t *pibEndAddr  = (uint64_t *)(SBE_LOADER_BASE_ORIGIN);
-
-    // Initialise complete PIBMEM with 0
-    // Though this is inefficient, this is quick fix to solve the
-    // HW write of data which is not 8 byte alligned. All write which
-    // are not 8 byte alligned, translates to read-modify-write. So
-    // if code does a write for 4 byte, first HW will read 8 bytes. If
-    // there is no data written there earlier and random data is present
-    // it can cause ecc errors.
-    // TODO via RTC 158797
-    // Though we may be able to optimise it, but than we have to put the
-    // logic in loadSection function which is not desired. So currently
-    // initialising all PIBMEM. If performance become concern during initial
-    // SBE load, we can optimise this.
-    // We have three options here in optimisation
-    //   1.  Put this logic in SEEPROM loader ( l1loader ). This was we can
-    //       reclaim space taken by l2 loader as stack/bss can grow in base
-    //       loader section after image load.
-    //   2.  Keep this code in PIBMEM. But do not initialise the memory taken
-    //       by base image.
-    //   3.  Do what is done in option 1 but only initialise memoty after base
-    //       section to end of PIBMEM. This will involve some more hardcoding
-    //       in seeprom loader.
-    // My preference will be option 1 but depends upon performance goals.
-    for( ; tempPibMemAddr < pibEndAddr; tempPibMemAddr++ )
-    {
-        *tempPibMemAddr = 0;
-    }
 
     loadSection(&(hdr->iv_section[P9_XIP_SECTION_SBE_BASE]), pibMemAddr);
     // Set the IVPR register. This is required so that interrupt vector table
