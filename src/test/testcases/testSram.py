6caa66c9af98a2b9489e2dc8e4fb910ba4140621
Murulidhar Nataraju <murulidhar@in.ibm.com>
2020-03-20 10:25:58 -0500

    P10 SRAM - Enable Test case
    Change-Id: Ie54f84cdf2bca1d6a6c3e89a5e868ecab5f9c6c3
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/93765
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/test/testcases/testSram.py
+++ src/test/testcases/testSram.py
@@ -5,7 +5,7 @@
 #
 # OpenPOWER sbe Project
 #
-# Contributors Listed Below - COPYRIGHT 2016,2019
+# Contributors Listed Below - COPYRIGHT 2016,2020
 # [+] International Business Machines Corp.
 #
 #
@@ -39,17 +39,16 @@ def getdoubleword(dataInInt):
     hex_string = '0'*(16-len(str(hex(dataInInt))[:18][2:])) + str(hex(dataInInt))[:18][2:]
     return list(struct.unpack('<BBBBBBBB',hex_string.decode('hex')))
 
-def putsram(addr, mode, data, primStatus, secStatus):
-    req = (getsingleword(5 + (len(data)/4))
+def putsram(addr, mode, data, primStatus, secStatus,fifoType):
+    req = (getsingleword(6 + (len(data)/4))
            + [0, 0, 0xA4, 0x04]
            + getsingleword(mode)
-           + getsingleword(addr)
+           + getdoubleword(addr)
            + getsingleword(len(data))
            + data)
-
     testUtil.runCycles( 10000000 )
-    testUtil.writeUsFifo( req )
-    testUtil.writeEot( )
+    testUtil.writeUsFifo( req,fifoType )
+    testUtil.writeEot(fifoType)
 
     if((primStatus != 0) or (secStatus != 0)):
         data = []
@@ -58,104 +57,134 @@ def putsram(addr, mode, data, primStatus, secStatus):
                + gethalfword(primStatus)
                + gethalfword(secStatus)
                + getsingleword(0x03))
-    testUtil.readDsFifo(expData)
-    testUtil.readEot( )
+    testUtil.readDsFifo(expData,fifoType)
+    testUtil.readEot(fifoType )
 
-def getsram(addr, mode, length, primStatus, secStatus):
-    req = (getsingleword(0x05)
+def getsram(addr, mode, length, primStatus, secStatus,fifoType):
+    
+    req = (getsingleword(0x06)
            + getsingleword(0xa403)
            + getsingleword(mode)
-           + getsingleword(addr)
+           + getdoubleword(addr)
            + getsingleword(length))
 
     testUtil.runCycles( 10000000 )
-    testUtil.writeUsFifo( req )
-    testUtil.writeEot( )
-
+    testUtil.writeUsFifo( req,fifoType )
+    testUtil.writeEot(fifoType )
+    testUtil.runCycles( 10000000 )
     data = []
     if((primStatus != 0) or (secStatus != 0)):
         length = 0
     for i in range(0, int(-(-float(length)//4))):
-        data += list(testUtil.readDsEntryReturnVal())
-    readLen = testUtil.readDsEntryReturnVal()
+        data += list(testUtil.readDsEntryReturnVal(fifoType))
+    readLen = testUtil.readDsEntryReturnVal(fifoType)
     if(getsingleword(length) != list(readLen)):
         print getsingleword(length)
         print list(readLen)
-        raise Exception("Invalid Length")
+        raise Exception("Ivalid Length")
 
     expResp = (getsingleword(0xc0dea403)
                + gethalfword(primStatus)
                + gethalfword(secStatus)
                + getsingleword(0x03))
-    testUtil.readDsFifo(expResp)
-    testUtil.readEot( )
+    testUtil.readDsFifo(expResp,fifoType)
+    testUtil.readEot(fifoType)
 
     return data[:length]
+
+
+def validateSRAMAccess( fifoType ):
+   testcase = ""
+   try:
+       #Data used to write to SRAM
+       data = os.urandom(128*2)
+       data = [ord(c) for c in data]
+
+       #---------------------------------------------
+       # Put and Get Occ Sram test - Circular Mode
+       #---------------------------------------------
+       testcase = "Circlular OCC SRAM Put and GET Testcase"
+       #0xC0 : Chiplet_ID 0x0 (not applicable for OCC access)
+       #       Circular Access Mode
+       #       Channel bits 0(which makes HWP to pick channel 3 as default)
+       putsram(0x00000000FFFBE000, 0xC0, data, 0, 0,fifoType)
+       readData = getsram(0x00000000FFFBE000, 0xC0, (128*2), 0, 0,fifoType)
+       if(data == readData):
+          print("Success: Put-get OCC sram, Circular Mode:FIFOTYPE:%d"%fifoType)
+       else:
+          print ("Read data does not match with data used to write")
+          print data
+          print readData
+          raise Exception('data mistmach')
+
+       #---------------------------------------------
+       # Put and Get Occ Sram test - Linear Mode
+       #---------------------------------------------
+       testUtil.runCycles( 10000000 )
+       testcase = "Linear OCC SRAM Put and GET Testcase"
+       #0x50 : Chiplet_ID 0x0 (not applicable for OCC access)
+       #       Liner Access Mode
+       #       Channel 1
+       putsram(0x00000000FFFBE000, 0x50, data, 0, 0,fifoType)
+       readData = getsram(0x00000000FFFBE000, 0x50, (128*2), 0, 0,fifoType)
+       if(data == readData):
+           print("Success: Put-Get sram Linear Mode,FIFOTYPE:%d"%fifoType)
+       else:
+           print ("Read data does not match with data used to write")
+           print data
+           print readData
+           raise Exception('data mistmach')
+  
+       #---------------------------------------------
+       # Put and Get QME Sram test
+       #---------------------------------------------
+       testUtil.runCycles( 10000000 )
+       testcase = "QME SRAM Put and GET Testcase"
+       #0x208000 : Chiplet 0x20 Multicast Bit:1
+       putsram(0x00000000ffff0000, 0x208000, data, 0, 0,fifoType)
+       testUtil.runCycles( 10000000 )
+       readData = getsram(0x00000000ffff0000, 0x200000, (128*2), 0, 0,fifoType)
+       if(data == readData):
+          print("Success: QME SRAM Put and GET Testcase,FIFOTYPE:%d"%fifoType)
+       else:
+          print ("Read data does not match with data used to write")
+          print data
+          print readData
+          raise Exception('data mistmach')
+
+       #---------------------------------------------
+       # Put and Get IO_PPE Sram test
+       #---------------------------------------------
+#TODO: Enable this once simics fixes autoincrementation of the
+#      IO_PPE SRAM Address
+#       testUtil.runCycles( 10000000 )
+#       testcase = "IO_PPE SRAM Put and GET Testcase"
+        #0x208000 : Chiplet 0x10 Multicast Bit:1
+#       putsram(0xffff280000000000, 0x108000, data, 0, 0,fifoType)
+#       testUtil.runCycles( 10000000 )
+#       readData = getsram(0xffff280000000000, 0x100000, (128*2), 0, 0,fifoType)
+#       if(data == readData):
+#          print("Success: IO_PPE SRAM Put and GET Testcase,FIFOTYPE:%d"%fifoType)
+#       else:
+#          print ("Read data does not match with data used to write")
+#          print data
+#          print readData
+#          raise Exception('data mistmach')
+
+   except:
+        print "FAILED Test Case:"+str(testcase)
+        raise Exception('Failure')
 
 # MAIN Test Run Starts Here...
 #-------------------------------------------------
 def main( ):
-    testcase = ""
-    try:
-        testUtil.runCycles( 10000000 )
-
-        # Put Occ Sram test - Linear - Can be tested over Normal
-        # Debug mode
-        data = os.urandom(128*2)
-        data = [ord(c) for c in data]
-        putsram(0xFFFBE000, 0x01, data, 0, 0)
-
-        readData = getsram(0xFFFBE000, 0x01, 128*2, 0, 0)
-
-        if(data == readData):
-            print("Success: put - get sram")
-        else:
-            print data
-            print readData
-            raise Exception('data mistmach')
-
-        # secure mem - write to disallowed mem
-        # start and end completely outside
-        testcase = "sec put test 1"
-        putsram(0xFFFBE000-256, 0x01, data, 0x0005, 0x0014)
-        print("Success: "+testcase)
-        # start outside and end inside the window
-        testcase = "sec put test 2"
-        putsram(0xFFFBE000-128, 0x01, data, 0x0005, 0x0014)
-        print("Success: "+testcase)
-        # start inside and end outside the window
-        testcase = "sec put test 3"
-        putsram(0xFFFBE000-128, 0x01, data, 0x0005, 0x0014)
-        print("Success: "+testcase)
-    
-        # comment out -ve tests for security wisnidw as we allow read always
-        # secure mem - read on disallowed mem
-        # start and end completely outside
-        #testcase = "sec get test 1"
-        #getsram(0xFFFBE000-256, 0x01, 256, 0x0005, 0x0014)
-        #print("Success: "+testcase)
-        # start outside and end inside the window
-        #testcase = "sec get test 2"
-        #getsram(0xFFFBE000-128, 0x01, 256, 0x0005, 0x0014)
-        #print("Success: "+testcase)
-        # start inside and end outside the window
-        #testcase = "sec get test 3"
-        #getsram(0xFFFBE000-128, 0x01, 256, 0x0005, 0x0014)
-        #print("Success: "+testcase)
-
-        # Put Occ Sram test - Circular - Can be enabled once we get
-        # valid address range to read the circular data
-        #testUtil.writeUsFifo( PUTSRAM_OCC_TESTDATA_1 )
-        #testUtil.writeEot( )
-        #testUtil.readDsFifo( PUTSRAM_OCC_EXPDATA_1 )
-        #testUtil.readEot( )
-        #testUtil.writeUsFifo( GETSRAM_OCC_TESTDATA_1 )
-        #testUtil.writeEot( )
-        #testUtil.readDsFifo( GETSRAM_OCC_EXPDATA_1 )
-        #testUtil.readEot( )
-    except:
-        print "FAILED Test Case:"+str(testcase)
-        raise Exception('Failure')
+      #Validate all SRAM access usecases using First FIFO
+      fifoType=0
+      validateSRAMAccess(fifoType)
+      testUtil.runCycles( 10000000 )
+      #Validate all SRAM access usecases using Second FIFO
+      fifoType=1
+      validateSRAMAccess(fifoType)
 
 #-------------------------------------------------
 # Calling all test code
