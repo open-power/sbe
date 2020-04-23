ed7d4bef0e77bc892c33f9db70523d1acb01a7c9
Murulidhar Nataraju <murulidhar@in.ibm.com>
2020-04-23 00:21:38 -0500

    Phase1: Enable MPIPL Procedure compilation
    Change-Id: Iee1ce336696e950c6f0e1534de70c47dd443e9f7
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/95611
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Srikantha S. Meesala <srikantha@in.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/build/import_hwp_mk/power/istepmpipl/istepmpiplfiles.mk
+++ src/build/import_hwp_mk/power/istepmpipl/istepmpiplfiles.mk
@@ -5,7 +5,7 @@
 #
 # OpenPOWER sbe Project
 #
-# Contributors Listed Below - COPYRIGHT 2016,2019
+# Contributors Listed Below - COPYRIGHT 2016,2020
 # [+] International Business Machines Corp.
 #
 #
@@ -30,30 +30,28 @@
 # Object Files
 ##########################################################################
 
-#ISTEPMPIPL-CPP-SOURCES = p9_hcd_core_stopclocks.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_hcd_cache_stopclocks.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_stopclocks.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_common_stopclocks.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_cplt_stopclocks.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_nest_stopclocks.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_tp_stopclocks.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_check_chiplet_states.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_hcd_l2_stopclocks.C
+ISTEPMPIPL-CPP-SOURCES =  p10_hcd_core_stopclocks.C
+ISTEPMPIPL-CPP-SOURCES += p10_hcd_cache_stopclocks.C
+ISTEPMPIPL-CPP-SOURCES += p10_hcd_eq_stopclocks.C
+ISTEPMPIPL-CPP-SOURCES += p10_common_stopclocks.C
+ISTEPMPIPL-CPP-SOURCES += p10_cplt_stopclocks.C
+ISTEPMPIPL-CPP-SOURCES += p10_stopclocks.C
+ISTEPMPIPL-CPP-SOURCES += p10_perv_sbe_cmn.C
+ISTEPMPIPL-CPP-SOURCES += p10_tp_stopclocks.C
+ISTEPMPIPL-CPP-SOURCES += p10_l2_flush.C
+ISTEPMPIPL-CPP-SOURCES += p10_l3_flush.C
+#ISTEPMPIPL-CPP-SOURCES += p10_suspend_powman.C
+ISTEPMPIPL-CPP-SOURCES += p10_query_corecachemma_access_state.C
+ISTEPMPIPL-CPP-SOURCES += p10_hcd_mma_poweroff.C
+ISTEPMPIPL-CPP-SOURCES += p10_hcd_core_poweroff.C
+ISTEPMPIPL-CPP-SOURCES += p10_hcd_cache_poweroff.C
+ISTEPMPIPL-CPP-SOURCES += p10_suspend_io.C
 #ISTEPMPIPL-CPP-SOURCES +=p9_block_wakeup_intr.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_l2_flush.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_l3_flush.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_sbe_check_quiesce.C
 #ISTEPMPIPL-CPP-SOURCES +=p9_inject_local_xstop.C
 #ISTEPMPIPL-CPP-SOURCES +=p9_sbe_sequence_drtm.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_query_core_access_state.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_quad_power_off.C
 #ISTEPMPIPL-CPP-SOURCES +=p9_pm_pfet_control.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_suspend_io.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_perv_sbe_cmn.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_suspend_powman.C
 #ISTEPMPIPL-CPP-SOURCES +=p9_collect_suspend_ffdc.C
 #ISTEPMPIPL-CPP-SOURCES +=p9_eq_clear_atomic_lock.C
-#ISTEPMPIPL-CPP-SOURCES +=p9_query_cache_access_state.C
 # Fast array procedures are moved here to locate it on seeprom
 #ISTEPMPIPL-CPP-SOURCES += p9_sbe_fastarray_abist_catchup.C
 #ISTEPMPIPL-CPP-SOURCES += p9_sbe_fastarray_cleanup.C
@@ -61,7 +59,6 @@
 # io eol toggle procedure moved here to locate it on seeprom
 #ISTEPMPIPL-CPP-SOURCES += p9_sbe_io_eol_toggle.C
 #ISTEPMPIPL-CPP-SOURCES += nvdimm_workarounds.C
-ISTEPMPIPL-CPP-SOURCES =
 ISTEPMPIPL-C-SOURCES =
 ISTEPMPIPL-S-SOURCES =
 
