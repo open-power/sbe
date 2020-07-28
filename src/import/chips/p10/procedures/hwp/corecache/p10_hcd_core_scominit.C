9f43bc9893f4df2590a88e69c736fe69280c40fa
Yue Du <daviddu@us.ibm.com>
2020-07-28 12:36:28 -0500

    QME: fix to 99660 on core_scominit and pair_mode
    https://rchgit01.rchland.ibm.com/gerrit1/#/c/99660/
    
    Change-Id: I30170f77b157e96c66442c2da690e5248d3c6dfe
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/101226
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Gregory S Still <stillgs@us.ibm.com>
    Reviewed-by: DAVID A KING <kingboy@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/101231
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/import/chips/p10/procedures/hwp/corecache/p10_hcd_core_scominit.C
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_core_scominit.C
@@ -37,7 +37,7 @@
 
 #include "p10_hcd_core_scominit.H"
 #include <p10_fbc_utils.H>
-#include <p10_scom_c_c.H>
+#include <p10_scom_c.H>
 
 #ifdef __PPE_QME
     #include <p10_l2_scom.H>
@@ -124,10 +124,52 @@ fapi_try_exit:
 //------------------------------------------------------------------------------
 
 #else
+
+namespace
+{
+///
+/// @brief Initialize the L2 topology id table entries
+/// @param[in] c                Reference to core target
+/// @param[in] topo_scoms       Vector where each element is the content to write
+///                             into the topology id table SCOM register.
+///                             topo_scoms[0] contains reg value for entries  0.. 7
+///                             topo_scoms[1] contains reg value for entries  8..15
+///                             topo_scoms[2] contains reg value for entries 16..23
+///                             topo_scoms[3] contains reg value for entries 24..31
+///                             assert(topo_scoms.size() == 4)
+/// @return fapi::ReturnCode    FAPI2_RC_SUCCESS on success, error otherwise
+///
+fapi2::ReturnCode init_topo_id_tables(const fapi2::Target < fapi2::TARGET_TYPE_CORE
+                                      | fapi2::TARGET_TYPE_MULTICAST > & c,
+                                      const std::vector<uint64_t>& topo_scoms)
+{
+    using namespace scomt::c;
+
+    PREP_L2_L2MISC_L2CERRS_TOPOTABLE0(c);
+    FAPI_TRY(PUT_L2_L2MISC_L2CERRS_TOPOTABLE0(c, topo_scoms[0]));
+    PREP_L2_L2MISC_L2CERRS_TOPOTABLE1(c);
+    FAPI_TRY(PUT_L2_L2MISC_L2CERRS_TOPOTABLE1(c, topo_scoms[1]));
+    PREP_L2_L2MISC_L2CERRS_TOPOTABLE2(c);
+    FAPI_TRY(PUT_L2_L2MISC_L2CERRS_TOPOTABLE2(c, topo_scoms[2]));
+    PREP_L2_L2MISC_L2CERRS_TOPOTABLE3(c);
+    FAPI_TRY(PUT_L2_L2MISC_L2CERRS_TOPOTABLE3(c, topo_scoms[3]));
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+};
+
 static inline fapi2::ReturnCode p10_hcd_core_scominit_sbe(
     const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST > & i_target)
 {
     // Placeholder for core/L2 scom inits needed during HBI
+    std::vector<uint64_t> l_topo_scoms;
+    fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> l_chip = i_target.getParent<fapi2::TARGET_TYPE_PROC_CHIP>();
+
+    // Get the register values for the SCOMs to setup the topology id table
+    FAPI_TRY(topo::get_topology_table_scoms(l_chip, l_topo_scoms));
+    // Setup the topology id tables for L2 via multicast
+    FAPI_TRY(init_topo_id_tables(i_target, l_topo_scoms));
 
 fapi_try_exit:
     return fapi2::current_err;
