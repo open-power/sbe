ccdf6df0e8f8fa9bb522ded49917b1c8fb13fdfe
Raja Das <rajadas2@in.ibm.com>
2020-08-14 04:13:21 -0500

    Added FastArray Data Length in the return buffer
    Added length at the last of fa_buffer before footer and cleanup.
    
    Change-Id: Ib75842c49a65b5107947e17f5983ca89754cfc30
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/102443
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/sbefw/app/power/sbecmdfastarray.C
+++ src/sbefw/app/power/sbecmdfastarray.C
@@ -34,7 +34,6 @@
 #include <hwp_data_stream.H>
 
 #include <plat_hwp_data_stream.H>
-
 #include "p10_sbe_fastarray.H"
 
 using namespace fapi2;
@@ -43,72 +42,73 @@ uint32_t sbeControlFastArray(uint8_t *i_pArg)
 {
     #define SBE_FUNC " sbeControlFastArray"
     SBE_ENTER(SBE_FUNC);
-    uint32_t l_rc = SBE_SEC_OPERATION_SUCCESSFUL;
-    sbeControlFastArrayCMD_t l_req = {};
+    uint32_t rc = SBE_SEC_OPERATION_SUCCESSFUL;
+    sbeControlFastArrayCMD_t req = {};
     sbeRespGenHdr_t respHdr;
     respHdr.init();
-    sbeResponseFfdc_t l_ffdc;
-    ReturnCode l_fapiRc;
+    sbeResponseFfdc_t ffdc;
+    ReturnCode fapiRc;
 
     do
     {
-        uint32_t l_len = SBE_GLOBAL->sbeFifoCmdHdr.len -
+        uint32_t len = SBE_GLOBAL->sbeFifoCmdHdr.len -
             sizeof(SBE_GLOBAL->sbeFifoCmdHdr)/sizeof(uint32_t);
-        const uint32_t l_fifo_control_data_length = l_len -
-            sizeof(l_req.hdr)/sizeof(uint32_t);
+        const uint32_t fifo_control_data_length = len -
+            sizeof(req.hdr)/sizeof(uint32_t);
 
-        if (l_len > sizeof(l_req)/sizeof(uint32_t))
+        if (len > sizeof(req)/sizeof(uint32_t))
         {
             SBE_ERROR(SBE_FUNC" Fastarray request is too large."
                       "request size: 0x%08x, max size: 0x%08x",
-                      l_len, sizeof(l_req)/sizeof(uint32_t));
+                      len, sizeof(req)/sizeof(uint32_t));
             respHdr.setStatus( SBE_PRI_INVALID_DATA,
                                SBE_SEC_GENERIC_FAILURE_IN_EXECUTION);
             // flush the fifo
-            l_rc = sbeUpFifoDeq_mult(l_len, NULL, true, true);
+            rc = sbeUpFifoDeq_mult(len, NULL, true, true);
             break;
         }
 
-        l_rc = sbeUpFifoDeq_mult(l_len, (uint32_t *)&l_req); //EOT fetch
-        CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(l_rc);
+        rc = sbeUpFifoDeq_mult(len, (uint32_t *)&req); //EOT fetch
+        CHECK_SBE_RC_AND_BREAK_IF_NOT_SUCCESS(rc);
 
         SBE_INFO(SBE_FUNC" targetType [0x%04X] chipletId [0x%02X] control_set [0x%02X] "
-                 "custom_data_length [0x%08X]", l_req.hdr.targetType,
-                 static_cast<uint8_t>(l_req.hdr.chipletId),
-                 static_cast<uint8_t>(l_req.hdr.control_set),
-                 l_req.hdr.custom_data_length);
+                 "custom_data_length [0x%08X]", req.hdr.targetType,
+                 static_cast<uint8_t>(req.hdr.chipletId),
+                 static_cast<uint8_t>(req.hdr.control_set),
+                 req.hdr.custom_data_length);
 
         // Construct a Target from Chiplet ID and Target Type
-        plat_target_handle_t l_tgtHndl;
-        if(!sbeGetFapiTargetHandle(l_req.hdr.targetType, l_req.hdr.chipletId,
-                                   l_tgtHndl))
+        plat_target_handle_t tgtHndl;
+        if(!sbeGetFapiTargetHandle(req.hdr.targetType, req.hdr.chipletId,
+                                   tgtHndl))
         {
             SBE_ERROR(SBE_FUNC "Invalid target - Type [0x%04x] Chiplet [0x%02X]",
-                            static_cast<uint16_t>(l_req.hdr.targetType),
-                            static_cast<uint8_t>(l_req.hdr.chipletId));
+                            static_cast<uint16_t>(req.hdr.targetType),
+                            static_cast<uint8_t>(req.hdr.chipletId));
             respHdr.setStatus(SBE_PRI_INVALID_DATA,
                               SBE_SEC_INVALID_TARGET_TYPE_PASSED);
             break;
         }
 
-        if (l_req.hdr.control_set == SBE_FASTARRAY_CUSTOM_CONTROL_SET)
+        if (req.hdr.control_set == SBE_FASTARRAY_CUSTOM_CONTROL_SET)
         {
-            if (l_req.hdr.custom_data_length != l_fifo_control_data_length)
+            if (req.hdr.custom_data_length != fifo_control_data_length)
             {
                 SBE_ERROR(SBE_FUNC" Control data size in request does not match "
                           "calculated size. length field: 0x%08x, calc length: 0x%08x",
-                          l_req.hdr.custom_data_length, l_fifo_control_data_length);
+                          req.hdr.custom_data_length, fifo_control_data_length);
                 respHdr.setStatus( SBE_PRI_INVALID_DATA,
                                    SBE_SEC_GENERIC_FAILURE_IN_EXECUTION);
                 break;
             }
-            hwp_array_istream l_istream(l_req.custom_control_data, l_req.hdr.custom_data_length);
-            sbefifo_hwp_data_ostream l_ostream;
-            SBE_EXEC_HWP(l_fapiRc, p10_sbe_fastarray, l_tgtHndl, l_istream, l_ostream);
+            hwp_array_istream istream(req.custom_control_data, req.hdr.custom_data_length);
+            sbefifo_hwp_data_ostream ostream;
+            SBE_EXEC_HWP(fapiRc, p10_sbe_fastarray, tgtHndl, istream, ostream);
+            ostream.put(ostream.words_written() * 4); //words_written needs to convert to number of bytes
         }
         else
         {
-            if (l_req.hdr.custom_data_length != 0)
+            if (req.hdr.custom_data_length != 0)
             {
                 SBE_ERROR(SBE_FUNC" Control data size in request is nonzero, but "
                           "custom control data is not selected.");
@@ -116,33 +116,34 @@ uint32_t sbeControlFastArray(uint8_t *i_pArg)
                                    SBE_SEC_GENERIC_FAILURE_IN_EXECUTION);
                 break;
             }
-            if (l_req.hdr.isControlSetValid() != true)
+            if (req.hdr.isControlSetValid() != true)
             {
-                SBE_ERROR(SBE_FUNC" Control Set not valid [%d]", (uint32_t)l_req.hdr.control_set);
+                SBE_ERROR(SBE_FUNC" Control Set not valid [%d]", (uint32_t)req.hdr.control_set);
                 respHdr.setStatus( SBE_PRI_INVALID_DATA,
                                    SBE_SEC_GENERIC_FAILURE_IN_EXECUTION);
                 break;
             }
 
             // TODO: Determine control data location in SEEPROM
-            uint32_t *l_control_data = (uint32_t*)l_req.hdr.getFastArrayXipOffset();
-            size_t l_control_data_size = l_req.hdr.getFastArrayXipSize();
+            uint32_t *control_data = (uint32_t*)req.hdr.getFastArrayXipOffset();
+            size_t control_data_size = req.hdr.getFastArrayXipSize();
             SBE_INFO( SBE_FUNC "Start Offset [0x%08X] Size [0x%08X]",
-                l_req.hdr.getFastArrayXipOffset(), l_req.hdr.getFastArrayXipSize());
+                req.hdr.getFastArrayXipOffset(), req.hdr.getFastArrayXipSize());
 
-            seeprom_hwp_data_istream l_istream(l_control_data, l_control_data_size);
-            sbefifo_hwp_data_ostream l_ostream;
-            SBE_EXEC_HWP(l_fapiRc, p10_sbe_fastarray, l_tgtHndl, l_istream, l_ostream);
+            seeprom_hwp_data_istream istream(control_data, control_data_size);
+            sbefifo_hwp_data_ostream ostream;
+            SBE_EXEC_HWP(fapiRc, p10_sbe_fastarray, tgtHndl, istream, ostream);
+            ostream.put(ostream.words_written() * 4); //words_written needs to convert to number of bytes
         }
 
-        if(l_fapiRc != FAPI2_RC_SUCCESS)
+        if(fapiRc != FAPI2_RC_SUCCESS)
         {
             SBE_ERROR(SBE_FUNC" HWP failure:targetType [0x%04X] "
                       "chipletId [0x%02X] control_set [0x%02X] ",
-                      static_cast<uint16_t>(l_req.hdr.targetType),
-                      static_cast<uint8_t>(l_req.hdr.chipletId),
-                      static_cast<uint32_t>(l_req.hdr.control_set));
-            l_ffdc.setRc(l_fapiRc);
+                      static_cast<uint16_t>(req.hdr.targetType),
+                      static_cast<uint8_t>(req.hdr.chipletId),
+                      static_cast<uint32_t>(req.hdr.control_set));
+            ffdc.setRc(fapiRc);
         }
 
     } while(false);
@@ -150,12 +151,12 @@ uint32_t sbeControlFastArray(uint8_t *i_pArg)
     // Now build and enqueue response into downstream FIFO
     // If there was a FIFO error, will skip sending the response,
     // instead give the control back to the command processor thread
-    if ( SBE_SEC_OPERATION_SUCCESSFUL == l_rc )
+    if ( SBE_SEC_OPERATION_SUCCESSFUL == rc )
     {
-        l_rc = sbeDsSendRespHdr( respHdr, &l_ffdc);
+        rc = sbeDsSendRespHdr( respHdr, &ffdc);
     }
 
     SBE_EXIT(SBE_FUNC);
-    return l_rc;
+    return rc;
     #undef SBE_FUNC
 }
