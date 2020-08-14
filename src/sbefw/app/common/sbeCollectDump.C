9a6285b7708a13f93149f9ea0963578c61b4257b
Sunil Kumar <skumar8j@in.ibm.com>
2020-08-14 04:48:33 -0500

    class definition for sbeCollectDump
    Change-Id: Idb4cb01b5f798deaf1b77a91c0943730c3aeab12
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/102444
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

---
+++ src/sbefw/app/common/sbeCollectDump.C
@@ -0,0 +1,68 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/sbefw/app/common/sbeCollectDump.C $                       */
+/*                                                                        */
+/* OpenPOWER sbe Project                                                  */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2020                             */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include"sbeCollectDump.H"
+
+using namespace fapi2;
+
+//Constuctor for sbeCollectDump class.
+sbeCollectDump::sbeCollectDump(uint32_t dumpID, sbeFifoType type, uint32_t & errHndl)
+{
+
+}
+
+//collectAllHDCTEntries()
+uint32_t collectAllHDCTEntries()
+{
+    return 0;
+}
+
+//parserSingleHDCTEntry()
+uint32_t parserSingleHDCTEntry()
+{
+    return 0;
+}
+
+//populateHDCTRowTOC()
+uint32_t populateHDCTRowTOC()
+{
+    return 0;
+}
+
+//collectSingleEntry()
+uint32_t collectSingleEntry(uint8_t * data, uint32_t len)
+{
+    return 0;
+}
+
+//writeDatatoFIFO()
+uint32_t writeDatatoFIFO(uint8_t * data, uint32_t len)
+{
+    return 0;
+}
+
+//checkHWStateofChipAndChiplets()
+bool checkHWStateofChipAndChiplets()
+{
+    return 0;
+}
