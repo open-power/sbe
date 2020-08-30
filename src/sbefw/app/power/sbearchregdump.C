6108e65d45b279998ebd5eddaa23ae815637e5d2
Murulidhar Nataraju <murulidhar@in.ibm.com>
2020-08-30 13:11:04 -0500

    MPIPL: Enable Architected register collection SET1
    - Disable Failing SPR registers
    - Default TIMA data
    - Remove TIMA collection utility interfaces
    - Enable SPR-GPR collection
    
    Change-Id: Ifc046e9692c923c1b7c9402329982705690a7318
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/103559
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/sbefw/app/power/sbearchregdump.C
+++ src/sbefw/app/power/sbearchregdump.C
@@ -50,7 +50,7 @@
 
 using namespace fapi2;
 
-#define SPR_LIST_SIZE  148
+#define SPR_LIST_SIZE  142
 #define GPR_LIST_SIZE  32
 #define TIMA_LIST_SIZE 8
 #define GPR_REG_TYPE   0x1
@@ -167,109 +167,6 @@ ReturnCode checkCoreStateForRam(
     #undef SBE_FUNC
 }
 
-ReturnCode sbeFetchTimaAddr( uint64_t & o_timaBarAddr,
-                             uint32_t & o_timaAddrKBrange )
-{
-    #define SBE_FUNC " sbeFetchTimaAddr "
-    SBE_ENTER(SBE_FUNC);
-    using namespace scomt::proc;
-    ReturnCode fapiRc = FAPI2_RC_SUCCESS;
-    do
-    {
-        Target<TARGET_TYPE_PROC_CHIP > procTgt = plat_getChipTarget();
-        fapi2::buffer<uint64_t> timaDataLoc;
-        o_timaBarAddr = 0;
-        auto nest0 = procTgt.getChildren<fapi2::TARGET_TYPE_PERV>
-                                               (fapi2::TARGET_FILTER_NEST_NORTH,
-                                                fapi2::TARGET_STATE_PRESENT)[0];
-        fapiRc = getscom_abs_wrap(&nest0, INT_CQ_IC_BAR, &timaDataLoc());
-        if(fapiRc != fapi2::FAPI2_RC_SUCCESS)
-        {
-            SBE_ERROR(SBE_FUNC "Failed to read INT_CQ_IC_BAR from Nest0");
-            break;
-        }
-        SBE_INFO("TIMA INPAR SCOM(0x%.8x%.8x) DATA:0x%.8x%.8x",
-                 ((INT_CQ_IC_BAR & 0xFFFFFFFF00000000ull) >> 32),(INT_CQ_IC_BAR & 0xFFFFFFFF),
-                 ((timaDataLoc & 0xFFFFFFFF00000000ull) >> 32),(timaDataLoc & 0xFFFFFFFF));
-
-        // Bit-1 from INT_CQ_IC_BAR is updated as 1 for PHYP uses 64k,
-        // if Bit-1 is 0 then  KVM uses 4k range
-        // TimaDataLoc.Bit[1] == 1 (64k Range) then INPAR[8:45] bits
-        // TimaDataLoc.Bit[1] == 0 ( 4k Range) then INPAR[8:49] bits
-        o_timaAddrKBrange = 4*1024;
-        o_timaBarAddr = timaDataLoc & 0x00FFFFFFFFFFC000; //BITS(8:49)
-        if (timaDataLoc.getBit<INT_CQ_IC_BAR_PAGE_SIZE_64K>())
-        {
-           // (64k Range) then INPAR[8:45] bits need take a BAR_SCOMADDRESS
-           o_timaBarAddr = ( o_timaBarAddr & 0x00FFFFFFFFFC0000 );//BITS(8:45)
-           o_timaAddrKBrange = 64*1024;
-        }
-
-        SBE_INFO("TIMA INPAR base address [0x%08X %08X]and IS_BAR_PAGE_SIZE_64K[%d]",
-            SBE::higher32BWord(o_timaBarAddr),SBE::lower32BWord(o_timaBarAddr),
-            timaDataLoc.getBit<INT_CQ_IC_BAR_PAGE_SIZE_64K>());
-
-    } while(0);
-
-    SBE_EXIT(SBE_FUNC);
-    return fapiRc;
-    #undef SBE_FUNC
-}
-
-ReturnCode getTimaBarBytes( uint64_t &i_threadTimaAddress, uint32_t &i_regIdx,
-                            uint64_t &o_data64 )
-{
-    #define SBE_FUNC " getTimaBarBytes"
-    SBE_ENTER(SBE_FUNC);
-    ReturnCode fapiRc = FAPI2_RC_SUCCESS;
-    o_data64 = 0;
-
-    // Collect TIMA architected data collection
-    union timaData_t
-    {
-        uint64_t tima8byteData;
-        uint8_t timaBuffer[8];  
-    };
-    timaData_t timaData;
-    timaData.tima8byteData = 0x0;
-    uint8_t readBytes = 8;
-    uint32_t timaFlags = fapi2::SBE_MEM_ACCESS_FLAGS_TARGET_PROC |
-                         fapi2::SBE_MEM_ACCESS_FLAGS_FAST_MODE_ON |
-                         fapi2::SBE_MEM_ACCESS_FLAGS_CACHE_INHIBITED_MODE;
-    Target<TARGET_TYPE_PROC_CHIP > procTgt = plat_getChipTarget();
-    uint64_t timaAddress = i_threadTimaAddress + i_regIdx*readBytes;
-    if(i_regIdx == 7)
-    {
-        readBytes = 1;
-    }
-    SBE_EXEC_HWP(fapiRc, p10_getmemproc, procTgt, timaAddress,
-                 readBytes, reinterpret_cast<uint8_t*>(timaData.timaBuffer), timaFlags);
-    if(fapiRc == FAPI2_RC_SUCCESS)
-    {
-        if(i_regIdx == 7)
-        {
-            // TIMA7 uses only 1 byte [ upper nibble in 64bits ].
-            // 0x01 this is the reads from ADU -> Put the data as below.
-            // 0x01 00 00 00 00 00 00 00
-            o_data64 = ((uint64_t)timaData.timaBuffer[0]) << 56;
-        }
-        else
-        {
-            o_data64 = timaData.tima8byteData;
-        }
-        SBE_INFO("TIMA Address[0x%08X %08X] Data[0x%08X %08X]",
-                SBE::higher32BWord(timaAddress),SBE::lower32BWord(timaAddress),
-                SBE::higher32BWord(o_data64),SBE::lower32BWord(o_data64)); 
-    }
-    else
-    {
-        SBE_ERROR("Failed to read TIMA address[0x%08X %08X]",
-                SBE::higher32BWord(timaAddress),SBE::lower32BWord(timaAddress));
-    }
-    SBE_EXIT(SBE_FUNC);
-    return fapiRc;
-    #undef SBE_FUNC
-}
 ///////////////////////////////////////////////////////////////////////
 // @brief sbeDumpArchRegs Dump out the architected registers
 //
@@ -279,14 +176,14 @@ ReturnCode sbeDumpArchRegs()
     #define SBE_FUNC " sbeDumpArchRegs "
     SBE_ENTER(SBE_FUNC);
     ReturnCode fapiRc = FAPI2_RC_SUCCESS;
-#if 0
     uint64_t dumpAddr = 0;
-    uint64_t timaBarAddr = 0;
-    uint32_t timaAddrKBrange = 0;
     sbeArchRegDumpProcHdr_t dumpProcHdr = {};
     sbeArchRegDumpThreadHdr_t dumpThreadHdr = {};
     sbeArchRegDumpEntries_t dumpRegData = {};
 
+    //TODO: Add below SPRs to the list after confirming the 
+    //requirement 496,497,505,506,507,511
+    //
     // Combined list of SPRs GPRs TIMAs
     static const uint16_t SPR_GPR_TIMA_list[] = {
         // List for TIMA 0-7 to be collected in MPIPL path
@@ -297,13 +194,12 @@ ReturnCode sbeDumpArchRegs()
         188 ,189 ,190 ,256 ,259 ,268 ,269 ,272 ,273 ,274 ,275 ,276 ,277 ,
         284 ,285 ,286 ,287 ,304 ,305 ,306 ,307 ,308 ,309 ,310 ,313 ,314 ,
         315 ,317 ,318 ,319 ,336 ,337 ,338 ,339 ,349 ,446 ,455 ,464 ,471 ,
-        496 ,497 ,505 ,506 ,507 ,511 ,736 ,737 ,738 ,752 ,753 ,754 ,768 ,
-        769 ,770 ,771 ,772 ,773 ,774 ,775 ,776 ,779 ,780 ,781 ,782 ,784 ,
-        785 ,786 ,787 ,788 ,789 ,790 ,791 ,792 ,795 ,796 ,797 ,798 ,799 ,
-        800 ,801 ,802 ,803 ,804 ,805 ,806 ,812 ,815 ,816 ,823 ,828 ,848 ,
-        849 ,850 ,851 ,853 ,855 ,861 ,880 ,881 ,882 ,884 ,885 ,895 ,896 ,
-        898 ,921 ,922 ,1006,1008,1023,2000,2001,2002,2003,2004,2005,2006,
-        2007,
+        736 ,737 ,738 ,752 ,753 ,754 ,768 ,769 ,770 ,771 ,772 ,773 ,774 ,
+        775 ,776 ,779 ,780 ,781 ,782 ,784 ,785 ,786 ,787 ,788 ,789 ,790 ,
+        791 ,792 ,795 ,796 ,797 ,798 ,799 ,800 ,801 ,802 ,803 ,804 ,805 ,
+        806 ,812 ,815 ,816 ,823 ,828 ,848 ,849 ,850 ,851 ,853 ,855 ,861 ,
+        880 ,881 ,882 ,884 ,885 ,895 ,896 ,898 ,921 ,922 ,1006,1008,1023,
+        2000,2001,2002,2003,2004,2005,2006,2007,
         // List for GPRs to be collected in MPIPL path
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
         18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
@@ -321,12 +217,6 @@ ReturnCode sbeDumpArchRegs()
         }
         SBE_INFO(SBE_FUNC "Stash Dump Addr = [0x%08X %08X] ",
                      SBE::higher32BWord(dumpAddr), SBE::lower32BWord(dumpAddr));
-        fapiRc = sbeFetchTimaAddr(timaBarAddr, timaAddrKBrange);
-        if(fapiRc != fapi2::FAPI2_RC_SUCCESS)
-        {
-            SBE_ERROR(SBE_FUNC "Failed to read TimaAddr");
-            break;
-        }
 
         // Initialise the PBA with the above address from stash,
         // The access API would use it in auto-increment mode.
@@ -405,6 +295,7 @@ ReturnCode sbeDumpArchRegs()
                         dumpThreadHdr.coreState = INVALID_CORE_STATE;
                         doRamming = false;
                     }
+                    SBE_DEBUG("RAM SETUP: coreTgt=0x%.8x thread=%d",coreTgt.get(),thread);
                 }
 
                 //Send the thread header content
@@ -415,13 +306,6 @@ ReturnCode sbeDumpArchRegs()
                     SBE_ERROR(SBE_FUNC "Failed to write thread header to hostboot");
                     break;
                 }
-
-                // Collect TIMA architected data collection
-                uint64_t threadTimaAddress = ( timaBarAddr +
-                                 ( (256 + ((chipUnitNum*4)+thread) ) * timaAddrKBrange ));
-                SBE_INFO(SBE_FUNC "Core UnitNum[%d], ThreadInstance[%d], ThreaTimaAddr[0x%08X %08X]",
-                         chipUnitNum,((chipUnitNum*4)+thread),SBE::higher32BWord(threadTimaAddress),
-                         SBE::lower32BWord(threadTimaAddress));
 
                 uint32_t totalRegisters=(sizeof(SPR_GPR_TIMA_list)/sizeof(uint16_t));
                 if(!doRamming)
@@ -471,7 +355,7 @@ ReturnCode sbeDumpArchRegs()
                                      SPR_GPR_TIMA_list[regIdx], &data64, true)
                         if(fapiRc!=FAPI2_RC_SUCCESS)
                         {
-                            SBE_ERROR("Failed to read SPR/GPR Register:threadNr:0x%x"
+                             SBE_ERROR("Failed to read SPR/GPR Register:threadNr:0x%x"
                             "coreChipletId:0x%02x, regNr:%u regType:%u",thread,
                             chipUnitNum, SPR_GPR_TIMA_list[regIdx], dumpRegData.regType);
                             dumpRegData.isRegDataValid = false;//Invalid data
@@ -482,27 +366,8 @@ ReturnCode sbeDumpArchRegs()
                     }
                     else
                     {
-                        fapiRc = getTimaBarBytes( threadTimaAddress, regIdx, data64 );
-                        //If any TIMA collection fails, attempt to collect other TIMA,
-                        if(fapiRc!=FAPI2_RC_SUCCESS)
-                        {
-                            SBE_ERROR("Failed to collect TIMA Offset[%d] for coreChipletId:0x%02x"
-                            "ThreadNumber:%d",SPR_GPR_TIMA_list[regIdx],chipUnitNum,thread);
-                            dumpRegData.isRegDataValid = false;//Invalid data
-                            dumpRegData.regVal = (uint64_t)fapiRc; //Save FAPI_RC
-                            dumpRegData.isFfdcPresent = true;//Indicate pressesnce of FFDC
-                            //If cores are in non-zero state, this failed register will be the
-                            //last register 
-                            if(!doRamming)
-                            {
-                                dumpRegData.isLastReg = true;//No more registers fetched
-                            }
-                            else //Bump up the regIdx to skip read of remaining TIMA offsets
-                            {
-                                uint8_t remainingTIMAOffsets = (TIMA_LIST_SIZE - regIdx)-1 ;
-                                regIdx += remainingTIMAOffsets;
-                            }
-                        }
+                        //TODO:Add logic to fetch TIMA offsets
+                        data64 = 0xDEADBEEFBEEFDEAD;
                     }
                     if(fapiRc == FAPI2_RC_SUCCESS)
                     {
@@ -524,7 +389,7 @@ ReturnCode sbeDumpArchRegs()
                     if(dumpRegData.isFfdcPresent)
                     {
                         //g_FfdcData Will have FFDC data collected on failure
-                        SBE_INFO("FAPI_RC=0x%.8x and FFDC Length =0x%.8x",
+                        SBE_DEBUG("FAPI_RC=0x%.8x and FFDC Length =0x%.8x",
                                   g_FfdcData.fapiRc,g_FfdcData.ffdcLength);
 
                         //Write Length of FFDC
@@ -551,20 +416,21 @@ ReturnCode sbeDumpArchRegs()
                         break;
                     }
                 }//End of Register loop
-                if(fapiRc)
+                 if(fapiRc)
                 {
                     break;
                 }
 
                 if(doRamming)
                 {
+                    SBE_DEBUG("Attempting RAM Cleanup core=0x%.8x Thread=%d",coreTgt.get(),thread);
                     //Clean up the ram core setup
                     SBE_EXEC_HWP_NOARG(fapiRc, ramCore.ram_cleanup)
                     if( fapiRc != FAPI2_RC_SUCCESS )
                     {
                         SBE_ERROR(SBE_FUNC" ram_cleanup failed. threadNr:0x%02X"
                                 " coreChipletId:0x%02X", thread, chipUnitNum);
-                        // Don't break, continue for the next thread
+                         // Don't break, continue for the next thread
                         continue;
                     }
                 }
@@ -575,18 +441,19 @@ ReturnCode sbeDumpArchRegs()
             }
         }//End of Core loop
 
-        // Just see that we are pushing the last PBA Frame here so as to flush
-        // anything which is stuck before of a non-aligned frame.
-        sbeArchRegDumpThreadHdr_t dump_dummy = {};
-        fapiRc = PBAInterface.accessWithBuffer(&dump_dummy, sizeof(dump_dummy), true);
-        if(fapiRc != fapi2::FAPI2_RC_SUCCESS)
+        if(fapiRc == FAPI2_RC_SUCCESS)
         {
-            SBE_ERROR(SBE_FUNC "failed to write the last frame to hostboot");
-            break;
+            // Just see that we are pushing the last PBA Frame here so as to flush
+            // anything which is stuck before of a non-aligned frame.
+            sbeArchRegDumpThreadHdr_t dump_dummy = {};
+            fapiRc = PBAInterface.accessWithBuffer(&dump_dummy, sizeof(dump_dummy), true);
+            if(fapiRc != fapi2::FAPI2_RC_SUCCESS)
+            {
+                SBE_ERROR(SBE_FUNC "failed to write the last frame to hostboot");
+                break;
+            }
         }
-
     }while(0);
-#endif
     SBE_EXIT(SBE_FUNC);
     return fapiRc;
     #undef SBE_FUNC
