45f2d0ec5ea5b7d2426a82107af402418a1edbc8
Murulidhar Nataraju <murulidhar@in.ibm.com>
2020-09-11 02:43:30 -0500

    Update p10_sbe_sync_quiesce_states.C
    Any bit of Bits 0-16 of Attribute ATTR_SBE_SYS_CONFIG set
    indicates that the processor with ATTR_PROC_FABRIC_EFF_TOPOLOGY_ID
    equal to that bit position is functional.
    
    Change-Id: I5cae5f3da0cffd821a4f12bb8326a080770d15a5
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/104189
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>
    Reviewed-by: NAREN A DEVAIAH <naren.devaiah@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/104190
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/perv/p10_sbe_sync_quiesce_states.C
+++ src/import/chips/p10/procedures/hwp/perv/p10_sbe_sync_quiesce_states.C
@@ -52,12 +52,35 @@
 ///----------------------------------------------------------------------------
 const uint64_t SECURITY_SWITCH_REG_ADDR  = 0x00010005;   // Security Switch Register Address
 const uint64_t ADU_XSCOM_BASE_ADDR       = 0x000E03FC00000000;
+//Each 5-bit topology index addresses a 16 terabyte space
+const uint64_t  MMIO_OFFSET_PER_TOPO_INDEX =  0x0000100000000000;
+
+/**
+ * @brief bit definitions for 5-bit mode 0/1 topology inde
+ */
+typedef union
+{
+    struct // GGG0C
+    {
+        uint8_t rsvd1: 3; // 0:2
+        uint8_t group: 3; // 3:5
+        uint8_t rsvd2: 1; // 6
+        uint8_t chip : 1; // 7
+    } mode0;
+    struct // 0GGCC
+    {
+        uint8_t rsvd1: 4; // 0:3
+        uint8_t group: 2; // 4:5
+        uint8_t chip : 2; // 6:7
+    } mode1;
+    uint8_t topoIndex;
+} topologyIndexBits_t;
 
 fapi2::ReturnCode p10_sbe_sync_quiesce_states(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip,
         uint8_t& o_quiesced_status)
 {
     FAPI_INF("p10_sbe_sync_quiesce_states : Entering ...");
-    uint64_t l_sys_config   = 0;
+    fapi2::buffer<uint64_t> sysConfig = 0x0;
     // Flags for ADU operation
     bool l_rnw = true;
     uint64_t address;
@@ -68,30 +91,24 @@ fapi2::ReturnCode p10_sbe_sync_quiesce_states(const fapi2::Target<fapi2::TARGET_
     bool lastGranule  = true;
 
     uint8_t  l_lqa_achieved = 0b00010000;
-    uint8_t l_maxchips = 63;
-
-    uint8_t validchip = 0;
-    uint64_t group_id;
-    uint64_t chip_id = 0;
-    uint8_t data[8];
-    uint8_t read_data;
-
+    uint8_t l_maxchips = 16;
+    uint32_t group_id = 0;
+    uint32_t chip_id = 0;
+    uint8_t data[8] = {0};
+    uint8_t read_data = 0;
+    uint8_t topologyMode = 0;
     const fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> FAPI_SYSTEM;
-
-    // Bit Mapping of l_sys_config
-    // 16-23: .........
-    // 08-15: Node 1, Porc 8/9/10/11/12/13/14/15
-    // 00-07: Node 0, Proc 0/1/2/3/4/5/6/7
 
     //Issue Reads to secure switch register(0x10005) of each of the other chips on the system and check for LQA bit(Bit 3)
     //Return back if LQA=0 for any chip - Start from beginning next time
     //If all valid chips have LQA bit set to '1', return successful RC back to upper level FW code
 
     // Gather system information
-    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_SBE_SYS_CONFIG, FAPI_SYSTEM, l_sys_config));
-    FAPI_INF("Current System configuration : %#010lx - %#x", (l_sys_config >> 32), (uint32_t)l_sys_config);
-
-
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_SBE_SYS_CONFIG, FAPI_SYSTEM, sysConfig()));
+    //Among 0-15 bits, any bit being set indicates that the processor with ATTR_PROC_FABRIC_EFF_TOPOLOGY_ID equal to
+    //that bit position is functional
+    FAPI_INF("Current System configuration : %.8x%.8x", (((uint64_t)sysConfig & 0xFFFFFFFF00000000ull) >> 32),
+             ((uint64_t)sysConfig & 0xFFFFFFFF));
     //Setup ADU flags
     l_myAduFlag.setOperationType(adu_operationFlag::CACHE_INHIBIT);   // XSCOM to PIB are Cache Inhibited
     l_myAduFlag.setTransactionSize(adu_operationFlag::TSIZE_8);       // 8 Bytes register read
@@ -104,31 +121,50 @@ fapi2::ReturnCode p10_sbe_sync_quiesce_states(const fapi2::Target<fapi2::TARGET_
     l_myAduFlag.setEccItagOverrideMode(false);                       // Don't overwrite ECC, not valid for Read operation
     l_myAduFlag.setNumLockAttempts(l_adu_lock_attempts);             // Number of lock attempts - Set to 5.
 
-    //Walk through l_sys_config, if the bit is set(valid chip in system), read LQA bit value
+    //Walk through sysConfig, if the bit is set(valid chip in system), read LQA bit value
     //Set quiesced_status as '1' - As soon as it encounters a chip with LQA not set, set quiesced_status to '0' and break
     o_quiesced_status = 1;
+    FAPI_ATTR_GET(fapi2::ATTR_PROC_FABRIC_TOPOLOGY_MODE, FAPI_SYSTEM, topologyMode);
 
-    while (validchip <= l_maxchips)
+    for(uint8_t TopologyId = 0; TopologyId < l_maxchips; ++TopologyId)
     {
 
-        // Identify if the chip is not matching to originator of the request - Current chip's LQA should already be set
-        //if ((l_sys_config & uint64_t(1)<<(63-validchip)) != l_current_position)
-        //Procedure will ignore current chip position and issue a read to all chips including itself
-        if ((l_sys_config & (uint64_t(1) << (63 - validchip))) != 0)
+        if(sysConfig.getBit(static_cast<uint32_t>(TopologyId)))
         {
+            FAPI_IMP("PROC chip with Effective Fabric Topology ID:0x%.8x is functional", TopologyId);
 
-            // Determine Group(Node) ID
-            group_id = validchip / 8; //Divider
+            // Determine Group(Node) ID and the ChipId
+            //If Mode = 0 , Topology ID: GGG_C
+            //If Mode = 1 , Topology ID: GG_CC
 
-            // Determine Chip ID
-            chip_id = validchip % 8; //Remainder
+            //5-bit mode 0/1 topology index value
+            topologyIndexBits_t topoIndexBits;
+            topoIndexBits.topoIndex = 0;
+
+            if(topologyMode == fapi2::ENUM_ATTR_PROC_FABRIC_TOPOLOGY_MODE_MODE0) //GGG_C
+            {
+                chip_id = TopologyId & 0x1;
+                group_id = (TopologyId & 0xE) >> 1;
+                topoIndexBits.mode0.group = group_id;
+                topoIndexBits.mode0.chip = chip_id;
+            }
+            else //GG_CC
+            {
+                chip_id = TopologyId & 0x3;
+                group_id = (TopologyId & 0xC) >> 2;
+                topoIndexBits.mode1.group = group_id;
+                topoIndexBits.mode1.chip = chip_id;
+            }
+
+            FAPI_INF("group_id=0x%.8x, chip_id=0x%.8x topoIndexBits.topoIndex=0x%x", group_id, chip_id, topoIndexBits.topoIndex);
 
             //XSCOM_BASE_ADDR as per ADU document
             //PIB Address(0x00010005) in address location 30:60
-            //Node Id in location 15:18, Chip id in location 19:21
-            //address =  0x0006010000080028 | (group_id << 45) | (chip_id << 42);
-            address =  ADU_XSCOM_BASE_ADDR | (group_id << 45) | (chip_id << 42) | (SECURITY_SWITCH_REG_ADDR << 3);
-            FAPI_INF("Address obtained =0x%.8x%.8x", ((address & 0xFFFFFFFF00000000ull) >> 32), (address & 0xFFFFFFFF));
+            //Topology Index (5bit) MODE0:GGG0C / MODE1:0GGCC
+            address =  (ADU_XSCOM_BASE_ADDR + (MMIO_OFFSET_PER_TOPO_INDEX * topoIndexBits.topoIndex)) |
+                       (SECURITY_SWITCH_REG_ADDR << 3);
+            FAPI_IMP("TopologyId=%d Address obtained =0x%.8x%.8x", TopologyId, ((address & 0xFFFFFFFF00000000ull) >> 32),
+                     (address & 0xFFFFFFFF));
             // To read from each chip
             // Set up ADU
             FAPI_TRY(p10_adu_setup(i_target_chip, address, l_rnw, l_myAduFlag.setFlag(), granulesBeforeSetup));
@@ -142,19 +178,16 @@ fapi2::ReturnCode p10_sbe_sync_quiesce_states(const fapi2::Target<fapi2::TARGET_
             {
                 //Return Pending return code
                 o_quiesced_status = 0;
-                FAPI_ERR("Current Chip on the system not quiesced - %#018lx", (uint64_t(1) << (63 - validchip)));
+                FAPI_IMP("Proc Chip with Effective Topology Id :0x%.8x is not Quiesced", TopologyId);
                 break; //From while loop with PENDING return code
             }
             else
             {
-                FAPI_IMP("Chip [%#018lx] has LQA set", (uint64_t(1) << (63 - validchip)));
-                FAPI_IMP("Current System configuration : %#010lx - %#x", (l_sys_config >> 32), (uint32_t)l_sys_config);
+                FAPI_IMP("Proc Chip with Effective Topology Id :0x%.8x has LQA set", TopologyId);
             }
         }
 
-        validchip++;
-
-    } //End of while (validchip <= 63)
+    }
 
     FAPI_INF("p10_sbe_sync_quiesce_states : Exiting ...");
 
