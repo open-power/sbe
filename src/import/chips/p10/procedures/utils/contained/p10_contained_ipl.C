0f5fcb88d06451cff7bb11b727e56a9b46ebe05b
Justin Smith <justin.i.smith@ibm.com>
2021-05-13 16:51:28 -0400

    dft/contained - workaround for sbe contained proc
    DFT builds p10_contained_ipl.C with intended use solely on the SBE.
    Therefore, we don not need the Cronus threading fallout protection added
    in 115718.
    
    Change-Id: I25a8eb41cd74ab3efb2d46ef5ecdb47d4629abad
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/116337
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Justin D Ginn <justin.ginn@ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/116346
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: RAJA DAS <rajadas2@in.ibm.com>

--- src/import/chips/p10/procedures/utils/contained/p10_contained_ipl.C
+++ src/import/chips/p10/procedures/utils/contained/p10_contained_ipl.C
@@ -453,6 +453,7 @@ extern "C" {
         const auto eqs_all_cores = i_target.getMulticast(fapi2::MCGROUP_GOOD_EQ, fapi2::MCCORE_ALL);
         const auto perv_eqs_w_cores = i_target.getMulticast<fapi2::TARGET_TYPE_PERV>(fapi2::MCGROUP_GOOD_EQ);
         fapi2::ATTR_ACTIVE_CORES_VEC_Type active_bvec = 0;
+#ifndef DFT
         fapi2::ATTR_SYSTEM_IPL_PHASE_Type sys_ipl_phase;
         fapi2::ATTR_PROC_CHIP_IPL_PHASE_Type chip_ipl_phase;
         fapi2::ATTR_CHIP_EC_FEATURE_CONTAINED_QME_MMA_AVAIL_Type l_contained_qme_mma_avail = 0;
@@ -478,6 +479,31 @@ extern "C" {
             FAPI_TRY(FAPI_ATTR_SET(fapi2::ATTR_SYSTEM_IPL_PHASE, SYS, sys_ipl_phase));
             FAPI_TRY(FAPI_ATTR_SET(fapi2::ATTR_PROC_CHIP_IPL_PHASE, i_target, chip_ipl_phase));
         }
+
+#else //DFT
+
+        fapi2::ATTR_SYSTEM_IPL_PHASE_Type ipl_phase;
+        fapi2::ATTR_CHIP_EC_FEATURE_CONTAINED_QME_MMA_AVAIL_Type l_contained_qme_mma_avail = 0;
+
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_EC_FEATURE_CONTAINED_QME_MMA_AVAIL, i_target, l_contained_qme_mma_avail));
+
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_SYSTEM_IPL_PHASE, SYS, ipl_phase));
+        is_dump_ipl = ipl_phase == fapi2::ENUM_ATTR_SYSTEM_IPL_PHASE_CONTAINED_IPL;
+
+        if (is_dump_ipl)
+        {
+            fapi2::ATTR_RUNN_MODE_Type runn_mode = fapi2::ENUM_ATTR_RUNN_MODE_OFF;
+            FAPI_TRY(FAPI_ATTR_SET(fapi2::ATTR_RUNN_MODE, SYS, runn_mode));
+            FAPI_INF("Running contained dump IPL");
+        }
+        else
+        {
+            ipl_phase = fapi2::ENUM_ATTR_SYSTEM_IPL_PHASE_CONTAINED_IPL;
+            FAPI_INF("Switching to ATTR_SYSTEM_IPL_PHASE[CONTAINED_IPL]");
+            FAPI_TRY(FAPI_ATTR_SET(fapi2::ATTR_SYSTEM_IPL_PHASE, SYS, ipl_phase));
+        }
+
+#endif //DFT
 
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_ACTIVE_CORES_VEC, i_target, active_bvec));
         FAPI_TRY(is_chc_ipl(chc));
