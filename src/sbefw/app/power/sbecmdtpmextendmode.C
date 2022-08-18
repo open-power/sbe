16877521fe3ab4bbfa7744e10048695e13fa6e33
akhis023 <akhis023@in.ibm.com>
2022-08-18 02:36:04 -0500

    Added support for Starting and Stoping Host
    Change-Id: I5e22f218664ec75d3fb67c2beff56fb0d037c7d2
    Signed-off-by: akhis023 <akhis023@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/137364
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: AMIT J TENDOLKAR <amit.tendolkar@in.ibm.com>

--- src/sbefw/app/power/sbecmdtpmextendmode.C
+++ src/sbefw/app/power/sbecmdtpmextendmode.C
@@ -30,6 +30,131 @@
 #include "sbeglobals.H"
 #include "sbeHostUtils.H"
 #include "sbestates.H"
+#include "p10_sbe_stop_hb.H"
+#include "sbeutil.H"
+#include "p10_sbe_instruct_start.H"
+#include "p10_sbe_exit_cache_contained.H"
+
+/**
+ * @brief Enum for operation's supported by ctrlHost()
+ *        0x1 => Start Host Boot
+ *        0x2 => Stop Host Boot
+ *
+ */
+enum hostCtrlFlags: uint8_t
+{
+    STOP_HOST  = 0x1,
+    START_HOST = 0x2,
+};
+
+/**
+ * @brief Function to Start/Stop Host
+ *
+ * @param i_flag Decides the operation to be performed on Host (stop/Start Host)
+ * @return uint32_t FAPI RC if any
+ */
+static uint32_t ctrlHost(hostCtrlFlags i_flag)
+{
+    #define SBE_FUNC " ctrlHost "
+    SBE_ENTER(SBE_FUNC)
+
+    uint32_t fapiRc = FAPI2_RC_SUCCESS;
+
+    do
+    {
+        Target<fapi2::TARGET_TYPE_PROC_CHIP> procTgt = plat_getChipTarget();
+        std::vector<fapi2::Target<fapi2::TARGET_TYPE_CORE>> l_active_core_targets;
+        std::vector<fapi2::Target<fapi2::TARGET_TYPE_CORE>> l_backing_cache_targets;
+        fapi2::Target<fapi2::TARGET_TYPE_CORE> l_master_core_target;
+        fapi2::Target<fapi2::TARGET_TYPE_CORE> l_master_core_pair_target;
+
+        // process input targets
+        SBE_EXEC_HWP(fapiRc,
+                     p10_sbe_exit_cache_contained_validate_core_inputs,
+                     procTgt,
+                     l_active_core_targets,
+                     l_backing_cache_targets,
+                     l_master_core_target,
+                     l_master_core_pair_target)
+
+        if (fapiRc)
+        {
+            SBE_ERROR(SBE_FUNC "Error from p10_sbe_exit_cache_contained_validate_core_inputs");
+            break;
+        }
+
+        if(i_flag == STOP_HOST)
+        {
+            SBE_EXEC_HWP(fapiRc,
+                         p10_sbe_stop_hb,
+                         l_active_core_targets);
+
+            if (fapiRc)
+            {
+                SBE_ERROR(SBE_FUNC "Error from p10_sbe_stop_hb");
+                break;
+            }
+        }
+
+        if(i_flag == START_HOST)
+        {
+            fapi2::ATTR_FUSED_CORE_MODE_Type l_fused_core;
+            //Need to derive the threads to start while resuming the HB based on
+            //the FUSED Core Mode.
+            // Check the fused core mode
+            FAPI_ATTR_GET(fapi2::ATTR_FUSED_CORE_MODE,
+                                fapi2::Target<fapi2::TARGET_TYPE_SYSTEM>(),
+                                l_fused_core);
+
+            if (l_fused_core)
+            {
+                //Start Instruction in thread0, thread1 for both masterCore
+                //and associated fused Core Pair Target. T0 & T1 are fixed
+                SBE_EXEC_HWP(fapiRc,
+                            p10_sbe_instruct_start,
+                            l_master_core_target,
+                            static_cast<ThreadSpecifier>(THREAD0 | THREAD1));
+
+                if (fapiRc)
+                {
+                    SBE_ERROR(SBE_FUNC "Error from p10_sbe_instruct_start for MasterCore"
+                            " in Fused Core mode");
+                    break;
+                }
+
+                SBE_EXEC_HWP(fapiRc,
+                            p10_sbe_instruct_start,
+                            l_master_core_pair_target,
+                            static_cast<ThreadSpecifier>(THREAD0 | THREAD1));
+
+                if (fapiRc)
+                {
+                    SBE_ERROR(SBE_FUNC "Error from p10_sbe_instruct_start for "
+                            "MasterCore Pair Target in Fused Core mode");
+                    break;
+                }
+            }
+            else
+            {
+                // Normal Core, Just start all threads in Master Core
+                SBE_EXEC_HWP(fapiRc,
+                            p10_sbe_instruct_start,
+                            l_master_core_target,
+                            ALL_THREADS);
+
+                if (fapiRc)
+                {
+                    SBE_ERROR(SBE_FUNC "Error from p10_sbe_instruct_start");
+                    break;
+                }
+            }
+        }
+    } while (0);
+
+    SBE_EXIT(SBE_FUNC)
+    return fapiRc;
+    #undef SBE_FUNC
+}
 
 uint32_t sbeTpmExtendMode(uint8_t *i_pArg)
 {
@@ -41,19 +166,6 @@ uint32_t sbeTpmExtendMode(uint8_t *i_pArg)
 
     do
     {
-        if(SBE_GLOBAL->sbePsu2SbeCmdReqHdr.flags & SBE_PSU_ENTER_TPM_EXTEND_MODE)
-        {
-            break;
-        }
-
-        if(SBE_GLOBAL->sbePsu2SbeCmdReqHdr.flags & SBE_PSU_EXIT_TPM_EXTEND_MODE)
-        {
-            break;
-        }
-
-        SBE_ERROR(SBE_FUNC" Not a valid command ");
-        rc = SBE_SEC_COMMAND_NOT_SUPPORTED;
-
         // Send Ack to Host via SBE_SBE2PSU_DOORBELL_SET_BIT1
         // This util method will check internally on the mbox0 register if ACK
         // is requested.
@@ -64,6 +176,48 @@ uint32_t sbeTpmExtendMode(uint8_t *i_pArg)
                     "SBE_SBE2PSU_DOORBELL_SET_BIT1");
             break;
         }
+
+        if(SBE_GLOBAL->sbePsu2SbeCmdReqHdr.flags & SBE_PSU_ENTER_TPM_EXTEND_MODE)
+        {
+            // Stop HB
+            fapiRc = ctrlHost(STOP_HOST);
+            if(fapiRc != FAPI2_RC_SUCCESS)
+            {
+                SBE_ERROR(SBE_FUNC "Failed in p10_sbe_stop_hb() ");
+                break;
+            }
+
+            // Disable Xscoms
+            SBE::disableXscoms();
+
+            // Run SMP HWP
+
+            // Collect secondary TPM measurements
+
+            // Enable Xscoms
+            SBE::enableXscoms();
+
+            // Start HB Alive timer
+
+            // Start HB
+            fapiRc = ctrlHost(START_HOST);
+            if(fapiRc != FAPI2_RC_SUCCESS)
+            {
+                SBE_ERROR(SBE_FUNC "Failed in p10_sbe_instruct_start() ");
+                break;
+            }
+
+            break;
+        }
+
+        if(SBE_GLOBAL->sbePsu2SbeCmdReqHdr.flags & SBE_PSU_EXIT_TPM_EXTEND_MODE)
+        {
+            break;
+        }
+
+        SBE_ERROR(SBE_FUNC " Not a valid command ");
+        rc = SBE_SEC_COMMAND_NOT_SUPPORTED;
+
     }while(0);
 
     if( (fapiRc != FAPI2_RC_SUCCESS) && (rc == SBE_SEC_OPERATION_SUCCESSFUL) )
