56f8f3fd44c781abbd6facb7fe48820db745187b
Aarushi24Soni <aarushi24soni@ibm.com>
2025-01-30 01:06:59 -0600

    Fix for junk characters in sbe_localreg_dump and pibms_reg_dump files.
    STGD: 661418
    Change-Id: I02b7abbc913bb36418ec9c5f6abf73103d2c38a2
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/ppe/+/184934
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>

--- src/tools/debug/sbe-debug.py
+++ src/tools/debug/sbe-debug.py
@@ -428,10 +428,9 @@ def sbeLocalRegister():
         print('Reg Number  Reg Value            Reg String')
         while(l_cnt < os.path.getsize(file_path)):
             str1 = struct.unpack(endianChar + "H",fileHandle.read(2))
-            str2 = fileHandle.read(32)
-            str2 = str2.decode(encoding='utf-8')
+            str2 = fileHandle.read(32).decode().strip('\x00')
             str3 = struct.unpack(endianChar + "Q",fileHandle.read(8))
-            print(str(format(str1[0],'02X')).ljust(11),str(format(str3[0],'016X')).ljust(20),str2.ljust(40))
+            print(str(format(str1[0],'02X')).ljust(11),str(format(str3[0],'016X')).ljust(20),str2)
             l_cnt = l_cnt + 42;
 
         print('********************************************************************')
@@ -470,11 +469,10 @@ def sbeState():
         print('Reg Number       Reg Value            Reg String')
         while(l_cnt < os.path.getsize(file_path)):
             str1 = struct.unpack(endianChar + "Q",fileHandle.read(8))
-            str2 = fileHandle.read(32)
-            str2 = str2.decode(encoding='utf-8')
+            str2 = fileHandle.read(32).decode().strip('\x00')
             str3 = struct.unpack(endianChar + "I",fileHandle.read(4))
             str4 = struct.unpack(endianChar + "Q",fileHandle.read(8))
-            print(str(format(str1[0],'016X')).ljust(11),str(format(str4[0],'016X')).ljust(20),str2.ljust(40))
+            print(str(format(str1[0],'016X')).ljust(11),str(format(str4[0],'016X')).ljust(20),str2)
             l_cnt = l_cnt + 52;
 
         print('********************************************************************')
